1bb365862fffd9f05960e1b158310676dce8229b
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-06-29 21:40:39 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 92 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 int n, m;
 int graph[1000][1000];
-int distances[1000][1000];
+int dist[1000][1000];
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 queue<pair<int, int>> q;
@@ -20,14 +20,14 @@ void bfs() {
         int x = q.front().second;
         
         q.pop();
-        distances[y][x]++;
+        dist[y][x]++;
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
 
-            if (is_inside(nx, ny) && graph[ny][nx] != 0) {
-                if (distances[ny][nx] == -1) {
-                    distances[ny][nx] = distances[y][x];
+            if (is_inside(nx,ny) && graph[ny][nx] != 0) {
+                if (dist[ny][nx] == -1) {
+                    dist[ny][nx] = dist[y][x];
                     q.push({ny, nx});
                 }
             }
@@ -40,15 +40,15 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> graph[i][j];
-            if (graph[i][j] == 0) distances[i][j] = 0;
-            else distances[i][j] = -1;
+            if (graph[i][j] == 0) dist[i][j] = 0;
+            else dist[i][j] = -1;
             if (graph[i][j] == 2) q.push({i, j});
         }
     }
     bfs();
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cout << distances[i][j] << " ";
+            cout << dist[i][j] << " ";
         }
         cout << endl;
     }
