a13370d0dff5f585f95a645dd6bb9ea2d5dfaecf
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-07-08 22:51:58 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int year;
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> year;
+    cout << year - 1946;
+}
