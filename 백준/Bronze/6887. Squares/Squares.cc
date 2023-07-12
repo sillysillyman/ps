1874dbf6edfbe26448d884c2844b688218d3a522
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-07-12 19:35:00 +0900

    [Bronze IV] Title: Squares, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/6887. Squares/Squares.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int N;
+
+    cin >> N;
+    cout << "The largest square has side length " << (int)sqrt(N) << ".";
+}
