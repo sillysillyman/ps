b9b0becafbdef6da5410570eb442d1cb3e7941c4
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-07-20 06:17:44 +0900

    [Bronze V] Title: Can you add this?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    int x, y;
+
+    cin >> N;
+    while (N--) {
+        cin >> x >> y;
+        cout << x + y << endl;
+    }
+}
