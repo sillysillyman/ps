dd82b13870d250f77a6dc657f9305c65067b97f9
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-07-24 21:10:35 +0900

    [Silver III] Title: 완전한 수열, Time: 12 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/26090. 완전한 수열/완전한 수열.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int primes[1000000];
+
+void print(vector<int> v) {
+    for (auto i : v) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+void eras() {
+    fill(primes, primes + 1000000, 1);
+    primes[0] = 0;
+    primes[1] = 0;
+    for (int i = 0; i < 1000000; i++) {
+        if (primes[i]) {
+            for (int j = 2; i*j < 1000000; j++) primes[i*j] = 0;
+        }
+    }
+}
+
+int main() {
+    int N;
+    int cnt = 0;
+
+    cin >> N;
+    
+    vector<int> sequence(N);
+    vector<int> accumulated(N);
+
+    eras();
+    for (int i = 0; i < N; i++) {
+        cin >> sequence[i];
+        if (i == 0) accumulated[0] = sequence[0];
+        else accumulated[i] = accumulated[i - 1] + sequence[i];
+    }
+    // print(sequence);
+    // print(accumulated);
+    for (int i = 0; i < N; i++) {
+        for (int j = i; j < N; j++) {
+            if (!primes[j - i + 1]) continue;
+            if (i == 0) {
+                if (primes[accumulated[j]]) cnt++;
+            } else {
+                if (primes[accumulated[j] - accumulated[i - 1]]) cnt++;
+            }
+        }
+    }
+    cout << cnt;
+}
+// 0 1  2  3
+// 2 7  4  1
+// 2 9 13 14
