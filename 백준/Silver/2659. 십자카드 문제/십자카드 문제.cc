985f960bb68d398baacd3a737f8d9e22a258c44b
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-07-28 08:32:03 +0900

    [Silver III] Title: 십자카드 문제, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2659. 십자카드 문제/십자카드 문제.cc
+++ 백준/Silver/2659. 십자카드 문제/십자카드 문제.cc
@@ -68,7 +68,6 @@ int main() {
         cross_card.pop();
         cross_card.push(top);
     }
-    // cout << "clock_number: " << clock_number << endl;
     for (int i = 1; i <= 9; i++) {
         for (int j = 1; j <= 9; j++) {
             for (int k = 1; k <= 9; k++) {
