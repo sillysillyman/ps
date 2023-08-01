d0253f15e7b0b881c0d7734829274fe3bda8ca51
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-08-01 16:42:34 +0900

    [Silver IV] Title: 나이트 투어, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1331. 나이트 투어/나이트 투어.cc
+++ 백준/Silver/1331. 나이트 투어/나이트 투어.cc
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-bool is_inside(int r, int c) {
-    return r >= 0 && r < 6 && c >= 0 && c < 6;
-}
-
 int main() {
     bool is_alreay_printed = false;
     int chessboard[6][6] = {0, };
@@ -21,10 +17,8 @@ int main() {
         int c = pos[0] - 'A';
 
         if (chessboard[r][c] && !is_alreay_printed) {
-            // cout << "visited!\n";
             cout << "Invalid\n";
             is_alreay_printed = true;
-            // return 0;
         } else chessboard[r][c] = 1;
         if (i == 0) {
             curr = {r, c};
@@ -32,9 +26,7 @@ int main() {
         }
         else {
             prev = curr;
-            // cout << "prev: " << prev.first << " " << prev.second << endl;
             curr = {r, c};
-            // cout << "curr: " << curr.first << " " << curr.second << endl;
 
             bool valid = false;
 
@@ -48,8 +40,6 @@ int main() {
             else if (make_pair(prev.first - 1, prev.second - 2) == curr) valid = true;
             if (i == 35) {
                 valid = false;
-                // cout << "curr: " << curr.first << ", " << curr.second << endl;
-                // cout << "init: " << init.first << ", " << init.second << endl;
                 if (make_pair(curr.first - 2, curr.second - 1) == init) valid = true;
                 else if (make_pair(curr.first - 2, curr.second + 1) == init) valid = true;
                 else if (make_pair(curr.first - 1, curr.second + 2) == init) valid = true;
@@ -60,19 +50,10 @@ int main() {
                 else if (make_pair(curr.first - 1, curr.second - 2) == init) valid = true;
             }
             if (!valid && !is_alreay_printed) {
-                // cout << "invalid movement\n";
                 cout << "Invalid\n";
                 is_alreay_printed = true;
-                // return 0;
             }
         }
     }
     if (!is_alreay_printed) cout << "Valid";
 }
-// 6 1 1 1 1 1 1
-// 5 1 1 1 1 1 1
-// 4 1 1 1 1 1 1
-// 3 1 1 1 1 1 1
-// 2 1 1 1 1 1 1
-// 1 1 1 1 1 1 1 
-//   A B C D E F
