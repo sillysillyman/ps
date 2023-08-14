05b38b268c2766f95e1eb0e0c2de2bc28dd181f1
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-08-14 22:23:57 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 4 ms, Memory: 5188 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
@@ -12,19 +13,28 @@ int main() {
     int q;
     char a;
     int l, r;
+    map<int, map<char, int>> m;
 
     cin >> S;
     cin >> q;
+    for (int i = 0; i < S.length(); i++) {
+        if (i == 0) m[i][S[i]]++;
+        else {
+            m[i] = m[i - 1];
+            m[i][S[i]]++;
+        }
+    }
+    // for (auto i : m) {
+    //     cout << i.first << " = {";
+    //     for (auto j : i.second) {
+    //         cout << j.first << ": " << j.second << ", ";
+    //     }
+    //     cout << "}\n";
+    // }
     for (int i = 0; i < q; i++) {
         cin >> a >> l >> r;
-        for (int i = 0; i < S.length(); i++) {
-            if (i == 0 && S[i] != a) accumulated[i] = 0;
-            else if (i == 0 && S[i] == a) accumulated[i] = 1;
-            else if (S[i] == a) accumulated[i] = accumulated[i - 1] + 1;
-            else accumulated[i] = accumulated[i - 1];
-        }
-        if (l == 0) cout << accumulated[r] << '\n';
-        else cout << accumulated[r] - accumulated[l - 1] << '\n';
+        if (l == 0) cout << m[r][a] << '\n';
+        else cout << m[r][a] - m[l - 1][a] << '\n';
     }
 }
 // 0 1 2 3 4 5 6 7 8 9 10 11 12
