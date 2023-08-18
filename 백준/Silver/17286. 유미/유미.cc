e8541b5ef8c80ef5c9feb9e4fdec8a30dbabc83f
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-08-18 21:32:54 +0900

    [Silver V] Title: 유미, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/17286. 유미/유미.cc
+++ 백준/Silver/17286. 유미/유미.cc
@@ -1,13 +1,9 @@
 #include <iostream>
-#include <algorithm>
 #include <cmath>
 
 using namespace std;
 
 int main() {
-    double dist;
-    double yuumi_dist[3];
-    double sides[3];
     pair<int, int> yuumi;
     pair<int, int> pos[3];
 
@@ -15,20 +11,24 @@ int main() {
     cin >> pos[0].first >> pos[0].second;
     cin >> pos[1].first >> pos[1].second;
     cin >> pos[2].first >> pos[2].second;
-    sides[0] = sqrt(pow(pos[0].first - pos[1].first, 2) + pow(pos[0].second - pos[1].second, 2));
-    sides[1] = sqrt(pow(pos[1].first - pos[2].first, 2) + pow(pos[1].second - pos[2].second, 2));
-    sides[2] = sqrt(pow(pos[2].first - pos[0].first, 2) + pow(pos[2].second - pos[0].second, 2));
+
+    double yuumi_dist[3];
+    double sides[3];
 
     yuumi_dist[0] = sqrt(pow(yuumi.first - pos[0].first, 2) + pow(yuumi.second - pos[0].second, 2));
     yuumi_dist[1] = sqrt(pow(yuumi.first - pos[1].first, 2) + pow(yuumi.second - pos[1].second, 2));
     yuumi_dist[2] = sqrt(pow(yuumi.first - pos[2].first, 2) + pow(yuumi.second - pos[2].second, 2));
-
+    sides[0] = sqrt(pow(pos[0].first - pos[1].first, 2) + pow(pos[0].second - pos[1].second, 2));
+    sides[1] = sqrt(pow(pos[1].first - pos[2].first, 2) + pow(pos[1].second - pos[2].second, 2));
+    sides[2] = sqrt(pow(pos[2].first - pos[0].first, 2) + pow(pos[2].second - pos[0].second, 2));
+    
+    double dist;
+    
     dist = yuumi_dist[0] + sides[0] + sides[1];
     dist = min(dist, yuumi_dist[0] + sides[2] + sides[1]);
     dist = min(dist, yuumi_dist[1] + sides[1] + sides[2]);
     dist = min(dist, yuumi_dist[1] + sides[0] + sides[2]);
     dist = min(dist, yuumi_dist[2] + sides[2] + sides[0]);
     dist = min(dist, yuumi_dist[2] + sides[1] + sides[0]);
-
     cout << (int)dist; 
 }
