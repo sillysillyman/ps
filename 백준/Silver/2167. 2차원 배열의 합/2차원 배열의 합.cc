c0c3da3ca0b9146e19f3b1b624cea62030d39959
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-08-21 06:55:15 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1296 ms, Memory: 2372 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[300][300];
+
+int main() {
+    int N, M;
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) cin >> arr[i][j];
+    }
+
+    int K;
+    int i, j, x, y;
+
+    cin >> K;
+    while (K--) {
+        int sum = 0;
+
+        cin >> i >> j >> x >> y;
+        for (int k = i - 1; k < x; k++) {
+            for (int l = j - 1; l < y; l++) sum += arr[k][l];
+        }
+        cout << sum << '\n';
+    }
+}
