c9adbdf72fdc7b234168e3405f4c709ca988042c
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-08-28 18:24:55 +0900

    [Silver II] Title: 이동하기, Time: 232 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int maze[1000][1000] = {};
+int dp[1000][1000] = {};
+
+int main() {
+    int N, M;
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) cin >> maze[i][j];
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (i == 0 && j == 0) dp[i][j] = maze[i][j];
+            else if (i == 0) dp[i][j] = dp[i][j - 1] + maze[i][j];
+            else if (j == 0) dp[i][j] = dp[i - 1][j] + maze[i][j];
+            else dp[i][j] = max(dp[i][j - 1], max(dp[i - 1][j], dp[i - 1][j - 1])) + maze[i][j];
+        }
+    }
+    cout << dp[N - 1][M - 1];
+}
