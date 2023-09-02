69aec2da322708454504f3ef7ea8e85e033a6668
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-09-02 16:18:58 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -8,21 +8,17 @@ int arr[9];
 int nums[9];
 int visited[9];
 
-void dfs(int cnt) {
+void dfs(int idx, int cnt) {
     if (cnt == M) {
         for (int i = 0; i < M; i++) cout << arr[i] << ' ';
         cout << '\n';
         return;
     }
-    for (int i = cnt; i < N; i++) {
+    for (int i = idx; i < N; i++) {
         if (!visited[i]) {
             visited[i] = 1;
-            if (arr[cnt - 1] < nums[i] || cnt == 0) arr[cnt] = nums[i];
-            else {
-                visited[i] = 0;
-                continue;
-            }
-            dfs(cnt + 1);
+            arr[cnt] = nums[i];
+            dfs(i + 1, cnt + 1);
             visited[i] = 0;
         }
     }
@@ -32,5 +28,5 @@ int main() {
     cin >> N >> M;
     for (int i = 0; i < N; i++) cin >> nums[i];
     sort(nums, nums + N);
-    dfs(0);
+    dfs(0, 0);
 }
