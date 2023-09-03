fed38025d3a4ec1001094ddbead450f00d7e01a7
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-09-03 20:13:19 +0900

    [Bronze IV] Title: Equality, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/13985. Equality/Equality.cc
+++ 백준/Bronze/13985. Equality/Equality.cc
@@ -4,16 +4,12 @@ using namespace std;
 
 int main() {
     int a, b, c;
-    char op;
     string equality;
 
     getline(cin, equality);
     a = equality[0] - '0';
     b = equality[4] - '0';
     c = equality[8] - '0';
-    op = equality[2];
-    if (op == '+') {
-        if (a + b == c) cout << "YES";
-        else cout << "NO";
-    }
+    if (a + b == c) cout << "YES";
+    else cout << "NO";
 }
