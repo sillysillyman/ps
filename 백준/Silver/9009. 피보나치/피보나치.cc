b9cc64940e36c62e517322c2570664f3328c6719
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-09-04 22:20:54 +0900

    [Silver I] Title: 피보나치, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9009. 피보나치/피보나치.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> fibo;
+
+int main() {
+    int T, n;
+
+    fibo.push_back(0);
+    fibo.push_back(1);
+    while (true) {
+        int num = *(fibo.end() - 2) + *(fibo.end() - 1);
+
+        if (num > 1000000000 || num < 0) break;
+        fibo.push_back(num);
+    }
+    cin >> T;
+    while (T--) {
+        cin >> n;
+
+        string res = "";
+        
+        for (int i = fibo.size() - 1; i >= 0; i--) {
+            if (n == 0) break;
+            if (fibo[i] > n) continue;
+            n -= fibo[i];
+            res = to_string(fibo[i]) + " " + res;
+        }
+        cout << res << '\n';
+    }
+}
