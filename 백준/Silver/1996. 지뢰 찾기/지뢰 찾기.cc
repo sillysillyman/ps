195c9672eb9865346d354b7992f7c524b4bec41b
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-11-22 17:47:33 +0900

    [Silver V] Title: 지뢰 찾기, Time: 44 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.cc
+++ 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.cc
@@ -68,6 +68,8 @@ char sweep(int i, int j) {
 }
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
   cin >> N;
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) cin >> map[i][j];
