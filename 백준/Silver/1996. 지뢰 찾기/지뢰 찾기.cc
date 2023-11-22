5701b829e20aff36027f9faad597499bcfb5a58e
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-11-22 17:47:08 +0900

    [Silver V] Title: 지뢰 찾기, Time: 96 ms, Memory: 2996 KB -BaekjoonHub

---
+++ 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+char map[1000][1000];
+
+char sweep(int i, int j) {
+  if (map[i][j] != '.') return '*';
+
+  int cnt = 0;
+
+  if (i == 0) {
+    if (j == 0) {
+      if (isdigit(map[i][j + 1])) cnt += map[i][j + 1] - '0';
+      if (isdigit(map[i + 1][j])) cnt += map[i + 1][j] - '0';
+      if (isdigit(map[i + 1][j + 1])) cnt += map[i + 1][j + 1] - '0';
+    } else if (j == N - 1) {
+      if (isdigit(map[i][j - 1])) cnt += map[i][j - 1] - '0';
+      if (isdigit(map[i + 1][j - 1])) cnt += map[i + 1][j - 1] - '0';
+      if (isdigit(map[i + 1][j])) cnt += map[i + 1][j] - '0';
+    } else {
+      if (isdigit(map[i][j - 1])) cnt += map[i][j - 1] - '0';
+      if (isdigit(map[i][j + 1])) cnt += map[i][j + 1] - '0';
+      if (isdigit(map[i + 1][j - 1])) cnt += map[i + 1][j - 1] - '0';
+      if (isdigit(map[i + 1][j])) cnt += map[i + 1][j] - '0';
+      if (isdigit(map[i + 1][j + 1])) cnt += map[i + 1][j + 1] - '0';
+    }
+  } else if (i == N - 1) {
+    if (j == 0) {
+      if (isdigit(map[i - 1][j])) cnt += map[i - 1][j] - '0';
+      if (isdigit(map[i - 1][j + 1])) cnt += map[i - 1][j + 1] - '0';
+      if (isdigit(map[i][j + 1])) cnt += map[i][j + 1] - '0';
+    } else if (j == N - 1) {
+      if (isdigit(map[i - 1][j - 1])) cnt += map[i - 1][j - 1] - '0';
+      if (isdigit(map[i - 1][j])) cnt += map[i - 1][j] - '0';
+      if (isdigit(map[i][j - 1])) cnt += map[i][j - 1] - '0';
+    } else {
+      if (isdigit(map[i - 1][j - 1])) cnt += map[i - 1][j - 1] - '0';
+      if (isdigit(map[i - 1][j])) cnt += map[i - 1][j] - '0';
+      if (isdigit(map[i - 1][j + 1])) cnt += map[i - 1][j + 1] - '0';
+      if (isdigit(map[i][j - 1])) cnt += map[i][j - 1] - '0';
+      if (isdigit(map[i][j + 1])) cnt += map[i][j + 1] - '0';
+    }
+  } else if (j == 0) {
+    if (isdigit(map[i - 1][j])) cnt += map[i - 1][j] - '0';
+    if (isdigit(map[i - 1][j + 1])) cnt += map[i - 1][j + 1] - '0';
+    if (isdigit(map[i][j + 1])) cnt += map[i][j + 1] - '0';
+    if (isdigit(map[i + 1][j])) cnt += map[i + 1][j] - '0';
+    if (isdigit(map[i + 1][j + 1])) cnt += map[i + 1][j + 1] - '0';
+  } else if (j == N - 1) {
+    if (isdigit(map[i - 1][j - 1])) cnt += map[i - 1][j - 1] - '0';
+    if (isdigit(map[i - 1][j])) cnt += map[i - 1][j] - '0';
+    if (isdigit(map[i][j - 1])) cnt += map[i][j - 1] - '0';
+    if (isdigit(map[i + 1][j - 1])) cnt += map[i + 1][j - 1] - '0';
+    if (isdigit(map[i + 1][j])) cnt += map[i + 1][j] - '0';
+  } else {
+    if (isdigit(map[i - 1][j - 1])) cnt += map[i - 1][j - 1] - '0';
+    if (isdigit(map[i - 1][j])) cnt += map[i - 1][j] - '0';
+    if (isdigit(map[i - 1][j + 1])) cnt += map[i - 1][j + 1] - '0';
+    if (isdigit(map[i][j - 1])) cnt += map[i][j - 1] - '0';
+    if (isdigit(map[i][j + 1])) cnt += map[i][j + 1] - '0';
+    if (isdigit(map[i + 1][j - 1])) cnt += map[i + 1][j - 1] - '0';
+    if (isdigit(map[i + 1][j])) cnt += map[i + 1][j] - '0';
+    if (isdigit(map[i + 1][j + 1])) cnt += map[i + 1][j + 1] - '0';
+  }
+  return (cnt < 10 ? cnt + '0' : 'M');
+}
+
+int main() {
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) cin >> map[i][j];
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) cout << sweep(i, j);
+    cout << '\n';
+  }
+}
