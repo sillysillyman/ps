0949ecbd770d7ed440205500e50d7c5ca02f11c5
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-11-29 16:21:04 +0900

    [Bronze II] Title: 저항, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.cc
+++ 백준/Bronze/1076. 저항/저항.cc
@@ -1,26 +1,24 @@
 #include <iostream>
 #include <map>
-#include <string>
 
 using namespace std;
 
+typedef long long ll;
+
 int main() {
-  string resistance;
+  ll resistance;
   string color1, color2, color3;
-  map<string, pair<string, string>> info = {
-      {"black", {"0", "1"}},        {"brown", {"1", "10"}},
-      {"red", {"2", "100"}},        {"orange", {"3", "1000"}},
-      {"yellow", {"4", "10000"}},   {"green", {"5", "100000"}},
-      {"blue", {"6", "1000000"}},   {"violet", {"7", "10000000"}},
-      {"grey", {"8", "100000000"}}, {"white", {"9", "1000000000"}}};
+  map<string, pair<int, int>> info = {
+      {"black", {0, 1}},        {"brown", {1, 10}},
+      {"red", {2, 100}},        {"orange", {3, 1000}},
+      {"yellow", {4, 10000}},   {"green", {5, 100000}},
+      {"blue", {6, 1000000}},   {"violet", {7, 10000000}},
+      {"grey", {8, 100000000}}, {"white", {9, 1000000000}}};
 
   cin >> color1 >> color2 >> color3;
-  if (color1 == "black" && color2 == "black")
-    resistance = "0";
-  else {
-    resistance =
-        info[color1].first + info[color2].first + info[color3].second.substr(1);
-    if (resistance[0] == '0') resistance = resistance.substr(1);
-  }
+
+  resistance =
+      (ll)(info[color1].first * 10 + info[color2].first) * info[color3].second;
+
   cout << resistance;
 }
