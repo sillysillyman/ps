920c4dd15239c435701073229bc57119243c5948
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-12-06 17:42:20 +0900

    [Silver I] Title: 어두운 건 무서워, Time: 868 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int photo[1000][1000];
+
+int main() {
+  int R, C, Q;
+  int r1, c1, r2, c2;
+
+  cin >> R >> C >> Q;
+  for (int i = 0; i < R; i++) {
+    for (int j = 0; j < C; j++) {
+      cin >> photo[i][j];
+      if (j != 0) photo[i][j] += photo[i][j - 1];
+    }
+  }
+  while (Q--) {
+    cin >> r1 >> c1 >> r2 >> c2;
+    r1--, c1--, r2--, c2--;
+
+    int brightness = 0;
+
+    for (int i = r1; i <= r2; i++) {
+      if (c1 == 0)
+        brightness += photo[i][c2];
+      else
+        brightness += photo[i][c2] - photo[i][c1 - 1];
+    }
+    brightness /= (r2 - r1 + 1) * (c2 - c1 + 1);
+    cout << brightness << '\n';
+  }
+}
