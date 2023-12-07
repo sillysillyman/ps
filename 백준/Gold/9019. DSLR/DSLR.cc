58b678ecd3796ffbac89ecf3de0f6f7cd26e56bc
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-12-08 03:13:46 +0900

    [Gold IV] Title: DSLR, Time: 3784 ms, Memory: 2276 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+const int MOD = 10000;
+string commands[4] = {"D", "S", "L", "R"};
+
+int calc(int n, string command) {
+  if (command == "D")
+    return (2 * n) % MOD;
+  else if (command == "S")
+    return (n == 0 ? 9999 : n - 1);
+  else if (command == "L")
+    return (n % 1000) * 10 + n / 1000;
+  else if (command == "R")
+    return (n % 10) * 1000 + n / 10;
+  else
+    return -1;
+}
+
+void bfs(int a, int b) {
+  int visited[10000] = {};
+  queue<pair<string, int>> q;
+
+  q.push({"", a});
+  visited[a] = 1;
+  while (!q.empty()) {
+    auto [command, n] = q.front();
+
+    q.pop();
+    if (n == b) {
+      cout << command << '\n';
+      break;
+    }
+
+    int m;
+
+    m = (2 * n) % MOD;
+    if (!visited[m]) {
+      visited[m] = 1;
+      q.push({command + "D", m});
+    }
+    m = (n == 0 ? 9999 : n - 1);
+    if (!visited[m]) {
+      visited[m] = 1;
+      q.push({command + "S", m});
+    }
+    m = (n % 1000) * 10 + n / 1000;
+    if (!visited[m]) {
+      visited[m] = 1;
+      q.push({command + "L", m});
+    }
+    m = (n % 10) * 1000 + n / 10;
+    if (!visited[m]) {
+      visited[m] = 1;
+      q.push({command + "R", m});
+    }
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int T;
+  int A, B;
+
+  cin >> T;
+  while (T--) {
+    cin >> A >> B;
+    bfs(A, B);
+  }
+}
