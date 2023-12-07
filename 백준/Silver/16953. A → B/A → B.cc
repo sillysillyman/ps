f4f5951b1303a2ead37c5fcedcb3c3ca0a555404
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-12-08 03:55:01 +0900

    [Silver II] Title: A → B, Time: 20 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+const int MOD = 1000000000;
+map<int, int> visited;
+
+int bfs(int a, int b) {
+  queue<pair<int, int>> q;
+
+  q.push({a, 1});
+  visited[a] = 1;
+  while (!q.empty()) {
+    auto [n, cnt] = q.front();
+
+    // cout << "n: " << n << " cnt: " << cnt << endl;
+    q.pop();
+    if (n == b) return cnt;
+    else if (n > b) break;    
+    if (long long m = (long long)n * 2; m <= b && !visited[m]) {
+      q.push({m, cnt + 1});
+      visited[m] = 1;
+    }
+    if (long long m = (long long)n * 10 + 1; m <= b && !visited[m]) {
+      q.push({m, cnt + 1});
+      visited[m] = 1;
+    }
+  }
+  return -1;
+}
+
+int main() {
+  int A, B;
+
+  cin >> A >> B;
+  cout << bfs(A, B);
+}
