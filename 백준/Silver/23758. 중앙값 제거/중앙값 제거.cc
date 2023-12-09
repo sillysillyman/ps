cdbfbfe5e2abcfcf747140848f1740cc9846393d
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-12-10 02:07:43 +0900

    [Silver I] Title: 중앙값 제거, Time: 404 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Silver/23758. 중앙값 제거/중앙값 제거.cc
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int arr[2000000];
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int N;
+  int cnt = 1;
+
+  cin >> N;
+  for (int i = 0; i < N; i++) cin >> arr[i];
+  sort(arr, arr + N);
+  for (int i = 0; i < (N + 1) / 2 ; i++) cnt += (int)log2(arr[i]);
+  cout << cnt;
+}
