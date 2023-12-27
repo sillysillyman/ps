a659232a2ff245c2e2b8129efba377074928bd2e
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-12-28 02:03:31 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -7,12 +7,11 @@ int K;
 vector<int> tree;
 vector<int> levels[10];
 
-void get_nums_of_level(int begin, int mid, int end, int level, int depth) {
+void get_nums_of_level(int begin, int mid, int end, int level) {
   levels[level].push_back(tree[mid]);
-  if (depth == K) return;
-  get_nums_of_level(begin, (begin + mid - 1) / 2, mid - 1, level + 1,
-                    depth + 1);
-  get_nums_of_level(mid + 1, (mid + end + 1) / 2, end, level + 1, depth + 1);
+  if (begin == end) return;
+  get_nums_of_level(begin, (begin + mid - 1) / 2, mid - 1, level + 1);
+  get_nums_of_level(mid + 1, (mid + end + 1) / 2, end, level + 1);
 }
 
 int main() {
@@ -26,7 +25,7 @@ int main() {
     cin >> num;
     tree.push_back(num);
   }
-  get_nums_of_level(0, tree.size() / 2, tree.size() - 1, 0, 0);
+  get_nums_of_level(0, tree.size() / 2, tree.size() - 1, 0);
   for (int i = 0; i < K; i++) {
     for (int num : levels[i]) cout << num << ' ';
     cout << '\n';
