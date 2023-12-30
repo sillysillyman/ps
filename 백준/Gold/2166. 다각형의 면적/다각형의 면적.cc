b8cad14ed36b68418faee31105ff6ca6652e2620
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-12-30 22:50:14 +0900

    [Gold V] Title: 다각형의 면적, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct point {
+  int x, y;
+};
+
+long long cross_product(point origin, point p, point q) {
+  point u = {p.x - origin.x, p.y - origin.y};
+  point v = {q.x - origin.x, q.y - origin.y};
+
+  return (long long)u.x * v.y - (long long)v.x * u.y;
+}
+
+int main() {
+  int N;
+  long double area = 0;
+
+  cin >> N;
+
+  point p;
+  vector<point> points(N);
+
+  for (int i = 0; i < N; i++) {
+    cin >> p.x >> p.y;
+    points[i] = p;
+  }
+//   cout << '\n';
+//   for (int i = 0; i < N; i++) cout << points[i].x << ' ' << points[i].y << '\n';
+  cout << fixed;
+  cout.precision(1);
+  for (int i = 1; i < N - 1; i++) {
+    area += cross_product(points[0], points[i + 1], points[i]) / 2.0;
+  }
+  cout << abs(area);
+}
