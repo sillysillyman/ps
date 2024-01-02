749ecb3c1c0dd3ae740c0b271df617454c27b599
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-03 00:40:21 +0900

    [Gold V] Title: 부분 삼각 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1548. 부분 삼각 수열/부분 삼각 수열.cc
+++ 백준/Gold/1548. 부분 삼각 수열/부분 삼각 수열.cc
@@ -14,19 +14,15 @@ int main() {
     return 0;
   }
   sort(sequence, sequence + N);
-//   for (int i = 0; i < N; ++i) cout << sequence[i] << ' ';
-//   cout << '\n';
-  
+
   int cnt, max_cnt = 0;
 
   for (int i = 0; i < N - 2; i++) {
-    // cout << "i: " << i << '\n';
     cnt = 2;
     for (int j = i + 2; j < N; j++) {
       if (sequence[i] + sequence[i + 1] > sequence[j]) cnt++;
     }
     max_cnt = max(max_cnt, cnt);
-    // cout << "max_cnt: " << max_cnt << '\n';
   }
   cout << max_cnt;
 }
