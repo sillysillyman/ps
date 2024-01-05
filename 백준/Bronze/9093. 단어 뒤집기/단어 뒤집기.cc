208a4c45f9e6c56ef0e65a91c1ed7996b6c25d0d
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-05 19:51:42 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 420 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.cc
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<string> split(string str, char delimiter) {
+  istringstream iss(str);
+  string buffer;
+  vector<string> splitted;
+
+  while (getline(iss, buffer, delimiter)) splitted.push_back(buffer);
+  return splitted;
+}
+
+int main() {
+  int T;
+
+  cin >> T;
+  cin.ignore();
+  while (T--) {
+    string sentence;
+    vector<string> words;
+
+    getline(cin, sentence);
+    words = split(sentence, ' ');
+    for (auto& word : words) reverse(word.begin(), word.end());
+    for (auto& word : words) cout << word << ' ';
+    cout << '\n';
+  }
+}
