d399efc80bf88e5675a7faf2deb4fd2d5c534cb2
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-07 02:33:16 +0900

    [Gold V] Title: A와 B 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.cc
+++ 백준/Gold/12919. A와 B 2/A와 B 2.cc
@@ -6,27 +6,16 @@ using namespace std;
 bool found = false;
 
 void dfs(string s, string t) {
-//   cout << t << endl;
   if (s == t) {
     found = true;
     return;
   }
   if (s.length() >= t.length()) return;
-
-  if (t.back() == 'A') {
-    dfs(s, t.substr(0, t.length() - 1));
-
-  }
+  if (t.back() == 'A') dfs(s, t.substr(0, t.length() - 1));
   if (t.front() == 'B') {
-    // string reversed = t;
-
-    // reverse(reversed.begin(), reversed.end());
     reverse(t.begin(), t.end());
-    // cout << "reversed: " << reversed << endl;
-    // dfs(s, reversed.substr(0, reversed.length() - 1));
     dfs(s, t.substr(0, t.length() - 1));
   }
-//   dfs(s, t.substr(0, t.length() - 1));
 }
 
 int main() {
