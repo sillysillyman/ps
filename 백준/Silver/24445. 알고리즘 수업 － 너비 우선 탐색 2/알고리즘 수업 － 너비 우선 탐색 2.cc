95c8749db0fedc1e92e61a876cd10ab45ee4107f
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-08 01:54:05 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 84 ms, Memory: 8840 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int order = 1;
+int visited[100001];
+vector<int> graph[100001];
+
+void bfs(int src) {
+  if (visited[src]) return;
+
+  queue<int> q;
+
+  visited[src] = order++;
+  q.push(src);
+  while (!q.empty()) {
+    int parent = q.front();
+
+    q.pop();
+    for (auto& child : graph[parent]) {
+      if (!visited[child]) {
+        visited[child] = order++;
+        q.push(child);
+      }
+    }
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int N, M, R;
+
+  cin >> N >> M >> R;
+  while (M--) {
+    int u, v;
+
+    cin >> u >> v;
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+  }
+  for (int i = 1; i <= N; i++)
+    sort(graph[i].begin(), graph[i].end(), greater<>());
+  bfs(R);
+  for (int i = 1; i <= N; i++) cout << visited[i] << '\n';
+}
