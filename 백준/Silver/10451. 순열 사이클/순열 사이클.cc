caa3257d768068ca520ae1d74a8d3af04d5f8441
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-08 03:14:57 +0900

    [Silver III] Title: 순열 사이클, Time: 40 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.cc
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.cc
@@ -6,28 +6,29 @@ int cycles;
 bool visited[1001];
 int permutation[1001];
 
-void find_cycle(int src_idx, int idx) {
+void dfs(int src_idx, int idx) {
   if (visited[idx]) return;
   visited[idx] = true;
   if (permutation[idx] == src_idx) {
     cycles++;
     return;
   }
-  find_cycle(src_idx, permutation[idx]);
+  dfs(src_idx, permutation[idx]);
 }
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
   int T, N;
 
   cin >> T;
   while (T--) {
-    cycles = 0;    
+    cycles = 0;
     cin >> N;
     fill(visited + 1, visited + N + 1, false);
     for (int i = 1; i <= N; i++) cin >> permutation[i];
-    for (int i = 1; i <= N; i++) find_cycle(i, i);
+    for (int i = 1; i <= N; i++) dfs(i, i);
     cout << cycles << '\n';
   }
 }
-// 1 2 3 4 5 6 7 8  9 10
-// 2 1 3 4 5 6 7 9 10  8
