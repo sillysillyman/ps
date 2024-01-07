2b87f68872950d897540c1b72c82769df37cbc1c
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-08 01:47:25 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 88 ms, Memory: 10928 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int order = 1;
+int visited[100001];
+vector<int> graph[100001];
+
+void dfs(int src) {
+  if (visited[src]) return;
+  visited[src] = order++;
+  for (auto& child : graph[src]) dfs(child);
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int N, M, R;
+
+  cin >> N >> M >> R;
+  while (M--) {
+    int u, v;
+
+    cin >> u >> v;
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+  }
+  for (int i = 1; i <= N; i++) sort(graph[i].begin(), graph[i].end(), greater<>());
+  dfs(R);
+  for (int i = 1; i <= N; i++) cout << visited[i] << '\n';
+}
