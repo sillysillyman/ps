352aeb0642cf8a19b160c05622cd5a212153f628
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-07 18:39:15 +0900

    [Silver II] Title: 결혼식, Time: 0 ms, Memory: 2304 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.cc
+++ 백준/Silver/5567. 결혼식/결혼식.cc
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
@@ -10,41 +10,36 @@ int visited[501];
 vector<int> people[501];
 
 void bfs() {
-    queue<pair<int, int>> q;
+  queue<pair<int, int>> q;
 
-    q.push({1, 0});
-    visited[1] = 1;
-    while (!q.empty()) {
-        int person = q.front().first;
-        int dist = q.front().second;
+  q.push({1, 0});
+  visited[1] = 1;
+  while (!q.empty()) {
+    auto [person, dist] = q.front();
 
-        q.pop();
-        for (auto i : people[person]) {
-            if (!visited[i]) {
-                q.push({i, dist + 1});
-                visited[i] = 1;
-                if (dist + 1 <= 2) {
-                    // cout << i << '\n';
-                    cnt++;
-                }
-            }
-        }
+    q.pop();
+    for (auto i : people[person]) {
+      if (!visited[i]) {
+        q.push({i, dist + 1});
+        visited[i] = 1;
+        if (dist + 1 <= 2) cnt++;
+      }
     }
+  }
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
 
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int from, to;
+  cin >> n >> m;
+  for (int i = 0; i < m; i++) {
+    int from, to;
 
-        cin >> from >> to;
-        people[from].push_back(to);
-        people[to].push_back(from);
-    }
-    bfs();
-    // cout << "cnt = " << cnt;
-    cout << cnt;
+    cin >> from >> to;
+    people[from].push_back(to);
+    people[to].push_back(from);
+  }
+  bfs();
+  cout << cnt;
 }
