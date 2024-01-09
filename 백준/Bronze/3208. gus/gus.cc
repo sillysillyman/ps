4fab1ddfd841654583d96170ba9ee764b3c8c63a
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-10 04:26:32 +0900

    [Bronze I] Title: gus, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3208. gus/gus.cc
+++ 백준/Bronze/3208. gus/gus.cc
@@ -2,49 +2,9 @@
 
 using namespace std;
 
-int M, N;
-int chocolate[100][100];
-
-bool is_eaten_all() {
-  for (int i = 0; i < M; i++) {
-    for (int j = 0; j < N; j++) {
-      if (!chocolate[i][j]) return false;
-    }
-  }
-  return true;
-}
-
 int main() {
-  int cnt = 0;
+  int M, N;
 
   cin >> M >> N;
-
-  int i = 0, j = 0;
-  pair<int, int> direction;
-  pair<int, int> right = {0, 1};
-  pair<int, int> left = {0, -1};
-  pair<int, int> up = {-1, 0};
-  pair<int, int> down = {1, 0};
-
-  direction = right;
-  while (!is_eaten_all()) {
-    chocolate[i][j] = 1;
-    if ((0 <= i + direction.first && i + direction.first < M &&
-         0 <= j + direction.second && j + direction.second < N) &&
-        !chocolate[i + direction.first][j + direction.second]) {
-      i += direction.first;
-      j += direction.second;
-    } else {
-      if (direction == right)
-        direction = down;
-      else if (direction == left)
-        direction = up;
-      else if (direction == up)
-        direction = right;
-      else if (direction == down)
-        direction = left;
-      cnt++;
-    }
-  }
-  cout << cnt - 1;
+  cout << min(2 * M - 2, 2 * N - 1);
 }
