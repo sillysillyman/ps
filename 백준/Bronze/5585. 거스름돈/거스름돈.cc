f64b52df25f9284a3bfd57932891a597dc673696
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-09 23:41:12 +0900

    [Bronze II] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.cc
+++ 백준/Bronze/5585. 거스름돈/거스름돈.cc
@@ -3,21 +3,10 @@
 using namespace std;
 
 int main() {
-  int cnt = 0;
-  int payment, change;
+  int change;
 
-  cin >> payment;
-  change = 1000 - payment;
-  cnt += change / 500;
-  change %= 500;
-  cnt += change / 100;
-  change %= 100;
-  cnt += change / 50;
-  change %= 50;
-  cnt += change / 10;
-  change %= 10;
-  cnt += change / 5;
-  change %= 5;
-  cnt += change;
-  cout << cnt;
+  cin >> change;
+  change = 1000 - change;
+  cout << change / 500 + (change % 500) / 100 + (change % 100) / 50 +
+              (change % 50) / 10 + (change % 10) / 5 + (change % 5);
 }
