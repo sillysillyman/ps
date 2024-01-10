4811ef5fe3199cf7e4fce45dab9fb5729e7332b1
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-11 02:32:07 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int N, K;
+int times[100001];
+
+bool is_inside(int idx) { return 0 <= idx && idx < 100001; }
+
+void bfs(int src) {
+  queue<int> q;
+
+  q.push(src);
+  while (!q.empty()) {
+    int cur = q.front();
+    // cout << cur << ": " << times[cur] << endl;
+    if (cur == K) {
+      cout << times[cur];
+      return;
+    }
+    q.pop();
+    if (is_inside(cur - 1) && times[cur] + 1 <= times[cur - 1]) {
+      times[cur - 1] = times[cur] + 1;
+      q.push(cur - 1);
+    }
+    if (is_inside(cur + 1) && times[cur] + 1 <= times[cur + 1]) {
+      times[cur + 1] = times[cur] + 1;
+      q.push(cur + 1);
+    }
+    if (is_inside(2 * cur) && times[cur] <= times[2 * cur]) {
+      times[2 * cur] = times[cur];
+      q.push(2 * cur);
+    }
+  }
+}
+
+int main() {
+  cin >> N >> K;
+  fill(times, times + 100001, 100000);
+  times[N] = 0;
+  bfs(N);
+//   cout << '\n';
+//   for (int i = 0; i <= K; i++) {
+//     cout << times[i] << ' ';
+//   }
+}
