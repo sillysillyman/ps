4326ab6cd37dbea1c9070bb74b16ca7b1bee1a94
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-10 20:50:43 +0900

    [Bronze IV] Title: Sounds fishy!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.cc
+++ 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.cc
@@ -2,36 +2,15 @@
 
 using namespace std;
 
-bool is_increasing(int *depth, int size) {
-  for (int i = 1; i < size; ++i) {
-    if (depth[i] <= depth[i - 1]) return false;
-  }
-  return true;
-}
-
-bool is_decreasing(int *depth, int size) {
-  for (int i = 1; i < size; ++i) {
-    if (depth[i] >= depth[i - 1]) return false;
-  }
-  return true;
-}
-
-bool is_constant(int *depth, int size) {
-  for (int i = 1; i < size; ++i) {
-    if (depth[0] != depth[i]) return false;
-  }
-  return true;
-}
-
 int main() {
-  int depth[4];
+  int a, b, c, d;
 
-  for (int i = 0; i < 4; i++) cin >> depth[i];
-  if (is_increasing(depth, 4))
+  cin >> a >> b >> c >> d;
+  if (a < b && b < c && c < d)
     cout << "Fish Rising";
-  else if (is_decreasing(depth, 4))
+  else if (a > b && b > c && c > d)
     cout << "Fish Diving";
-  else if (is_constant(depth, 4))
+  else if (a == b && b == c && c == d)
     cout << "Fish At Constant Depth";
   else
     cout << "No Fish";
