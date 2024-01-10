1db9ea4086d20a4483393a8139d0f0f88191c5a7
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-10 19:57:38 +0900

    [Silver I] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2527. 직사각형/직사각형.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+struct Point {
+  int x, y;
+};
+
+// (p.x, q.y)--(q.x, q.y)
+// |                    |
+// (p.x, p.y)--(q.x, p.y)
+struct Rectangle {
+  Point p, q;
+};
+
+char classify(Rectangle& r1, Rectangle& r2) {
+  if ((r1.q.x == r2.p.x && r1.p.y == r2.q.y) ||
+      (r1.q.x == r2.p.x && r1.q.y == r2.p.y) ||
+      (r1.p.x == r2.q.x && r1.q.y == r2.p.y) ||
+      (r1.p.x == r2.q.x && r1.p.y == r2.q.y)) return 'c';
+  if ((r1.p.x == r2.q.x) || (r1.q.x == r2.p.x)) {
+    if ((r1.p.y <= r2.q.y && r1.q.y >= r2.p.y) ||
+        (r2.p.y <= r1.q.y && r2.q.y >= r1.p.y)) return 'b';
+    else return 'd';
+  } else if ((r1.p.y == r2.q.y) || (r1.q.y == r2.p.y)) {
+    if ((r1.p.x <= r2.q.x && r1.q.x >= r2.p.x) ||
+        (r2.p.x <= r1.q.x && r2.q.x >= r1.p.x)) return 'b';
+    else return 'd';
+  }
+  // else if ((r1.q.x < r2.p.x && r1.p.y > r2.q.y) ||
+  //          (r1.q.x < r2.p.x && r1.q.y < r2.p.y) ||
+  //          (r1.p.x > r2.q.x && r1.q.y < r2.p.y) ||
+  //          (r1.p.x > r2.q.x && r1.p.y > r2.q.y)) return 'd';
+  if (r1.q.x < r2.p.x || r1.p.x > r2.q.x || r1.p.y > r2.q.y || r1.q.y < r2.p.y) return 'd';
+  else return 'a';
+}
+
+int main() {
+  Point p, q, r, s;
+
+  for (int i = 0; i < 4; i++) {
+    cin >> p.x >> p.y >> q.x >> q.y >> r.x >> r.y >> s.x >> s.y;
+
+    Rectangle r1{p, q};
+    Rectangle r2{r, s};
+    cout << classify(r1, r2) << '\n';
+  }
+}
