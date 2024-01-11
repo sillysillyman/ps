6ae98ec2b525ac8f64376918adb9370f36373343
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-11 21:08:09 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -2,51 +2,59 @@
 
 using namespace std;
 
-int chess_board[50][50] = {};
+int board[50][50] = {};
 
 int get_cnt(int i, int j) {
-    int black_started_cnt = 0, white_started_cnt = 0;
+  int black_started_cnt = 0, white_started_cnt = 0;
 
-    for (int k = i; k < i + 8; k ++) {
-        for (int l = j; l < j + 8; l++) {
-            if ((k + l)%2 == 0) {
-                if (chess_board[k][l] == -1) {
-                    white_started_cnt++;
-                } else if (chess_board[k][l] == 1) {
-                    black_started_cnt++;
-                } else continue;
-            } else {
-                if (chess_board[k][l] == 1) {
-                    white_started_cnt++;
-                } else if (chess_board[k][l] == -1) {
-                    black_started_cnt++;
-                } else continue;
-            }
-        }
+  for (int k = i; k < i + 8; k++) {
+    for (int l = j; l < j + 8; l++) {
+      if ((k + l) % 2 == 0) {
+        if (board[k][l] == -1) {
+          white_started_cnt++;
+        } else if (board[k][l] == 1) {
+          black_started_cnt++;
+        } else
+          continue;
+      } else {
+        if (board[k][l] == 1) {
+          white_started_cnt++;
+        } else if (board[k][l] == -1) {
+          black_started_cnt++;
+        } else
+          continue;
+      }
     }
-    return black_started_cnt > white_started_cnt ? white_started_cnt : black_started_cnt;
+  }
+  return black_started_cnt > white_started_cnt ? white_started_cnt
+                                               : black_started_cnt;
 }
 
 int main() {
-    int N, M; // 8 <= N, M <= 50
+  int N, M;
 
-    cin >> N >> M;
+  cin >> N >> M;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            char square;
-            cin >> square;
-            if (square == 'B') chess_board[i][j] = -1;
-            else if (square == 'W') chess_board[i][j] = 1;
-            else continue;
-        }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      char square;
+
+      cin >> square;
+      if (square == 'B')
+        board[i][j] = -1;
+      else if (square == 'W')
+        board[i][j] = 1;
+      else
+        continue;
     }
-    int min_cnt = 50*50;
-    for (int i = 0; i <= N - 8; i++) {
-        for (int j = 0; j <= M - 8; j++) {
-            int cnt = get_cnt(i, j);
-            if (cnt < min_cnt) min_cnt = cnt;
-        }
+  }
+
+  int min_cnt = 50 * 50;
+
+  for (int i = 0; i <= N - 8; i++) {
+    for (int j = 0; j <= M - 8; j++) {
+      if (int cnt = get_cnt(i, j); cnt < min_cnt) min_cnt = cnt;
     }
-    cout << min_cnt;
+  }
+  cout << min_cnt;
 }
