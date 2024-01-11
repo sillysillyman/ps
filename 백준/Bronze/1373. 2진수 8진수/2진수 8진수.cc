f1775de5cb8d0a3b9138a61f1c42404516510a58
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-11 18:48:53 +0900

    [Bronze I] Title: 2진수 8진수, Time: 84 ms, Memory: 4932 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.cc
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.cc
@@ -3,20 +3,27 @@
 using namespace std;
 
 int main() {
-  string binary;
-  string octal;
+  string binary, octal;
 
   cin >> binary;
   while (binary.length() % 3 != 0) binary = "0" + binary;
   for (int i = 0; i < binary.length(); i += 3) {
-    if (stoi(binary.substr(i, 3)) == 0) octal += "0";
-    else if (stoi(binary.substr(i, 3)) == 1) octal += "1";
-    else if (stoi(binary.substr(i, 3)) == 10) octal += "2";
-    else if (stoi(binary.substr(i, 3)) == 11) octal += "3";
-    else if (stoi(binary.substr(i, 3)) == 100) octal += "4";
-    else if (stoi(binary.substr(i, 3)) == 101) octal += "5";
-    else if (stoi(binary.substr(i, 3)) == 110) octal += "6";
-    else if (stoi(binary.substr(i, 3)) == 111) octal += "7";
+    if (stoi(binary.substr(i, 3)) == 0)
+      octal += "0";
+    else if (stoi(binary.substr(i, 3)) == 1)
+      octal += "1";
+    else if (stoi(binary.substr(i, 3)) == 10)
+      octal += "2";
+    else if (stoi(binary.substr(i, 3)) == 11)
+      octal += "3";
+    else if (stoi(binary.substr(i, 3)) == 100)
+      octal += "4";
+    else if (stoi(binary.substr(i, 3)) == 101)
+      octal += "5";
+    else if (stoi(binary.substr(i, 3)) == 110)
+      octal += "6";
+    else if (stoi(binary.substr(i, 3)) == 111)
+      octal += "7";
   }
   cout << octal;
 }
