a0d7412fcb83abc4697e12d23911e4cd806b780c
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-13 20:06:36 +0900

    [Gold IV] Title: 약수의 합, Time: 100 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Gold/17425. 약수의 합/약수의 합.cc
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+long long sums[1000001];
+long long accumulated[1000001];
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int T;
+  
+  for (int i = 1; i < 1000001; i++) {
+    for (int j = 1; i * j < 1000001; j++) {
+      sums[i * j] += i;
+    }
+    if (i == 1) accumulated[i] = sums[i];
+    else accumulated[i] = accumulated[i - 1] + sums[i];
+  }
+  cin >> T;
+  while (T--) {
+    int N;
+
+    cin >> N;
+    cout << accumulated[N] << '\n';
+  }
+}
