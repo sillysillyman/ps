261733eaee4c4fd065c7917e5819dd90ad1b6ebc
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-16 02:56:27 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dp[10001];
+
+int main() {
+  int n, k;
+
+  cin >> n >> k;
+
+  vector<int> coins(n);
+
+  for (int i = 0; i < n; i++) cin >> coins[i];
+  dp[0] = 1;
+  for (int coin : coins) {
+    for (int i = coin; i <= k; i++) dp[i] = dp[i] + dp[i - coin];
+  }
+  cout << dp[k];
+}
