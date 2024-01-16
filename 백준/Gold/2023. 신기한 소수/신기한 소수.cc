3d82b7bc849371883838c361a32c020d5e3893b7
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-16 21:43:17 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int digits[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+bool is_prime(int num) {
+  if (num <= 1) return false;
+  if (num == 2) return true;
+  for (int i = 2; i * i <= num; i++) {
+    if (num % i == 0) return false;
+  }
+  return true;
+}
+
+void dfs(int prime) {  
+  int num = prime;
+
+  for (int i = 0; i < 10; i++) {
+    prime = 10 * num + digits[i];
+    if (!is_prime(prime)) continue;
+    if (to_string(prime).length() == N) cout << prime << '\n';
+    dfs(prime);
+  }
+}
+
+int main() {
+  cin >> N;
+  dfs(0);
+}
