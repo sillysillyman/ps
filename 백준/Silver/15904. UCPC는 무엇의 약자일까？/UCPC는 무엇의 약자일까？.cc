a340b76966363229b70c530af3755c3979967232
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-23 22:50:47 +0900

    [Silver V] Title: UCPC는 무엇의 약자일까?, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15904. UCPC는 무엇의 약자일까？/UCPC는 무엇의 약자일까？.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  bool success[4];
+  string str;
+
+  getline(cin, str);
+  fill(success, success + 4, false);
+  for (auto c : str) {
+    if (c == 'U' && !success[1] && !success[2] && !success[3]) success[0] = true;
+    else if (success[0] && c == 'C' && !success[2] && !success[3]) success[1] = true;
+    else if (success[0] && success[1] && c == 'P' && !success[3]) success[2] = true;
+    else if (success[0] && success[1] && success[2] && c == 'C') success[3] = true;
+  }
+//   cin.ignore();
+//   for (auto i : success) cout << i << ' ';
+//   cout << '\n';
+  if (success[0] && success[1] && success[2] && success[3])
+    cout << "I love UCPC";
+  else
+    cout << "I hate UCPC";
+}
