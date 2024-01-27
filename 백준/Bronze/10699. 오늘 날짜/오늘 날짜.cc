eb77414b52e583f78a738b20c6cfdbe1a039db93
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-01-27 18:17:21 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,15 @@
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  time_t timer = time(NULL);
+  tm *t = localtime(&timer);
+  
+  cout << t->tm_year + 1900 << '-';
+  cout.width(2);
+  cout.fill('0');
+  cout << t->tm_mon + 1 << '-';
+  cout << t->tm_mday;
+}
