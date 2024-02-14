41a092f68972352b29788698913d3bee281ee2c7
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-14 13:13:38 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.cc
+++ 백준/Silver/1072. 게임/게임.cc
@@ -1,26 +1,26 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    double X, Y, Z;
-    int cnt = -1;
+  int X, Y, Z;
+  int cnt = -1;
 
-    cin >> X >> Y;
-    Z = floor(100*Y/X);
+  cin >> X >> Y;
+  Z = floor(100 * (double)Y / X);
 
-    int low = 1;
-    int high = 1000000000;
+  int low = 1, high = 1e9;
 
-    while (low <= high) {
-        int mid = (high + low)/2;
-        double rate = floor(100*(Y + mid)/(X + mid));
+  while (low <= high) {
+    int mid = (low + high) / 2;
+    int rate = floor(100 * (double)(Y + mid) / (X + mid));
 
-        if (rate > Z) {
-            high = mid - 1;
-            cnt = mid;
-        } else low = mid + 1;
-    }
-    cout << cnt;
+    if (rate > Z) {
+      high = mid - 1;
+      cnt = mid;
+    } else
+      low = mid + 1;
+  }
+  cout << cnt;
 }
