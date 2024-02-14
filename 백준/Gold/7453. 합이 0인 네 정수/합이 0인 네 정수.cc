182482c1140fe09dfe86a51c1adea8db4f1181f4
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-14 13:44:13 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 3456 ms, Memory: 166144 KB -BaekjoonHub

---
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<int> A, B, C, D, AB, CD;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  long long cnt = 0;
+
+  cin >> n;
+  A.resize(n);
+  B.resize(n);
+  C.resize(n);
+  D.resize(n);
+  for (int i = 0; i < n; i++) cin >> A[i] >> B[i] >> C[i] >> D[i];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      AB.push_back(A[i] + B[j]);
+      CD.push_back(C[i] + D[j]);
+    }
+  }
+  sort(AB.begin(), AB.end());
+  sort(CD.begin(), CD.end());
+  for (int num : AB) {
+    int t = -num;
+
+    if (!binary_search(CD.begin(), CD.end(), t)) continue;
+
+    int lower = lower_bound(CD.begin(), CD.end(), t) - CD.begin();
+    int upper = upper_bound(CD.begin(), CD.end(), t) - CD.begin();
+
+    cnt += (upper - lower);
+  }
+  cout << cnt;
+}
