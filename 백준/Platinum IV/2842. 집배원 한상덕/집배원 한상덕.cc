39546aee04e893e4c43ada21c0934ffd44742672
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-14 17:04:47 +0900

    [Platinum IV] Title: 집배원 한상덕, Time: 224 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Platinum IV/2842. 집배원 한상덕/집배원 한상덕.cc
+++ 백준/Platinum IV/2842. 집배원 한상덕/집배원 한상덕.cc
@@ -19,7 +19,6 @@ vector<int> altitudes;
 bool is_inside(int x, int y) { return 0 <= x && x < N && 0 <= y && y < N; }
 
 int bfs(int low, int high) {
-//   cout << '\n';
   int visit_cnt = 0;
   int min_altitude = 1e6;
   int max_altitude = 0;
@@ -31,20 +30,17 @@ int bfs(int low, int high) {
     auto [x, y] = q.front();
 
     if (map[x][y] == 'K') visit_cnt++;
-    // cout << "cur x: " << x << " cur y: " << y << '\n';
     min_altitude = min(min_altitude, contours[x][y]);
     max_altitude = max(max_altitude, contours[x][y]);
-    if (visit_cnt == dst_cnt) {
-    //   cout << "max_altitude: " << max_altitude << '\n';
-    //   cout << "min_altitude: " << min_altitude << '\n';
-      return max_altitude - min_altitude;
-    }
+    if (visit_cnt == dst_cnt) return max_altitude - min_altitude;
     q.pop();
     for (int i = 0; i < 8; i++) {
       int nx = x + dx[i];
       int ny = y + dy[i];
 
-      if (is_inside(nx, ny) && !visited[nx][ny] && (altitudes[low] <= contours[nx][ny]) && (contours[nx][ny] <= altitudes[high])) {
+      if (is_inside(nx, ny) && !visited[nx][ny] &&
+          (altitudes[low] <= contours[nx][ny]) &&
+          (contours[nx][ny] <= altitudes[high])) {
         visited[nx][ny] = true;
         q.push({nx, ny});
       }
@@ -58,12 +54,12 @@ int main() {
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
       cin >> map[i][j];
-      if (map[i][j] == 'P') src = {i, j};
-      else if (map[i][j] == 'K') dst_cnt++;
+      if (map[i][j] == 'P')
+        src = {i, j};
+      else if (map[i][j] == 'K')
+        dst_cnt++;
     }
   }
-//   cout << '\n';
-//   cout << "dst_cnt: " << dst_cnt << '\n';
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
       cin >> contours[i][j];
@@ -72,22 +68,21 @@ int main() {
   }
   sort(altitudes.begin(), altitudes.end());
   altitudes.erase(unique(altitudes.begin(), altitudes.end()), altitudes.end());
-//   cout << "altitudes: ";
-//   for (int altitude : altitudes) cout << altitude << ' ';
-//   cout << '\n';
-  
+
   int low = 0, high = 0;
-  
+
   while (low <= high && high < altitudes.size()) {
     fill(&visited[0][0], &visited[N - 1][N], false);
 
     int degree_of_fatigue = bfs(low, high);
 
-    // cout << "low: " << low << ", high: " << high << ", degree of fatigue: " << degree_of_fatigue << '\n';
-    if (degree_of_fatigue == -1) high++;
+    if (degree_of_fatigue == -1)
+      high++;
     else {
-      if (low == high) high++;
-      else low++;
+      if (low == high)
+        high++;
+      else
+        low++;
       min_degree_of_fatigue = min(min_degree_of_fatigue, degree_of_fatigue);
     }
   }
