43172d6fa7a44b561669e07244731ea9994e195f
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-15 10:28:07 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze I/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze I/11653. 소인수분해/소인수분해.cc
@@ -4,15 +4,14 @@ using namespace std;
 
 int main() {
   int N;
-  int divisor = 2;
 
   cin >> N;
   if (N == 1) return 0;
-  while (N != 1) {
-    if (N % divisor == 0) {
-      cout << divisor << '\n';
-      N /= divisor;
-    } else
-      divisor++;
+  for (int i = 2; i * i <= N; i++) {
+    while (N % i == 0) {
+      cout << i << '\n';
+      N /= i;
+    }
   }
+  if (N > 1) cout << N;
 }
