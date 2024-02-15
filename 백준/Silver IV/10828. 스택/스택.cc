0791ab37d143027c44d1012dc6bfccf72664ebfc
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-15 14:10:44 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver IV/10828. 스택/스택.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int N;
+  string command;
+  stack<int> s;
+
+  cin >> N;
+  while (N--) {
+    cin >> command;
+    if (command == "push") {
+      int X;
+
+      cin >> X;
+      s.push(X);
+    } else if (command == "pop") {
+      if (s.empty())
+        cout << -1 << '\n';
+      else {
+        cout << s.top() << '\n';
+        s.pop();
+      }
+    } else if (command == "size")
+      cout << s.size() << '\n';
+    else if (command == "empty") {
+      if (s.empty())
+        cout << 1 << '\n';
+      else
+        cout << 0 << '\n';
+    } else if (command == "top") {
+      if (s.empty())
+        cout << -1 << '\n';
+      else
+        cout << s.top() << '\n';
+    }
+  }
+}
