e40ef0e1016b4cd4451306fb96add1ef3545c83d
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-19 15:59:58 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int pebbles[50];
+
+int main() {
+  int M, K, total = 0;
+  double total_prob = 0;
+
+  cin >> M;
+  for (int i = 0; i < M; i++) {
+    cin >> pebbles[i];
+    total += pebbles[i];
+  }
+  cin >> K;
+  for (int i = 0; i < M; i++) {
+    long long numerator = pebbles[i];
+    long long denominator = total;
+    double ith_prob = 1;
+    // cout << "numerator: " << numerator << " denominator: " << denominator << '\n';
+    for (int j = 0; j < K; j++) {
+      ith_prob *= ((double)numerator / denominator);
+      numerator--;
+      denominator--;
+    }
+    total_prob += ith_prob;
+  }
+  cout << fixed;
+  cout.precision(10);
+  cout << total_prob;
+}
