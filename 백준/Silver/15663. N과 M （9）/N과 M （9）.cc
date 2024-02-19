8bcf9f4fd86c55e9873d4d251831f3a28392c379
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-19 14:28:47 +0900

    [Silver II] Title: N과 M (9), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int arr[8];
+int num[8];
+bool visited[8];
+
+void dfs(int depth) {
+  if (depth == M) {
+    for (int i = 0; i < M; i++) cout << num[i] << ' ';
+    cout << '\n';
+    return;
+  }
+
+  int prev = 0;
+
+  for (int i = 0; i < N; i++) {
+    if (!visited[i] && prev != arr[i]) {
+      visited[i] = true;
+      num[depth] = arr[i];
+      prev = arr[i];
+      dfs(depth + 1);
+      visited[i] = false;
+    }
+  }
+}
+
+int main() {
+  cin >> N >> M;
+  fill(visited, visited + N, false);
+  for (int i = 0; i < N; i++) cin >> arr[i];
+  sort(arr, arr + N);
+  dfs(0);
+}
