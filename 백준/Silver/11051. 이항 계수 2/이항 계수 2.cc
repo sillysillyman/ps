b4f10510b951d44f33b5ec13c68724cdd0497520
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-19 09:40:15 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -2,20 +2,20 @@
 
 using namespace std;
 
-typedef long long ll;
-
-const int MOD = 10007;
+const int MOD = 10'007;
 int dp[1001][1001];
 
 int main() {
-    int N, K;
+  int N, K;
 
-    cin >> N >> K;
-    for (int i = 1; i <= N; i++) {
-        for (int j = 0; j <= min(i, K); j++) {
-            if (j == 0 || j == i) dp[i][j] = 1;
-            else dp[i][j] = (dp[i - 1][j]%MOD + dp[i - 1][j - 1]%MOD)%MOD;
-        }
+  cin >> N >> K;
+  for (int i = 1; i <= N; i++) {
+    for (int j = 0; j <= min(i, K); j++) {
+      if (j == 0 || j == i)
+        dp[i][j] = 1;
+      else
+        dp[i][j] = (dp[i - 1][j] % MOD + dp[i - 1][j - 1] % MOD) % MOD;
     }
-    cout << dp[N][K];
+  }
+  cout << dp[N][K];
 }
