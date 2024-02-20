ef39bbc66e37a42b535ad854bd5ddd08840be7f4
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-20 09:29:02 +0900

    [Gold IV] Title: 네트워크 연결, Time: 32 ms, Memory: 4460 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Info {
+  int cost, u, v;
+
+  bool operator<(const Info other) const { return cost < other.cost; }
+};
+
+int min_cost = 0;
+int parents[1001];
+vector<Info> infos;
+
+int find(int x) {
+  if (parents[x] == x) return x;
+  return parents[x] = find(parents[x]);
+}
+
+void merge(int x, int y) {
+  x = find(x);
+  y = find(y);
+  if (x == y) return;
+  if (x < y) parents[y] = x;
+  else parents[x] = y;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int N, M;
+
+  cin >> N >> M;
+  for (int i = 1; i <= N; i++) parents[i] = i;
+  for (int i = 0; i < M; i++) {
+    Info info;
+
+    cin >> info.u >> info.v >> info.cost;
+    infos.push_back(info);
+  }
+//   cout << '\n';
+//   for (Info info : infos) {
+//     cout << info.cost << ' ' << info.u << ' ' << info.v << '\n';
+//   }
+//   cout << '\n';
+  sort(infos.begin(), infos.end());
+//   for (Info info : infos) {
+//     cout << info.cost << ' ' << info.u << ' ' << info.v << '\n';
+//   }
+//   cout << '\n';
+  for (Info info : infos) {
+    if (find(info.u) == find(info.v)) continue;
+    // cout << "connect " << info.u << " and " << info.v << '\n';
+    merge(info.u, info.v);
+    min_cost += info.cost;
+  }
+  cout << min_cost;
+}
