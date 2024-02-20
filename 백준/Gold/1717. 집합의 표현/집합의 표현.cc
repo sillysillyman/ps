b95a33e7156a32c095d7ee50f09f50ff1cba345f
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-20 09:58:52 +0900

    [Gold V] Title: 집합의 표현, Time: 32 ms, Memory: 6324 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -2,19 +2,21 @@
 
 using namespace std;
 
-int parents[1000001];
+int parents[1'000'001];
 
 int find(int x) {
   if (parents[x] == x) return x;
-  else return parents[x] = find(parents[x]);
+  return parents[x] = find(parents[x]);
 }
 
 void merge(int x, int y) {
   x = find(x);
   y = find(y);
   if (x == y) return;
-  if (x < y) parents[y] = x;
-  else parents[x] = y;
+  if (x < y)
+    parents[y] = x;
+  else
+    parents[x] = y;
 }
 
 int main() {
@@ -26,18 +28,16 @@ int main() {
   cin >> n >> m;
   for (int i = 0; i <= n; i++) parents[i] = i;
   while (m--) {
-    // for (int i = 0; i <= n; i++) cout << i << " ";
-    // cout << '\n';
-    // for (int i = 0; i <= n; i++) cout << parents[i] << ' ';
-    // cout << '\n';
-    // cout << '\n';
     int cmd, a, b;
 
     cin >> cmd >> a >> b;
-    if (cmd == 0) merge(a, b);
+    if (cmd == 0)
+      merge(a, b);
     else if (cmd == 1) {
-      if (find(a) == find(b)) cout << "YES\n";
-      else cout << "NO\n";
+      if (find(a) == find(b))
+        cout << "YES\n";
+      else
+        cout << "NO\n";
     }
   }
 }
