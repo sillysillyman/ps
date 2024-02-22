ac65c4dee14991433c126ef8691468e9e6079515
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-22 10:53:51 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -2,17 +2,19 @@
 
 using namespace std;
 
-int lcs[1001][1001] = {};
+int dp[1001][1001];
 
 int main() {
-    string s, t;
+  string s, t;
 
-    cin >> s >> t;
-    for (int i = 0; i < s.length(); i++) {
-        for (int j = 0; j < t.length(); j++) {
-            if (s[i] == t[j]) lcs[i + 1][j + 1] = lcs[i][j] + 1;
-            else lcs[i + 1][j + 1] = max(lcs[i][j + 1], lcs[i + 1][j]);
-        }
+  cin >> s >> t;
+  for (int i = 0; i < s.length(); i++) {
+    for (int j = 0; j < t.length(); j++) {
+      if (s[i] == t[j])
+        dp[i + 1][j + 1] = dp[i][j] + 1;
+      else
+        dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
     }
-    cout << lcs[s.length()][t.length()];
+  }
+  cout << dp[s.length()][t.length()];
 }
