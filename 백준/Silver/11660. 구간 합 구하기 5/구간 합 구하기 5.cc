9c0c4d57fc1631bd586269f1703b297298337995
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-22 10:44:54 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 128 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -2,33 +2,24 @@
 
 using namespace std;
 
-int matrix[1024][1024];
-int accumulated[1024][1024];
+int dp[1025][1025];
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int N, M;
-    int x1, y1, x2, y2;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-    cin >> N >> M;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> matrix[i][j];
-            if (j == 0) accumulated[i][j] = matrix[i][j];
-            else accumulated[i][j] = accumulated[i][j - 1] + matrix[i][j];
-        }
+  int N, M;
+
+  cin >> N >> M;
+  for (int i = 1; i <= N; i++) {
+    for (int j = 1; j <= N; j++) {
+      cin >> dp[i][j];
+      dp[i][j] += dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
     }
-    for (int i = 0; i < M; i++) {
-        int sum = 0;
-        
-        cin >> x1 >> y1 >> x2 >> y2;
-        x1--; y1--; x2--; y2--;
-        for (int j = x1; j <= x2; j++) {
-            if (y1 == 0) sum += accumulated[j][y2];    
-            else sum += accumulated[j][y2] - accumulated[j][y1 - 1];
-        }
-        cout << sum << '\n';
-    }
+  }
+  for (int x1, y1, x2, y2, i = 0; i < M; i++) {
+    cin >> x1 >> y1 >> x2 >> y2;
+    cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]
+         << '\n';
+  }
 }
