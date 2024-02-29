586b6e380e9d7cb101fda1e2ed18b90cba1fb162
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-02-29 12:54:04 +0900

    [Bronze III] Title: 배수 찾기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4504. 배수 찾기/배수 찾기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+
+  cin >> n;
+  while (true) {
+    int num;
+
+    cin >> num;
+    if (num == 0) break;
+    cout << num << " is ";
+    if (num % n) cout << "NOT ";
+    cout << "a multiple of " << n << ".\n";
+  }
+}
