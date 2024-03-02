98c0912aacfa1484acb724ce7a90258ed8383238
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-03 05:02:15 +0900

    [Gold I] Title: 소수의 곱, Time: 160 ms, Memory: 100340 KB -BaekjoonHub

---
+++ 백준/Gold/2014. 소수의 곱/소수의 곱.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int K, N;
+  vector<int> primes;
+  priority_queue<int, vector<int>, greater<>> pq;
+
+  cin >> K >> N;
+  for (int prime, i = 0; i < K; ++i) {
+    cin >> prime;
+    primes.push_back(prime);
+  }
+  pq.push(1);
+  while (N--) {
+    int num = pq.top();
+
+    for (int prime : primes) {
+      if ((long long)num * prime <= ~(1 << 31)) pq.push(num * prime);
+    }
+    pq.pop();
+    while (pq.top() == num) pq.pop();
+  }
+  cout << pq.top();
+}
