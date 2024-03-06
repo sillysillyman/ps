c32351ff961dc8f6c1efb8d606b31c271bd8a826
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-06 23:17:01 +0900

    [Gold IV] Title: N-Queen, Time: 4144 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -2,33 +2,28 @@
 
 using namespace std;
 
-int board[15];
 int N;
 int cnt = 0;
+int board[15];
 
 bool is_promising(int level) {
-    for (int i = 0; i < level; ++i) {
-        if ((board[i] == board[level]) || (abs(board[i] - board[level]) == abs(level - i))) {
-            return false;
-        }
-    }
-    return true;
+  for (int i = 0; i < level; i++) {
+    if (board[i] == board[level] || abs(board[i] - board[level]) == level - i)
+      return false;
+  }
+  return true;
 }
 
 void n_queen(int n) {
-    if (n == N) {
-        cnt++;
-    } else {
-        for (int i = 0; i < N; ++i) {
-            board[n] = i;
-            if (is_promising(n)) n_queen(n + 1);
-        }
-    }
+  if (n == N) cnt++;
+  for (int i = 0; i < N; i++) {
+    board[n] = i;
+    if (is_promising(n)) n_queen(n + 1);
+  }
 }
 
 int main() {
-    cin >> N;
-    n_queen(0);
-    cout << cnt;
-    return 0;
+  cin >> N;
+  n_queen(0);
+  cout << cnt;
 }
