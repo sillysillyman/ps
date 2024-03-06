72f4d334711ab892a58e40bb2bb2b42a6b5dc612
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-07 04:26:15 +0900

    [Gold IV] Title: 좋다, Time: 16 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int arr[2000];
+
+int main() {
+  int N;
+  int cnt = 0;
+
+  cin >> N;
+  for (int i = 0; i < N; i++) cin >> arr[i];
+  sort(arr, arr + N);
+  for (int i = 0; i < N; i++) {
+    int left = 0;
+    int right = N - 1;
+
+    while (left < right) {
+      if (left == i) {
+        left++;
+        continue;
+      }
+      if (right == i) {
+        right--;
+        continue;
+      }
+      if (arr[i] == arr[left] + arr[right]) {
+        cnt++;
+        break;
+      }
+      if (arr[i] > arr[left] + arr[right])
+        left++;
+      else if (arr[i] < arr[left] + arr[right])
+        right--;
+    }
+  }
+  cout << cnt;
+}
