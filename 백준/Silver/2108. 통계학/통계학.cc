a3610d1226a504a2e5f07af394122610e42d4ac2
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-06 17:29:47 +0900

    [Silver III] Title: 통계학, Time: 48 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -1,87 +1,44 @@
-#include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
-const int MAX = 8001;
-int nums[MAX] = {};
-
-int get_avg(int *nums, int n) {
-    int avg;
-    int sum = 0;
-
-    for (int i = 0; i < MAX; i++) {
-        sum += (i - MAX/2)*nums[i];
-    }
-    avg = round(float(sum)/n);
-    avg = !avg ? abs(avg) : avg;
-    return avg;
-}
-
-int get_median(int *nums, int n) {
-    int cnt = 0;
-
-    for (int i = 0; i < MAX; i++) {
-        if (nums[i]) {
-            for (int j = 0; j < nums[i]; j++) {
-                if (cnt == n/2) return i - 4000;
-                cnt++;
-            }
-        }
-    }
-}
-
-int get_mode(int *nums, int n) {
-    int mode;
-    int mode_cnt = 0;
-    bool second_mode = false;
-
-    for (int i = 0; i < MAX; i++) {
-        if (nums[i] > mode_cnt) mode_cnt = nums[i];
-    }
-    for (int i = 0; i < MAX; i++) {
-        if (nums[i] == mode_cnt) {
-            if (second_mode) {
-                mode = i - 4000;
-                break;
-            }
-            mode = i - 4000;
-            second_mode = true;
-        }
-    }
-    return mode;
-}
-
-int get_range(int *nums, int n) {
-    int min, max;
-    
-    for (int i = 0; i < MAX; i++) {
-        if (nums[i] > 0) {
-            min = i - 4000;
-            break;
-        }
-    }
-    for (int i = MAX; i >= 0; i--) {
-        if (nums[i] > 0) {
-            max = i - 4000;
-            break;
-        }
-    }
-    return max - min;
-}
+int arr[8001];
 
 int main() {
-    int N;
-    int num;
-    
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> num;
-        nums[num + MAX/2]++;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int N;
+  int sum = 0, avg, median, mode, range;
+
+  cin >> N;
+  for (int num, min_val, max_val, i = 0; i < N; i++) {
+    cin >> num;
+    sum += num;
+    arr[num + 4000]++;
+    if (i == 0) {
+      min_val = num;
+      max_val = num;
     }
-    cout << get_avg(nums, N) << endl;
-    cout << get_median(nums, N) << endl;
-    cout << get_mode(nums, N) << endl;
-    cout << get_range(nums, N) << endl;
+    min_val = min(min_val, num);
+    max_val = max(max_val, num);
+    range = max_val - min_val;
+  }
+  avg = round(float(sum) / N);
+  for (int cnt = 0, i = 0; i < 8001; i++) {
+    if (arr[i] && cnt <= N / 2 + 1 && N / 2 + 1 <= cnt + arr[i]) {
+      median = i - 4000;
+      break;
+    }
+    cnt += arr[i];
+  }
+  mode = max_element(arr, arr + 8001) - arr;
+  if (int mode_candidate = max_element(arr + mode + 1, arr + 8001) - arr;
+      arr[mode] == *(mode_candidate + arr))
+    mode = mode_candidate - 4000;
+  else
+    mode -= 4000;
+  cout << avg << '\n' << median << '\n' << mode << '\n' << range;
 }
