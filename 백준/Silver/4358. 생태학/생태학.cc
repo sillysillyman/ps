906008ab42c8e01928ed6e820b55b07087ae6e7a
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-08 15:02:01 +0900

    [Silver II] Title: 생태학, Time: 168 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.cc
+++ 백준/Silver/4358. 생태학/생태학.cc
@@ -6,15 +6,15 @@ using namespace std;
 map<string, int> trees;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-    int size = 0;
-    string tree;
+  int size = 0;
+  string tree;
 
-    while (getline(cin, tree) && ++size) trees[tree]++;
-
-    cout.precision(4);
-    cout << fixed;
-    for (auto p : trees) cout << p.first << " " << (double)p.second/size*100 << '\n';
+  while (getline(cin, tree) && ++size) trees[tree]++;
+  cout.precision(4);
+  cout << fixed;
+  for (auto p : trees)
+    cout << p.first << " " << (double)p.second / size * 100 << '\n';
 }
