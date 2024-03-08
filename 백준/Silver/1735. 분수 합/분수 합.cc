e9523cf456f2869fa59138afb8fad0ca64efc4f6
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-08 15:04:20 +0900

    [Silver III] Title: 분수 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.cc
+++ 백준/Silver/1735. 분수 합/분수 합.cc
@@ -2,36 +2,13 @@
 
 using namespace std;
 
-int gcd(int a, int b) {
-    return b ? gcd(b, a%b) : a;
-}
-
-long long lcm(int a, int b) {
-    return a*b/gcd(a, b);
-}
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
 
 int main() {
-    int A, B, C, D;
+  int a, b, c, d, e, f;
 
-    cin >> A >> B >> C >> D;
-    if (gcd(A, B) != 1) {
-        int gcd_AB = gcd(A, B);
-
-        A /= gcd_AB;
-        B /= gcd_AB;
-    }
-    if (gcd(C, D) != 1) {
-        int gcd_CD = gcd(C, D);
-        
-        C /= gcd_CD;
-        D /= gcd_CD;
-    }
-
-    long long denominator = lcm(B, D);
-    long long numerator = A*denominator/B + C*denominator/D;
-    int gcd_fraction = gcd(numerator, denominator);
-
-    denominator /= gcd_fraction;
-    numerator /= gcd_fraction;
-    cout << numerator << ' ' << denominator;
+  cin >> a >> b >> c >> d;
+  e = a * d + b * c;
+  f = b * d;
+  cout << e / gcd(e, f) << ' ' << f / gcd(e, f);
 }
