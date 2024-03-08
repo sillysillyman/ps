c7b077a63ecc75a0d030296620f3fad2922975f5
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-08 17:24:38 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 14020 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,14 +1,9 @@
-import java.util.Scanner;
-
 public class Main {
 
-  public static void main(String[] args) throws Exception {
-    Scanner sc = new Scanner(System.in);
-
+  public static void main(String[] args) {
     System.out.println("\\    /\\");
     System.out.println(" )  ( ')");
     System.out.println("(  /  )");
     System.out.print(" \\(__)|");
-    sc.close();
   }
 }
