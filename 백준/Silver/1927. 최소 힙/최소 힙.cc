b8856a00538f79b8d05b0841c088f20adfccbf35
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-08 15:01:46 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,26 +1,29 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-priority_queue<int, vector<int>, greater<int>> heap;
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-    int N;
+  int N;
+  priority_queue<int, vector<int>, greater<int>> min_heap;
+
+  cin >> N;
+  while (N--) {
     int x;
 
-    cin >> N;
-    while (N--) {
-        cin >> x;
-        if (x == 0) {
-            if (heap.empty()) cout << 0 << '\n';
-            else {
-                cout << heap.top() << '\n';
-                heap.pop();
-            }
-        } else heap.push(x);
-    }
+    cin >> x;
+    if (x == 0) {
+      if (min_heap.empty())
+        cout << 0 << '\n';
+      else {
+        cout << min_heap.top() << '\n';
+        min_heap.pop();
+      }
+    } else
+      min_heap.push(x);
+  }
 }
