318d8ab49537709100965bb8206934684c10071b
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-08 16:30:32 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int A = sc.nextInt();
     int B = sc.nextInt();
 
-    System.out.println(A - B);
+    System.out.print(A - B);
     sc.close();
   }
 }
