e3f4140529955e6b4168648dbc8dc44c35ddece8
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-15 04:30:40 +0900

    [Gold III] Title: 선분 교차 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17386. 선분 교차 1/선분 교차 1.cc
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.cc
@@ -1,29 +1,34 @@
 #include <iostream>
 
 using namespace std;
+using ll = long long;
 
-typedef long long ll;
-typedef pair<int, int> point;
+struct Point {
+  ll x, y;
+};
 
-int ccw(point p1, point p2, point p3) {
-    point v = {p2.first - p1.first, p2.second - p1.second};
-    point w = {p3.first - p2.first, p3.second - p2.second};
+struct Line {
+  Point p, q;
+};
 
-    ll cross_product = (ll)v.first*w.second - (ll)w.first*v.second;
-
-    if (cross_product < 0) return -1;
-    else if (cross_product > 0) return 1;
-    else return 0;
+int ccw(Point p, Point q, Point r) {
+  if (ll cross_product = (q.x - p.x) * (r.y - p.y) - (r.x - p.x) * (q.y - p.y);
+      cross_product > 0)
+    return 1;
+  else if (cross_product < 0)
+    return -1;
+  else
+    return 0;
 }
 
 int main() {
-    int x1, y1, x2, y2, x3, y3, x4, y4;
+  Line L1, L2;
 
-    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
-
-    bool cond1 = ccw({x1, y1}, {x2, y2}, {x3, y3})*ccw({x1, y1}, {x2, y2}, {x4, y4}) < 0;
-    bool cond2 = ccw({x3, y3}, {x4, y4}, {x1, y1})*ccw({x3, y3}, {x4, y4}, {x2, y2}) < 0;
-    
-    if (cond1 && cond2) cout << 1;
-    else cout << 0;
+  cin >> L1.p.x >> L1.p.y >> L1.q.x >> L1.q.y;
+  cin >> L2.p.x >> L2.p.y >> L2.q.x >> L2.q.y;
+  if (ccw(L1.p, L1.q, L2.p) * ccw(L1.p, L1.q, L2.q) < 0 &&
+      ccw(L2.p, L2.q, L1.p) * ccw(L2.p, L2.q, L1.q) < 0)
+    cout << 1;
+  else
+    cout << 0;
 }
