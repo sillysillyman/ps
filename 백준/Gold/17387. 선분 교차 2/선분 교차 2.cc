eb58c4f2e4b87382da10c23cbb2782124d0b3c8d
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-15 19:44:19 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -1,47 +1,37 @@
 #include <iostream>
 
 using namespace std;
+using ll = long long;
 
-typedef long long ll;
-typedef pair<int, int> point;
-typedef pair<point, point> line;
+struct Point {
+  ll x, y;
+};
 
-int ccw(point p1, point p2, point p3) {
-    point v = {p2.first - p1.first, p2.second - p1.second};
-    point w = {p3.first - p2.first, p3.second - p2.second};
-    ll cross_product = (ll)v.first*w.second - (ll)w.first*v.second;
+struct Line {
+  Point p, q;
+};
 
-    if (cross_product < 0) return -1;
-    else if (cross_product > 0) return 1;
-    else return 0;
+int ccw(Point p, Point q, Point r) {
+  ll cross_product = (q.x - p.x) * (r.y - p.y) - (r.x - p.x) * (q.y - p.y);
+
+  if (cross_product) return cross_product / abs(cross_product);
+  return 0;
 }
 
-bool is_on_the_line(point p, line l) {
-    if (ccw(l.first, l.second, p) != 0) return false;
-    
-    int x = p.first, y = p.second;
-    int x1 = l.first.first, y1 = l.first.second;
-    int x2 = l.second.first, y2 = l.second.second;
-
-    if (x1 <= x && x <= x2 && y1 <= y && y <= y2) return true;
-    else if (x2 <= x && x <= x1 && y1 <= y && y <= y2) return true;
-    else if (x1 <= x && x <= x2 && y2 <= y && y <= y1) return true;
-    else if (x2 <= x && x <= x1 && y2 <= y && y <= y1) return true;
-    else return false;
+bool is_point_on_line(Point p, Line l) {
+  if (ccw(p, l.p, l.q)) return false;
+  if (l.p.x > l.q.x || (l.p.x == l.q.x && l.p.y > l.q.y)) swap(l.p, l.q);
+  return l.p.x == l.q.x ? l.p.y <= p.y && p.y <= l.q.y
+                        : l.p.x <= p.x && p.x <= l.q.x;
 }
 
 int main() {
-    line L1, L2;
-    point P1, P2, P3, P4;
+  Line L1, L2;
 
-    cin >> P1.first >> P1.second >> P2.first >> P2.second;
-    cin >> P3.first >> P3.second >> P4.first >> P4.second;
-    L1 = {P1, P2};
-    L2 = {P3, P4};
-
-    if ((ccw(P1, P2, P3)*ccw(P1, P2, P4) < 0) && (ccw(P3, P4, P1)*ccw(P3, P4, P2) < 0)) cout << 1;
-    else {
-        if (is_on_the_line(P1, L2) || is_on_the_line(P2, L2) || is_on_the_line(P3, L1) || is_on_the_line(P4, L1)) cout << 1;
-        else cout << 0;
-    }
+  cin >> L1.p.x >> L1.p.y >> L1.q.x >> L1.q.y;
+  cin >> L2.p.x >> L2.p.y >> L2.q.x >> L2.q.y;
+  cout << ((ccw(L1.p, L1.q, L2.p) * ccw(L1.p, L1.q, L2.q) < 0 &&
+            ccw(L2.p, L2.q, L1.p) * ccw(L2.p, L2.q, L1.q) < 0) ||
+           (is_point_on_line(L1.p, L2) || is_point_on_line(L1.q, L2) ||
+            is_point_on_line(L2.p, L1) || is_point_on_line(L2.q, L1)));
 }
