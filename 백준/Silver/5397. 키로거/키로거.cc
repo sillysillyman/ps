3d7ad5af94e1ce8d220908a32ac931b95f339084
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-23 20:14:06 +0900

    [Silver II] Title: 키로거, Time: 264 ms, Memory: 19384 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int T;
+
+  cin >> T;
+  while (T--) {
+    string keylogger;
+    list<char> pwd;
+    auto cursor = pwd.begin();
+
+    cin >> keylogger;
+    for (char log : keylogger) {
+      if (log == '<') {
+        if (cursor != pwd.begin()) cursor--;
+      } else if (log == '>') {
+        if (cursor != pwd.end()) cursor++;
+      } else if (log == '-') {
+        if (!pwd.empty() && cursor != pwd.begin()) cursor = pwd.erase(--cursor);
+      } else {
+        pwd.insert(cursor, log);
+      }
+    }
+    for (auto letter : pwd) cout << letter;
+    cout << '\n';
+  }
+}
