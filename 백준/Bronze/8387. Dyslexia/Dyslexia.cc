7b21d12e33330fc4b0f2fc8656ac4c2092c23f49
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-30 03:20:23 +0900

    [Bronze III] Title: Dyslexia, Time: 0 ms, Memory: 2524 KB -BaekjoonHub

---
+++ 백준/Bronze/8387. Dyslexia/Dyslexia.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n, cnt = 0;
+  string original, rewritten;
+
+  cin >> n;
+  cin >> original >> rewritten;
+  for (int i = 0; i < n; i++) {
+    if (original[i] == rewritten[i]) cnt++;
+  }
+  cout << cnt;
+}
