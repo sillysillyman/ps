0943e6270de49e3991dc45a3624407e3b2570376
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-31 03:21:35 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.cc
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int sum = 0;
+  int dwarves[9];
+
+  for (int i = 0; i < 9; i++) {
+    cin >> dwarves[i];
+    sum += dwarves[i];
+  }
+
+  bool found = false;
+
+  for (int i = 0; i < 8; i++) {
+    for (int j = i + 1; j < 9; j++) {
+      if (dwarves[i] + dwarves[j] == sum - 100) {
+        dwarves[i] = 100;
+        dwarves[j] = 100;
+        found = true;
+        break;
+      }
+    }
+    if (found) break;
+  }
+  sort(dwarves, dwarves + 9);
+  for (int i = 0; i < 7; i++) cout << dwarves[i] << '\n';
+}
