72a701cb4cd33e28b702747c69332d7e26be4129
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-03-31 05:13:38 +0900

    [Bronze II] Title: KMP는 왜 KMP일까?, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.cc
+++ 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.cc
@@ -1,23 +1,15 @@
 #include <iostream>
-#include <sstream>
-#include <vector>
 
 using namespace std;
 
-vector<string> split(string str, char delimiter) {
-  string buffer;
-  istringstream iss(str);
-  vector<string> res;
-
-  while (getline(iss, buffer, delimiter)) res.push_back(buffer);
-  return res;
-}
-
 int main() {
-  string s;
-  vector<string> v;
-    
-  cin >> s;
-  v = split(s, '-');
-  for (auto& s : v) cout << s[0];
+  int len;
+  string str;
+
+  cin >> str;
+  len = str.length();
+  cout << str[0];
+  for (int i = 1; i < len; i++) {
+    if (str[i - 1] == '-') cout << str[i];
+  }
 }
