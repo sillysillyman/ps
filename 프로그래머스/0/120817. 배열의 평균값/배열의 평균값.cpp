4293cf8117019f8299a58cfe83ae0723d52d1f3f
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-04-25 11:21:07 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 3.72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+  return (double)accumulate(numbers.begin(), numbers.end(), 0) / numbers.size();
+}
