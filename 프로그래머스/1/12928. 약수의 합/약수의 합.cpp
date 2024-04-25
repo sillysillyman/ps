3589d503614db63f4301068284bade430f284c56
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-04-25 11:35:42 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+  int answer = 0;
+
+  for (int i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      if (i * i == n)
+        answer += i;
+      else
+        answer += i + n / i;
+    }
+  }
+  return answer;
+}
