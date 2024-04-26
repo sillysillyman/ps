1d450e82b912680522353fe0550fdc8a06702e85
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-04-26 11:07:12 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+string solution(string s) {
+  bool is_even = true;
+
+  for (int i = 0; i < s.length(); i++) {
+    if (s[i] == ' ') {
+      is_even = true;
+      continue;
+    }
+    if (is_even)
+      s[i] = toupper(s[i]);
+    else
+      s[i] = tolower(s[i]);
+    is_even ^= true;
+  }
+  return s;
+}
