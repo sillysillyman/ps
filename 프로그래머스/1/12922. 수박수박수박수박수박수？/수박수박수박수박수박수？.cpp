2a23e9151526833215eb72b3d3f21a3e1ad5fe70
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-04-26 10:16:20 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+string solution(int n) {
+  string answer = "";
+
+  for (int i = 0; i < n; i++) {
+    if (!(i % 2))
+      answer += "수";
+    else
+      answer += "박";
+  }
+  return answer;
+}
