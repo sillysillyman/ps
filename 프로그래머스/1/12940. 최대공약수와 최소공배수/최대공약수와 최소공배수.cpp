cec0486a193a5f499c988f2343366c66f7fe5738
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-04-26 10:52:38 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -0,0 +1,12 @@
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+  vector<int> answer;
+
+  answer.push_back(gcd(n, m));
+  answer.push_back(lcm(n, m));
+  return answer;
+}
