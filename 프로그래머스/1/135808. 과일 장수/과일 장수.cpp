e044fcbb99ab44cfd66f7387fb5f7e30f1c26f72
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-04-30 23:37:54 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+  int answer = 0;
+
+  sort(score.begin(), score.end(), greater());
+  for (int i = m - 1; i < score.size(); i += m) answer += score[i] * m;
+  return answer;
+}
