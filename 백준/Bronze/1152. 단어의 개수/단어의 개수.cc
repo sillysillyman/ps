620c7d6a9e40d48f1954e6c209fd3036f8219188
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-03 15:21:13 +0900

    [Bronze II] Title: 단어의 개수, Time: 44 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -1,28 +1,11 @@
 #include <iostream>
-#include <string>
-#include <sstream>
-#include <vector>
 
 using namespace std;
 
 int main() {
-    string line;
-    vector<string> words;
+  int cnt = 0;
+  string word;
 
-    getline(cin, line);
-
-    int npos;
-
-    npos = line.find_first_not_of(' ');
-    line.erase(0, npos);
-    npos = line.find_last_not_of(' ');
-    line.erase(npos + 1);
-
-    istringstream iss(line);
-    string buffer;
-
-    while (getline(iss, buffer, ' ')) {
-        words.push_back(buffer);
-    }
-    cout << words.size();
+  while (!(cin >> word).eof()) cnt++;
+  cout << cnt;
 }
