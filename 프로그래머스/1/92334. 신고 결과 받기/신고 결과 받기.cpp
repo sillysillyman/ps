f2a71f0536a79061dd31fafdc0601e8959e903d8
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-10 22:51:31 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.03 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -1,38 +1,9 @@
-#include <algorithm>
-#include <map>
-#include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-pair<string, string> split(string str, char delimeter) {
-  istringstream iss(str);
-  string buffer;
-  vector<string> res;
-
-  while (getline(iss, buffer, delimeter)) res.push_back(buffer);
-  return {res[0], res[1]};
-}
-
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
-  vector<int> answer;
-  map<string, int> reported_cnts;
-  map<string, vector<string>> report_logs;
-
-  sort(report.begin(), report.end());
-  report.erase(unique(report.begin(), report.end()), report.end());
-  for (auto& log : report) {
-    auto [from, to] = split(log, ' ');
-
-    report_logs[from].push_back(to);
-    reported_cnts[to]++;
-  }
-  answer.resize(id_list.size());
-  for (int i = 0; i < id_list.size(); i++) {
-    for (string& reported_id : report_logs[id_list[i]]) {
-      if (reported_cnts[reported_id] >= k) answer[i]++;
-    }
-  }
-  return answer;
+    vector<int> answer;
+    return answer;
 }
