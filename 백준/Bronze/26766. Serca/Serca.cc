b7a80e059c1f6a1708e8cb640798718fc691ac51
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-13 01:22:44 +0900

    [Bronze V] Title: Serca, Time: 120 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N;
+
+  cin >> N;
+  while (N--) {
+    cout << " @@@   @@@ \n"
+            "@   @ @   @\n"
+            "@    @    @\n"
+            "@         @\n"
+            " @       @ \n"
+            "  @     @  \n"
+            "   @   @   \n"
+            "    @ @    \n"
+            "     @     \n";
+  }
+}
