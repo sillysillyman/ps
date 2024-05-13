54cf4a49fce4ad5b6de497eabf89f0ee6c0a0dfe
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-13 17:52:09 +0900

    [Gold IV] Title: 오큰수, Time: 208 ms, Memory: 16104 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <stack>
+#include <vector>
 
 using namespace std;
 
@@ -11,15 +11,15 @@ int main() {
   cin.tie(nullptr);
 
   int N;
-  stack<int> stack;
+  vector<int> stack;
 
   cin >> N;
   for (int i = 0; i < N; i++) cin >> arr[i];
   fill(nge, nge + N, -1);
   for (int i = N - 1; i >= 0; i--) {
-    while (!stack.empty() && arr[i] >= stack.top()) stack.pop();
-    if (!stack.empty()) nge[i] = stack.top();
-    stack.push(arr[i]);
+    while (!stack.empty() && arr[i] >= stack.back()) stack.pop_back();
+    if (!stack.empty()) nge[i] = stack.back();
+    stack.push_back(arr[i]);
   }
   for (int i = 0; i < N; i++) cout << nge[i] << ' ';
 }
