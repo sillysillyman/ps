b1f504e53923aec84ae8d45562e9fc3593c8ac1f
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-14 22:06:13 +0900

    [Silver I] Title: 영재의 징검다리, Time: 64 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/24392. 영재의 징검다리/영재의 징검다리.cc
+++ 백준/Silver/24392. 영재의 징검다리/영재의 징검다리.cc
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <numeric>
 
 using namespace std;
 
 const int MOD = 1'000'000'007;
-long long dp[1'000][1'000];
+int dp[1'000][1'000];
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -20,11 +19,24 @@ int main() {
     for (int j = 0; j < M; j++) {
       if (!dp[i][j]) continue;
       dp[i][j] = 0;
-      if (j > 0) dp[i][j] += dp[i + 1][j - 1];
+      if (j > 0) {
+        dp[i][j] += dp[i + 1][j - 1];
+        dp[i][j] %= MOD;
+      }
       dp[i][j] += dp[i + 1][j];
-      if (j < M - 1) dp[i][j] += dp[i + 1][j + 1];
       dp[i][j] %= MOD;
+      if (j < M - 1) {
+        dp[i][j] += dp[i + 1][j + 1];
+        dp[i][j] %= MOD;
+      }
     }
   }
-  cout << (accumulate(&dp[0][0], &dp[0][M], 0LL)) % MOD;
+
+  int sum = 0;
+
+  for (int j = 0; j < M; j++) {
+    sum += dp[0][j];
+    sum %= MOD;
+  }
+  cout << sum;
 }
