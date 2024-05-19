3b32ea28e57e0168d24a13f6a48097c5dacf6546
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-20 03:39:44 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 12.62 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
 
 ### 성능 요약
 
-메모리: 79 MB, 시간: 10.36 ms
+메모리: 78 MB, 시간: 12.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 18일 01:32:38
+2024년 05월 20일 03:39:39
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -4,8 +4,8 @@ class Solution {
 
     public String solution(String s) {
         int[] nums = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
-        int min = Arrays.stream(nums).min().orElse(Integer.MIN_VALUE);
-        int max = Arrays.stream(nums).max().orElse(Integer.MAX_VALUE);
+        int min = Arrays.stream(nums).min().getAsInt();
+        int max = Arrays.stream(nums).max().getAsInt();
 
         return min + " " + max;
     }
