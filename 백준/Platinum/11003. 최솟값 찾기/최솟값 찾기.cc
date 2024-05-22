4ac563ba3358f6403232bf2462d0b9334c368928
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-22 10:39:12 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1764 ms, Memory: 100452 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -1,30 +1,25 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 
 using namespace std;
 
-int arr[5000000];
 priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
 
 int main() {
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(nullptr);
 
   int N, L;
 
   cin >> N >> L;
-  for (int i = 0; i < N; i++) cin >> arr[i];
-  for (int i = 0; i < N; i++) {
-    if (i < L) {
-      pq.push({arr[i], i});
-      cout << pq.top().first << ' ';
-    } else {
-      pq.push({arr[i], i});
+  for (int num, i = 0; i < N; i++) {
+    cin >> num;
+    if (i < L)
+      pq.push({num, i});
+    else {
+      pq.push({num, i});
       while (pq.top().second <= i - L) pq.pop();
-      cout << pq.top().first << ' ';
     }
+    cout << pq.top().first << ' ';
   }
 }
-// 12 3
-// 1 1 2 3 6 2 3 7 3 5 2 6
