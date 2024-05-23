1f0a2d10160576b0e7cd75478dcc058e84116a31
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-23 18:23:26 +0900

    [Bronze IV] Title: 홀짝홀짝, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.cc
+++ 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.cc
@@ -1,19 +1,22 @@
-#include <cstdio>
+#include <iostream>
 
 using namespace std;
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
   int N, odd_cnt = 0, even_cnt = 0;
 
-  scanf("%d\n", &N);
+  cin >> N;
   while (N--) {
     char digit;
 
-    scanf("%c", &digit);
+    cin >> digit;
     if ((digit - '0') & 1)
       ++odd_cnt;
     else
       ++even_cnt;
   }
-  printf("%d", odd_cnt == even_cnt ? -1 : (odd_cnt < even_cnt ? 0 : 1));
+  cout << (odd_cnt == even_cnt ? -1 : (odd_cnt < even_cnt ? 0 : 1));
 }
