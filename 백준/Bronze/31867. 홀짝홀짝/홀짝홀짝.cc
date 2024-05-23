84a81bd42bcfada2b5cde6583f62bf83ed8bf58c
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-23 18:30:11 +0900

    [Bronze IV] Title: 홀짝홀짝, Time: 4 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.cc
+++ 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.cc
@@ -6,16 +6,16 @@ int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int N, odd_cnt = 0, even_cnt = 0;
+  int N, odd_cnt = 0;
   string num;
 
   cin >> N;
   cin >> num;
   for (char& digit : num) {
-    if ((digit - '0') & 1)
-      ++odd_cnt;
-    else
-      ++even_cnt;
+    if ((digit - '0') & 1) ++odd_cnt;
   }
+
+  int even_cnt = num.length() - odd_cnt;
+
   cout << (odd_cnt == even_cnt ? -1 : (odd_cnt < even_cnt ? 0 : 1));
 }
