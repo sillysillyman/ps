e75ffa0169f00c18ba91d77d2067b8ee4cbd9e48
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-24 11:22:12 +0900

    [level 2] Title: 삼각 달팽이, Time: 163.01 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
@@ -2,41 +2,26 @@
 
 using namespace std;
 
-bool is_inside(int x, int y, int n) {
-  return 0 <= x && x < n && 0 <= y && y <= x;
-}
-
 vector<int> solution(int n) {
-  int val = 1;
+  int val = 1, x = -1, y = 0;
   vector<int> answer;
   vector<vector<int>> triangle(n, vector<int>(n, 0));
 
-  int x = 0, y = 0;
-
-  while (is_inside(x, y, n) && !triangle[x][y]) {
-    for (; x < n; x++) {
-      if (triangle[x][y]) break;
-      triangle[x][y] = val++;
-    }
-    --x;
-    ++y;
-    for (; y < n; y++) {
-      if (triangle[x][y]) break;
-      triangle[x][y] = val++;
-    }
-    --y;
-    for (int k = 1; k <= n; k++) {
-      if (triangle[x - k][y - k]) {
-        x -= k - 1;
-        y -= k - 1;
-        break;
+  for (int step = 0; step < n; step++) {
+    for (int i = step; i < n; i++) {
+      if (step % 3 == 0)
+        ++x;
+      else if (step % 3 == 1)
+        ++y;
+      else {
+        --x;
+        --y;
       }
-      triangle[x - k][y - k] = val++;
+      triangle[x][y] = val++;
     }
-    ++x;
   }
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < i + 1; j++) answer.push_back(triangle[i][j]);
+    for (int j = 0; j <= i; j++) answer.push_back(triangle[i][j]);
   }
   return answer;
 }
