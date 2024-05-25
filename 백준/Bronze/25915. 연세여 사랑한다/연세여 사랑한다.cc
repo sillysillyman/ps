60c60636335521b05e4f035a3e489a5796688651
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-25 21:31:17 +0900

    [Bronze III] Title: 연세여 사랑한다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.cc
+++ 백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.cc
@@ -1,14 +1,8 @@
 #include <cstdio>
 
 int main() {
-  char curr;
-  char pwd[12] = "ILOVEYONSEI";
-  int dist = 0;
+  char start;
 
-  scanf("%c", &curr);
-  for (int i = 0; i < 11; i++) {
-    dist += pwd[i] >= curr ? pwd[i] - curr : curr - pwd[i];
-    curr = pwd[i];
-  }
-  printf("%d", dist);
+  scanf("%c", &start);
+  printf("%d", ('I' > start ? 'I' - start : start - 'I') + 84);
 }
