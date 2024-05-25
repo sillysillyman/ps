e3f01a0c203d28874d4884575de294621808561d
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-25 21:28:27 +0900

    [Bronze III] Title: 연세여 사랑한다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.cc
+++ 백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.cc
@@ -1,16 +1,14 @@
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
 
 int main() {
   char curr;
+  char pwd[12] = "ILOVEYONSEI";
   int dist = 0;
-  string pwd = "ILOVEYONSEI";
 
-  cin >> curr;
-  for (char& next : pwd) {
-    dist += abs(next - curr);
-    curr = next;
+  scanf("%c", &curr);
+  for (int i = 0; i < 11; i++) {
+    dist += pwd[i] >= curr ? pwd[i] - curr : curr - pwd[i];
+    curr = pwd[i];
   }
-  cout << dist;
+  printf("%d", dist);
 }
