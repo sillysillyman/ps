3d9c9987e90a27eb5ff3a58fb0d23807aef54fa1
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-27 13:01:56 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.cpp
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.cpp
@@ -1,54 +1,32 @@
-#include <algorithm>
 #include <numeric>
 #include <vector>
 
 using namespace std;
 
 int solution(vector<int> arrayA, vector<int> arrayB) {
+  bool is_any_divisible;
   int answer = 0;
   int gcdA, gcdB;
 
-  sort(arrayA.begin(), arrayA.end());
-  sort(arrayB.begin(), arrayB.end());
-  arrayA.erase(unique(arrayA.begin(), arrayA.end()), arrayA.end());
-  arrayB.erase(unique(arrayB.begin(), arrayB.end()), arrayB.end());
   gcdA = arrayA[0];
   gcdB = arrayB[0];
   for (int num : arrayA) gcdA = gcd(gcdA, num);
   for (int num : arrayB) gcdB = gcd(gcdB, num);
-  for (int i = 1; i < gcdA; i++) {
-    if (gcdA % i) continue;
-
-    bool is_any_divisible = false;
-    int divisor = gcdA / i;
-
-    for (int num : arrayB) {
-      if (!(num % divisor)) {
-        is_any_divisible = true;
-        break;
-      }
-    }
-    if (!is_any_divisible) {
-      answer = divisor;
+  is_any_divisible = false;
+  for (int num : arrayB) {
+    if (!(num % gcdA)) {
+      is_any_divisible = true;
       break;
     }
   }
-  for (int i = 1; i < gcdB; i++) {
-    if (gcdB % i) continue;
-
-    bool is_any_divisible = false;
-    int divisor = gcdB / i;
-
-    for (int num : arrayA) {
-      if (!(num % divisor)) {
-        is_any_divisible = true;
-        break;
-      }
-    }
-    if (!is_any_divisible) {
-      answer = max(answer, divisor);
+  if (!is_any_divisible) answer = gcdA;
+  is_any_divisible = false;
+  for (int num : arrayA) {
+    if (!(num % gcdB)) {
+      is_any_divisible = true;
       break;
     }
   }
+  if (!is_any_divisible) answer = max(answer, gcdB);
   return answer;
 }
