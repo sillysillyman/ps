92ff6b5d85d18a0fd691726cab57265d1d8f4454
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-27 14:41:16 +0900

    [level 2] Title: 배달, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -0,0 +1,51 @@
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+const int INF = 250'000'000;
+vector<int> dists;
+vector<vector<pair<int, int>>> graph;
+
+void init(int size) {
+  dists.assign(size + 1, INF);
+  graph.assign(size + 1, vector<pair<int, int>>());
+}
+
+void dijkstra(int src) {
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+
+  dists[src] = 0;
+  pq.push({0, src});
+  while (!pq.empty()) {
+    auto [curr_dist, curr] = pq.top();
+
+    pq.pop();
+    if (curr_dist > dists[curr]) continue;
+    for (auto& [next, next_dist] : graph[curr]) {
+      if (dists[next] > dists[curr] + next_dist) {
+        dists[next] = dists[curr] + next_dist;
+        pq.push({dists[next], next});
+      }
+    }
+  }
+}
+
+int solution(int N, vector<vector<int>> road, int K) {
+  int answer = 0;
+
+  init(N);
+  for (auto& info : road) {
+    int from = info[0];
+    int to = info[1];
+    int dist = info[2];
+
+    graph[from].push_back({to, dist});
+    graph[to].push_back({from, dist});
+  }
+  dijkstra(1);
+  for (int i = 1; i <= N; i++) {
+    if (dists[i] <= K) ++answer;
+  }
+  return answer;
+}
