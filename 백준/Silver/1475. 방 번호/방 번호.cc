840659b2dc4221deb36f2d47932b08fea3fab80b
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-30 14:27:20 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -6,18 +6,21 @@ using namespace std;
 int digits[10];
 
 int main() {
-  string N;
+  int N;
 
   cin >> N;
-  for (char& digit : N) {
-    if (digit != '6' && digit != '9')
-      ++digits[digit - '0'];
+  while (N) {
+    int digit = N % 10;
+
+    if (digit != 6 && digit != 9)
+      ++digits[digit];
     else {
       if (digits[6] == digits[9])
-        ++digits[digit - '0'];
+        ++digits[digit];
       else
         ++digits[digits[6] < digits[9] ? 6 : 9];
     }
+    N /= 10;
   }
   cout << *max_element(digits, digits + 10);
 }
