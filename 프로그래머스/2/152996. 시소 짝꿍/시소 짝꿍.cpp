3f892b23b7bc9fe54124c711ea923c1bdeeb8312
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-05-30 17:42:11 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
@@ -11,9 +11,12 @@ ll solution(vector<int> weights) {
   for (int weight : weights) ++weight_map[weight];
   for (auto& [weight, cnt] : weight_map) {
     if (cnt > 1) answer += cnt * (cnt - 1) / 2;
-    if (!(weight % 2)) answer += cnt * weight_map[3 * weight / 2];
-    answer += cnt * weight_map[2 * weight];
-    if (!(weight % 3)) answer += cnt * weight_map[4 * weight / 3];
+    if (!(weight % 2) && weight_map.find(3 * weight / 2) != weight_map.end())
+      answer += cnt * weight_map[3 * weight / 2];
+    if (weight_map.find(2 * weight) != weight_map.end())
+      answer += cnt * weight_map[2 * weight];
+    if (!(weight % 3) && weight_map.find(4 * weight / 3) != weight_map.end())
+      answer += cnt * weight_map[4 * weight / 3];
   }
   return answer;
 }
