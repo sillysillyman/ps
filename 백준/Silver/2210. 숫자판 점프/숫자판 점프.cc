ef42442b13496e9d37446b3a62dead5963ff7bb7
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-06-14 21:47:23 +0900

    [Silver II] Title: 숫자판 점프, Time: 0 ms, Memory: 1440 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.cc
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.cc
@@ -1,18 +1,18 @@
-#include <iostream>
+#include <cstdio>
 #include <set>
 
 using namespace std;
 
 int dx[4] = {-1, 0, 0, 1};
 int dy[4] = {0, -1, 1, 0};
-char graph[5][5];
+int graph[5][5];
 set<int> nums;
 
 bool is_inside(int x, int y) { return 0 <= x && x < 5 && 0 <= y && y < 5; }
 
-void dfs(int x, int y, string num) {
-  if (num.length() == 6) {
-    nums.insert(stoi(num));
+void dfs(int x, int y, int num, int depth) {
+  if (depth == 6) {
+    nums.insert(num);
     return;
   }
   for (int i = 0; i < 4; i++) {
@@ -20,16 +20,16 @@ void dfs(int x, int y, string num) {
     int ny = y + dy[i];
 
     if (!is_inside(nx, ny)) continue;
-    dfs(nx, ny, num + graph[nx][ny]);
+    dfs(nx, ny, 10 * num + graph[nx][ny], depth + 1);
   }
 }
 
 int main() {
   for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 5; j++) cin >> graph[i][j];
+    for (int j = 0; j < 5; j++) scanf("%d", &graph[i][j]);
   }
   for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 5; j++) dfs(i, j, "");
+    for (int j = 0; j < 5; j++) dfs(i, j, graph[i][j], 1);
   }
-  cout << nums.size();
+  printf("%d", nums.size());
 }
