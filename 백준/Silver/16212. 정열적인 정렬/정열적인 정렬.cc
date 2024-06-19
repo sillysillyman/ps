0321cd24c12b364537cabcf5755dbcaaba8cd3fa
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-06-19 10:20:48 +0900

    [Silver V] Title: 정열적인 정렬, Time: 0 ms, Memory: 1120 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.cc
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <cstdio>
+
+using namespace std;
+
+int arr[1'000];
+
+int main() {
+  int N;
+
+  scanf("%d", &N);
+  for (int i = 0; i < N; i++) scanf("%d", &arr[i]);
+  sort(arr, arr + N);
+  for (int i = 0; i < N; i++) printf("%d ", arr[i]);
+}
