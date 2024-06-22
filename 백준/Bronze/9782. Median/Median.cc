a5bef47ea105fa2d946ccf634635f485bd690f00
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-06-23 02:43:39 +0900

    [Bronze IV] Title: Median, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/9782. Median/Median.cc
+++ 백준/Bronze/9782. Median/Median.cc
@@ -1,23 +1,16 @@
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-
   int t = 0;
 
-  cout << fixed;
-  cout.precision(1);
   while (true) {
     int n;
     float median;
 
-    cin >> n;
+    scanf("%d", &n);
     if (n == 0) break;
     for (int num, i = 0; i < n; i++) {
-      cin >> num;
+      scanf("%d", &num);
       if (n & 1 && i == n / 2)
         median = num;
       else if (i == n / 2 - 1)
@@ -25,6 +18,6 @@ int main() {
       else if (i == n / 2)
         median = (median + num) / 2;
     }
-    cout << "Case " << ++t << ": " << median << '\n';
+    printf("Case %d: %.1f\n", ++t, median);
   }
 }
