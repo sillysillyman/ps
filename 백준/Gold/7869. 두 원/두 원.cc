8c10f6463ae80c24f06bb6be46a1598310fa4974
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-07-04 09:41:07 +0900

    [Gold II] Title: 두 원, Time: 0 ms, Memory: 1252 KB -BaekjoonHub

---
+++ 백준/Gold/7869. 두 원/두 원.cc
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+  double x1, y1, r1, x2, y2, r2, d;
+
+  scanf("%lf %lf %lf %lf %lf %lf", &x1, &y1, &r1, &x2, &y2, &r2);
+  if (r2 > r1) {
+    swap(x1, x2);
+    swap(y1, y2);
+    swap(r1, r2);
+  }
+  d = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+  if (d >= r1 + r2)
+    printf("%.3lf", 0.0);
+  else if (d == 0 || d <= r1 - r2)
+    printf("%.3lf", M_PI * r2 * r2);
+  else {
+    double theta1 = acos((r1 * r1 - r2 * r2 + d * d) / (2 * r1 * d));
+    double theta2 = acos((r2 * r2 - r1 * r1 + d * d) / (2 * r2 * d));
+
+    printf("%.3lf", r1 * r1 * (theta1 - sin(theta1) * cos(theta1)) +
+                        r2 * r2 * (theta2 - sin(theta2) * cos(theta2)));
+  }
+}
