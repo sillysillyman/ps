5f6c6726daf9221dead6c0fd999a0de817005cb0
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-07-19 23:24:24 +0900

    [Silver V] Title: 복붙의 달인, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11008. 복붙의 달인/복붙의 달인.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int T;
+
+  cin >> T;
+  while (T--) {
+    int time = 0, slen, plen;
+    string str, pattern;
+
+    cin >> str >> pattern;
+    slen = str.length();
+    plen = pattern.length();
+    for (int i = 0; i < slen;) {
+      if (i <= slen - plen && str.substr(i, plen) == pattern)
+        i += plen;
+      else
+        ++i;
+      ++time;
+    }
+    cout << time << '\n';
+  }
+}
