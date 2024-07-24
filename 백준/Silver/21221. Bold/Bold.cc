4f00f560b07e66a5d008b0922cc4abae02ff578b
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-07-24 20:42:11 +0900

    [Silver V] Title: Bold, Time: 0 ms, Memory: 1136 KB -BaekjoonHub

--- 백준/Silver/21221. Bold/Bold.cc
+++ 백준/Silver/21221. Bold/Bold.cc
@@ -8,7 +8,7 @@ int main() {
 
   scanf("%d %d", &n, &m);
   for (int i = 0; i < n; i++) {
-    scanf("%s", &board[i]);
+    scanf("%s", board[i]);
     for (int j = 0; j < m; j++) bolded[i][j] = board[i][j];
   }
   for (int i = 0; i < n; i++) {
