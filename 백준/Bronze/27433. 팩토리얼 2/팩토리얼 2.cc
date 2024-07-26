da38985d78fe5409c6d31f47bb9abbb2b3436914
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-07-27 00:56:20 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 1172 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -1,15 +1,15 @@
-#include <iostream>
+#include <cstdio>
 #include <functional>
 
 using namespace std;
 
 int main() {
-    int N;
-    function<long long(int)> factorial = [&factorial](int n) -> long long {
-        if (n <= 1) return 1;
-        else return n*factorial(n - 1);
-    };
+  int N;
+  function<long long(int)> factorial = [&factorial](int n) -> long long {
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
+  };
 
-    cin >> N;
-    cout << factorial(N);
+  scanf("%d", &N);
+  printf("%lld", factorial(N));
 }
