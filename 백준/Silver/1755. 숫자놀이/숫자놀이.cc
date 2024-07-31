420fa5131108afbb8cf7973bfb1476ba7474da12
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-07-31 23:18:07 +0900

    [Silver IV] Title: 숫자놀이, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1755. 숫자놀이/숫자놀이.cc
+++ 백준/Silver/1755. 숫자놀이/숫자놀이.cc
@@ -4,18 +4,19 @@
 
 using namespace std;
 
-string digits[10] = {"z", "o", "tw", "th", "fo", "fi", "si", "se", "e", "n"};
-pair<string, int> nums[99];
+int digits[10] = {9, 4, 8, 7, 2, 1, 6, 5, 0, 3};
+pair<int, int> nums[99];
 
 int main() {
-  short M, N;
+  int M, N;
 
-  scanf("%hd%hd", &M, &N);
+  scanf("%d%d", &M, &N);
   for (int i = M; i <= N; i++)
-    nums[i - M] = {(i > 9 ? digits[i / 10] : "") + digits[i % 10], i};
+    nums[i - M] = {
+        (i > 9 ? 10 * digits[i / 10] + digits[i % 10] : 10 * digits[i]), i};
   sort(nums, nums + N - M + 1);
   for (int i = 0; i < N - M + 1; i++) {
-    printf("%hd ", nums[i].second);
+    printf("%d ", nums[i].second);
     if (!((i + 1) % 10)) printf("\n");
   }
 }
