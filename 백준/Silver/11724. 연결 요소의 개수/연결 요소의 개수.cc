2cb0bac8f5a4424300b00290c9209b20b6c258a6
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-05 21:18:11 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 108 ms, Memory: 5608 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,37 +1,38 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 
 using namespace std;
 
 int N, M;
+bool visited[1001];
 vector<int> graph[1001];
-int visited[1001];
 
 void dfs(int node) {
-    if (visited[node]) return;
-    else {
-        visited[node] = 1;
-        for (auto i: graph[node]) {
-            dfs(i);
-        }
+  if (visited[node])
+    return;
+  else {
+    visited[node] = 1;
+    for (auto i : graph[node]) {
+      dfs(i);
     }
+  }
 }
 
 int main() {
-    int u, v;
-    int components;
-    
-    cin >> N >> M;
-    components = N;
-    for (int i = 0; i < M; i++) {
-        cin >> u >> v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-    for (int i = 0; i <= N; i++) {
-        if (visited[i]) components--;
-        if (graph[i].empty()) continue;
-        dfs(i);
-    }
-    cout << components;
+  int u, v;
+  int components;
+
+  scanf("%d%d", &N, &M);
+  components = N;
+  for (int i = 0; i < M; i++) {
+    scanf("%d%d", &u, &v);
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+  }
+  for (int i = 0; i <= N; i++) {
+    if (visited[i]) components--;
+    if (graph[i].empty()) continue;
+    dfs(i);
+  }
+  printf("%d", components);
 }
