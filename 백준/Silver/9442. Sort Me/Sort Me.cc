2c7ae50c475d4539b9097402aab4686094db3f33
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-07 21:06:16 +0900

    [Silver IV] Title: Sort Me, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/9442. Sort Me/Sort Me.cc
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  for (int t = 1;; t++) {
+    int n;
+    cin >> n;
+    if (!n) break;
+
+    string str;
+    map<char, int> order;
+    vector<string> words(n);
+
+    cin >> str;
+    for (int i = 0; i < 26; i++) order[str[i]] = i;
+    for (int i = 0; i < n; i++) cin >> words[i];
+    sort(words.begin(), words.end(), [&order](string& a, string& b) {
+      int len = min(a.length(), b.length());
+      if (a.substr(0, len) == b.substr(0, len)) return a.length() < b.length();
+      for (int i = 0; i < len; i++) {
+        if (order[a[i]] == order[b[i]]) continue;
+        return (order[a[i]] < order[b[i]]);
+      }
+      return true;
+    });
+    cout << "year " << t << '\n';
+    for (auto& word : words) cout << word << '\n';
+  }
+}
