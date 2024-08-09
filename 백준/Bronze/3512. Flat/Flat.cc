50518a98bd1b7d14a3a401d5c3d8bf49a39d0341
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-10 03:32:22 +0900

    [Bronze III] Title: Flat, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3512. Flat/Flat.cc
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstring>
+
+int main() {
+  int n, c, bedroom = 0, balcony = 0, total = 0;
+
+  scanf("%d%d", &n, &c);
+  while (n--) {
+    int a;
+    char t[9];
+
+    scanf("%d%s", &a, t);
+    total += a;
+    if (!strcmp(t, "bedroom"))
+      bedroom += a;
+    else if (!strcmp(t, "balcony"))
+      balcony += a;
+  }
+  printf("%d\n%d\n%.6f", total, bedroom, c * (double)(total - 0.5 * balcony));
+}
