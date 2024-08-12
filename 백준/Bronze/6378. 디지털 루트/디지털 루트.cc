dffb55e88f6acdd53cb3304c07be204de63cc71a
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-12 12:31:06 +0900

    [Bronze III] Title: 디지털 루트, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/6378. 디지털 루트/디지털 루트.cc
+++ 백준/Bronze/6378. 디지털 루트/디지털 루트.cc
@@ -1,23 +1,12 @@
 #include <cstdio>
-#include <cstring>
+
+char digits[1001];
 
 int main() {
-  while (true) {
+  while (scanf("%s", digits) && digits[0] != '0') {
     int root = 0;
-    char digits[1'001];
 
-    scanf("%s", digits);
-    if (!strcmp(digits, "0")) break;
-    for (int len = strlen(digits), i = 0; i < len; i++) root += digits[i] - '0';
-    while (root > 9) {
-      int digit_sum = 0;
-
-      while (root) {
-        digit_sum += root % 10;
-        root /= 10;
-      }
-      root = digit_sum;
-    }
-    printf("%d\n", root);
+    for (int i = 0; digits[i] != '\0'; i++) root += digits[i] - '0';
+    printf("%d\n", (root - 1) % 9 + 1);
   }
 }
