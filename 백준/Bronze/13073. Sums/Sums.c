18608a1e41cebdf9056bf57651fa8597f7ffc755
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-16 20:59:06 +0900

    [Bronze III] Title: Sums, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/13073. Sums/Sums.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int t;
+
+  scanf("%d", &t);
+  while (t--) {
+    int N;
+
+    scanf("%d", &N);
+    printf("%d %d %d\n", N * (1 + N) / 2, N * N, N * (N + 1));
+  }
+}
