d091c3483b1d353d51e9fcf685de9e97cc7a6335
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-16 21:01:06 +0900

    [Bronze III] Title: Sums, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/13073. Sums/Sums.cc
+++ 백준/Bronze/13073. Sums/Sums.cc
@@ -1,13 +1,18 @@
-#include <cstdio>
+#include <iostream>
+
+using namespace std;
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
   int t;
 
-  scanf("%d", &t);
+  cin >> t;
   while (t--) {
     int N;
 
-    scanf("%d", &N);
-    printf("%d %d %d\n", N * (1 + N) / 2, N * N, N * (N + 1));
+    cin >> N;
+    cout << N * (1 + N) / 2 << ' ' << N * N << ' ' << N * (N + 1) << '\n';
   }
 }
