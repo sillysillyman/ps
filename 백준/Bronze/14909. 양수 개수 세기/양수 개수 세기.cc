e24a453091d46f004018a7d94eedc4da7fd5d84a
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-18 21:12:31 +0900

    [Bronze III] Title: 양수 개수 세기, Time: 100 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14909. 양수 개수 세기/양수 개수 세기.cc
+++ 백준/Bronze/14909. 양수 개수 세기/양수 개수 세기.cc
@@ -1,8 +1,12 @@
-#include <cstdio>
+#include <iostream>
+
+using namespace std;
 
 int n, cnt;
 
 int main() {
-  while (~scanf("%d", &n)) cnt += n > 0;
-  printf("%d", cnt);
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  while (cin >> n) cnt += n > 0;
+  cout << cnt;
 }
