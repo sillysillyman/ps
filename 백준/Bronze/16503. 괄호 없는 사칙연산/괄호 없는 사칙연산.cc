4a07feb36f3fe39fb812ca9c8425701b1ee92ff0
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-23 01:24:44 +0900

    [Bronze III] Title: 괄호 없는 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/16503. 괄호 없는 사칙연산/괄호 없는 사칙연산.cc
+++ 백준/Bronze/16503. 괄호 없는 사칙연산/괄호 없는 사칙연산.cc
@@ -1,6 +1,5 @@
-#include <iostream>
-
-using namespace std;
+#include <algorithm>
+#include <cstdio>
 
 int calc(int x, int y, char op) {
   switch (op) {
@@ -20,9 +19,10 @@ int main() {
   int K1, K2, K3;
   char O1, O2;
 
-  cin >> K1 >> O1 >> K2 >> O2 >> K3;
+  scanf("%d %c %d %c %d", &K1, &O1, &K2, &O2, &K3);
+
   int ans2 = calc(calc(K1, K2, O1), K3, O2);
   int an2 = calc(K1, calc(K2, K3, O2), O1);
 
-  cout << min(ans2, an2) << '\n' << max(ans2, an2);
+  printf("%d\n%d", std::min(ans2, an2), std::max(ans2, an2));
 }
