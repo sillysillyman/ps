5af0d78e9e438d66fb3a6dddfdfaf917a6da19d6
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-26 00:07:26 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -25,22 +25,12 @@ int main() {
             if (v[j] < v[i]) lis_v[i] = max(lis_v[i], lis_v[j] + 1);
             if (w[j] < w[i]) lis_w[i] = max(lis_w[i], lis_w[j] + 1);
         }
+
     }
     int max_bitonic_len = 0;
-
-    // cout << endl;
-    // for (int i = 0; i < N; i++) cout << lis_v[i] << ' ';
-    // cout << endl;
-    // for (int i = 0; i < N; i++) cout << lis_w[i] << ' ';
-    // cout << endl;
 
     for (int i = 0; i < N; i++) {
         max_bitonic_len = max(max_bitonic_len, lis_v[i] + lis_w[N - i - 1] - 1);
     }
     cout << max_bitonic_len;
 }
-// 1 5 2 1 4 3 4 5 2 1
-// 1 2 2 2 3 3 4 5 5 5
-
-// 1 2 5 4 3 4 1 2 5 1
-// 1 2 3 3 3 4 4 4 5 5
