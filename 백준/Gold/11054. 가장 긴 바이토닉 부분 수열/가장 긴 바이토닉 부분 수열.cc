ce55382a43016f518d144b2204d690c3f46fa4f1
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-26 00:12:12 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 1236 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -1,36 +1,36 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <cstdio>
+#include <vector>
 
 using namespace std;
 
+int lis_v[1'000];
+int lis_w[1'000];
+
 int main() {
-    int N, num;
-    int lis_v[1000];
-    int lis_w[1000];
-    vector<int> v;
-    vector<int> w;
+  int N;
 
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> num;
-        v.push_back(num);
-        w.push_back(num);
+  scanf("%d", &N);
+  vector<int> v(N);
+  vector<int> w(N);
+  for (int num, i = 0; i < N; i++) {
+    scanf("%d", &num);
+    v[i] = num;
+    w[i] = num;
+  }
+  reverse(w.begin(), w.end());
+  for (int i = 0; i < N; i++) {
+    lis_v[i] = 1;
+    lis_w[i] = 1;
+    for (int j = 0; j < i; j++) {
+      if (v[j] < v[i]) lis_v[i] = max(lis_v[i], lis_v[j] + 1);
+      if (w[j] < w[i]) lis_w[i] = max(lis_w[i], lis_w[j] + 1);
     }
-    reverse(w.begin(), w.end());
-    for (int i = 0; i < N; i++) {
-        lis_v[i] = 1;
-        lis_w[i] = 1;
-        for (int j = 0; j < i; j++) {
-            if (v[j] < v[i]) lis_v[i] = max(lis_v[i], lis_v[j] + 1);
-            if (w[j] < w[i]) lis_w[i] = max(lis_w[i], lis_w[j] + 1);
-        }
+  }
 
-    }
-    int max_bitonic_len = 0;
+  int max_bitonic_len = 1;
 
-    for (int i = 0; i < N; i++) {
-        max_bitonic_len = max(max_bitonic_len, lis_v[i] + lis_w[N - i - 1] - 1);
-    }
-    cout << max_bitonic_len;
+  for (int i = 0; i < N; i++)
+    max_bitonic_len = max(max_bitonic_len, lis_v[i] + lis_w[N - i - 1] - 1);
+  printf("%d", max_bitonic_len);
 }
