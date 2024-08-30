2859cef75107281ab99489af54911daee3d956a6
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-30 20:59:49 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 1228 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -6,15 +6,15 @@ using namespace std;
 
 int N, M;
 vector<int> arr;
-vector<int> stack;
+vector<int> seq;
 
 void dfs(int curr, int depth) {
   if (depth == M) {
-    for (int num : stack) printf("%d ", num);
+    for (int num : seq) printf("%d ", num);
     printf("\n");
     return;
   }
-  stack[depth] = arr[curr];
+  seq[depth] = arr[curr];
   for (int i = curr; i < N; i++) {
     dfs(i, depth + 1);
     if (depth == M - 1) break;
@@ -24,7 +24,7 @@ void dfs(int curr, int depth) {
 int main() {
   scanf("%d%d", &N, &M);
   arr.resize(N);
-  stack.resize(M);
+  seq.resize(M);
   for (int e, i = 0; i < N; i++) scanf("%d", &arr[i]);
   sort(arr.begin(), arr.end());
   arr.erase(unique(arr.begin(), arr.end()), arr.end());
