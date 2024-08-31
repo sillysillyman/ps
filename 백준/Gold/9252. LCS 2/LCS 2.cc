51e42d67e14bcb56369af8bff436f1d709042e2a
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-31 22:47:32 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 5024 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -1,29 +1,31 @@
-#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
 
-using namespace std;
-
-int dp[1'001][1'001];
+const int MAX = 1'000;
+int dp[MAX + 1][MAX + 1];
 
 int main() {
-  string s, t;
+  char s[MAX + 1], t[MAX + 1], lcs[MAX + 1];
 
-  cin >> s >> t;
+  scanf("%s%s", s, t);
 
-  int s_len = s.length();
-  int t_len = t.length();
+  int s_len = strlen(s);
+  int t_len = strlen(t);
   for (int i = 0; i < s_len; i++) {
     for (int j = 0; j < t_len; j++) {
       if (s[i] == t[j])
         dp[i + 1][j + 1] = dp[i][j] + 1;
       else
-        dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
+        dp[i + 1][j + 1] = std::max(dp[i][j + 1], dp[i + 1][j]);
     }
   }
 
-  string lcs;
+  int idx = dp[s_len][t_len];
+  lcs[idx] = '\0';
   for (int i = s_len, j = t_len; i && j;) {
     if (s[i - 1] == t[j - 1]) {
-      lcs = s[i - 1] + lcs;
+      lcs[--idx] = s[i - 1];
       --i;
       --j;
     } else if (dp[i - 1][j] > dp[i][j - 1])
@@ -31,5 +33,5 @@ int main() {
     else
       --j;
   }
-  cout << dp[s_len][t_len] << '\n' << lcs;
+  printf("%d\n%s", dp[s_len][t_len], lcs);
 }
