f4ed014b145c6d9b2dad96a86462b20969ba6f58
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-09-01 00:12:49 +0900

    [Gold III] Title: 진법 변환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1112. 진법 변환/진법 변환.cc
+++ 백준/Gold/1112. 진법 변환/진법 변환.cc
@@ -14,26 +14,19 @@ int main() {
     if (b > 0) x = -x;
   }
   while (x) {
-    // cout << "x = " << x << endl;
-    // cout << "x % b = " << x % b << endl;
     digit = x % b;
     if (digit >= 0) {
       res = to_string((x % b)) + res;
-      //   cout << "res: " << res << endl;
       carry = 0;
     } else {
-      if (digit > b) {
+      if (digit > b)
         res = to_string(x % b - b) + res;
-        carry = 1;
-      } else {
+      else
         res = to_string(x % b + b) + res;
-        // carry = 0;
-      }
-      //   cout << "res: " << res << endl;
+      carry = 1;
     }
     x /= b;
     x += carry;
-    // cout << '\n';
   }
   if (negative && b > 0) res = '-' + res;
   if (res.empty())
