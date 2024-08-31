853ade19cd363df64b1e889360982f848340b268
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-08-31 19:22:25 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[1'001][1'001];
+
+int main() {
+  string s, t;
+
+  cin >> s >> t;
+
+  int s_len = s.length();
+  int t_len = t.length();
+  for (int i = 0; i < s_len; i++) {
+    for (int j = 0; j < t_len; j++) {
+      if (s[i] == t[j])
+        dp[i + 1][j + 1] = dp[i][j] + 1;
+      else
+        dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
+    }
+  }
+
+  int i = s_len, j = t_len;
+  string lcs;
+
+  while (i > 0 && j > 0) {
+    if (s[i - 1] == t[j - 1]) {
+      lcs = s[i - 1] + lcs;
+      --i;
+      --j;
+    } else if (dp[i - 1][j] > dp[i][j - 1])
+      --i;
+    else
+      --j;
+  }
+  cout << dp[s_len][t_len] << '\n' << lcs;
+}
