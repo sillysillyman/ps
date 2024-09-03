d73da2bfc9a8f134168eccf1a135519070c43840
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-09-04 06:14:21 +0900

    [Bronze IV] Title: Zero or One, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/13623. Zero or One/Zero or One.cc
+++ 백준/Bronze/13623. Zero or One/Zero or One.cc
@@ -4,21 +4,6 @@ int main() {
   int A, B, C;
 
   scanf("%d%d%d", &A, &B, &C);
-  if (A + B + C == 0 || A + B + C == 3)
-    printf("*");
-  else if (A + B + C == 1) {
-    if (A)
-      printf("A");
-    else if (B)
-      printf("B");
-    else
-      printf("C");
-  } else {
-    if (!A)
-      printf("A");
-    else if (!B)
-      printf("B");
-    else
-      printf("C");
-  }
+  printf("%c", A ? (B ? (C ? '*' : 'C') : (C ? 'B' : 'A'))
+                 : (B ? (C ? 'A' : 'B') : (C ? 'C' : '*')));
 }
