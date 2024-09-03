cf971e23683f4cb1653ed92962f4b986b5a6b0c8
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-09-04 01:25:54 +0900

    [Bronze II] Title: 24, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1408. 24/24.cc
+++ 백준/Bronze/1408. 24/24.cc
@@ -1,47 +1,20 @@
 #include <cstdio>
 
-struct Time {
-  int hour, minute, second;
-
-  Time(int hour, int minute, int second)
-      : hour(hour), minute(minute), second(second) {}
-
-  Time(char str[9]) {
-    hour = 10 * (str[0] - '0') + str[1] - '0';
-    minute = 10 * (str[3] - '0') + str[4] - '0';
-    second = 10 * (str[6] - '0') + str[7] - '0';
-  }
-
-  Time operator-(Time& other) {
-    int res_hour, res_minute, res_second;
-
-    res_second = second - other.second;
-    if (res_second < 0) {
-      res_second += 60;
-      --minute;
-    }
-    res_minute = minute - other.minute;
-    if (res_minute < 0) {
-      res_minute += 60;
-      --hour;
-    }
-    res_hour = hour - other.hour;
-    if (res_hour < 0) res_hour += 24;
-
-    return Time(res_hour, res_minute, res_second);
-  }
-
-  void to_string(char* str) {
-    snprintf(str, 9, "%02d:%02d:%02d", hour, minute, second);
-  }
-};
-
 int main() {
-  char now_str[9], start_str[9], diff_str[9];
-  scanf("%s%s", now_str, start_str);
+  int h1, m1, s1, h2, m2, s2;
 
-  Time now = Time(now_str);
-  Time start = Time(start_str);
-  (start - now).to_string(diff_str);
-  printf("%s", diff_str);
+  scanf("%d:%d:%d%d:%d:%d", &h1, &m1, &s1, &h2, &m2, &s2);
+  s2 -= s1;
+  if (s2 < 0) {
+    s2 += 60;
+    --m2;
+  }
+  m2 -= m1;
+  if (m2 < 0) {
+    m2 += 60;
+    --h2;
+  }
+  h2 -= h1;
+  if (h2 < 0) h2 += 24;
+  printf("%02d:%02d:%02d", h2, m2, s2);
 }
