357f31089dff2f15b9579726ede50bbeb115b268
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-09-19 20:00:22 +0900

    [Gold III] Title: 색 막대, Time: 320 ms, Memory: 37220 KB -BaekjoonHub

--- 백준/Gold/1696. 색 막대/색 막대.cc
+++ 백준/Gold/1696. 색 막대/색 막대.cc
@@ -1,4 +1,4 @@
-#include <cstdio>
+#include <iostream>
 #include <map>
 
 using namespace std;
@@ -19,11 +19,13 @@ ll to_hash(char *s) {
 }
 
 int main() {
-  for (int i = 0; i < 500'000; i++) p[i] = i;
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
 
   int x, y, odd = 0, idx = 0;
 
-  while (~scanf("%s %s", c1, c2)) {
+  for (int i = 0; i < 500'000; i++) p[i] = i;
+  while (cin >> c1 >> c2) {
     ll h1 = to_hash(c1), h2 = to_hash(c2);
     if (m.find(h1) == m.end()) m[h1] = idx++;
     if (m.find(h2) == m.end()) m[h2] = idx++;
@@ -34,6 +36,6 @@ int main() {
   for (int i = 0; i < idx; i++)
     if (deg[i] & 1) ++odd;
   for (int i = 0; i < idx; i++)
-    if (find(i) != find(0)) return puts("Impossible"), 0;
-  puts(odd == 0 || odd == 2 ? "Possible" : "Impossible");
+    if (find(i) != find(0)) return cout << "Impossible", 0;
+  cout << (odd == 0 || odd == 2 ? "Possible" : "Impossible");
 }
