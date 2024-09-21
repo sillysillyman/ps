8298a7f5ef10ce4b81f066e84822764ff955c104
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-09-21 18:39:12 +0900

    [Silver V] Title: 부울행렬의 부울곱, Time: 20 ms, Memory: 1136 KB -BaekjoonHub

--- 백준/Silver/14492. 부울행렬의 부울곱/부울행렬의 부울곱.cc
+++ 백준/Silver/14492. 부울행렬의 부울곱/부울행렬의 부울곱.cc
@@ -7,20 +7,14 @@ int main() {
   int N, cnt = 0;
 
   scanf("%d", &N);
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
       if (int b; scanf("%d", &b) && b) A[i][j] = 1;
-    }
-  }
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
       if (int b; scanf("%d", &b) && b) B[j][i] = 1;
-    }
-  }
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
       if ((A[i] & B[j]).any()) ++cnt;
-    }
-  }
   printf("%d", cnt);
 }
