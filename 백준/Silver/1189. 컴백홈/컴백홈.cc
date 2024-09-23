ff5a53d88083a168791bc63f7000cc6e4fc0fb00
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-09-23 23:22:59 +0900

    [Silver I] Title: 컴백홈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.cc
+++ 백준/Silver/1189. 컴백홈/컴백홈.cc
@@ -5,15 +5,14 @@ int dx[4] = {-1, 0, 0, 1};
 int dy[4] = {0, -1, 1, 0};
 char graph[5][5];
 
-bool is_inside(int x, int y) { return 0 <= x && x < R && 0 <= y && y < C; }
-
 void dfs(int x, int y, int depth) {
   if (x == 0 && y == C - 1 && depth == K) ++cnt;
   graph[x][y] = 'T';
   for (int i = 0; i < 4; i++) {
     int nx = x + dx[i];
     int ny = y + dy[i];
-    if (is_inside(nx, ny) && graph[nx][ny] == '.') dfs(nx, ny, depth + 1);
+    if (0 <= nx && nx < R && 0 <= ny && ny < C && graph[nx][ny] == '.')
+      dfs(nx, ny, depth + 1);
   }
   graph[x][y] = '.';
 }
