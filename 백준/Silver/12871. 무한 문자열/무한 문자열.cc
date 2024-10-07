898a0d7888a7ab91dae748d343f2418e880c2c03
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-10-08 00:38:09 +0900

    [Silver V] Title: 무한 문자열, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/12871. 무한 문자열/무한 문자열.cc
+++ 백준/Silver/12871. 무한 문자열/무한 문자열.cc
@@ -4,16 +4,14 @@
 
 using namespace std;
 
-int main() {
-  char s[101], t[101], temp_s[2451], temp_t[2451];
+char s[101], t[101], temp_s[2451], temp_t[2451];
 
+int main() {
   scanf("%s%s", s, t);
 
   int len_s = strlen(s), len_t = strlen(t);
   int n = lcm(len_s, len_t);
 
-  strcpy(temp_s, s);
-  strcpy(temp_t, t);
   while (strlen(temp_s) < n) strcat(temp_s, s);
   while (strlen(temp_t) < n) strcat(temp_t, t);
   printf("%d", !strcmp(temp_s, temp_t));
