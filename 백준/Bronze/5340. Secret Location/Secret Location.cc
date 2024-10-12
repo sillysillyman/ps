fea7b4785ac66f0869c0d6aba0145ae3b37b4842
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-10-13 00:28:29 +0900

    [Bronze III] Title: Secret Location, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5340. Secret Location/Secret Location.cc
+++ 백준/Bronze/5340. Secret Location/Secret Location.cc
@@ -5,11 +5,9 @@ int main() {
   int arr[6];
   char str[181];
 
-  for (int len, i = 0; i < 6; i++) {
+  for (int i = 0; i < 6; i++) {
     scanf(" %[^\n]", str);
-    len = strlen(str);
-    if (str[len - 1] == ' ') str[len--] = '\0';
-    arr[i] = len;
+    arr[i] = strlen(str) - (str[strlen(str) - 1] == ' ');
   }
   printf("Latitude %d:%d:%d\nLongitude %d:%d:%d\n", arr[0], arr[1], arr[2],
          arr[3], arr[4], arr[5]);
