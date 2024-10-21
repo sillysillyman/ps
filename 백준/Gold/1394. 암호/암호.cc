02506ed3deb0da5e0254d6331c408d93bb008e83
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-10-21 19:45:56 +0900

    [Gold V] Title: 암호, Time: 12 ms, Memory: 3684 KB -BaekjoonHub

--- 백준/Gold/1394. 암호/암호.cc
+++ 백준/Gold/1394. 암호/암호.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 const int MOD = 900'528;
-map<char, int> idxes;
+map<char, int> idx;
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -15,7 +15,7 @@ int main() {
 
   cin >> str >> pwd;
   len = str.length();
-  for (int i = 0; i < len; i++) idxes[str[i]] = i;
-  for (char& ch : pwd) cnt = (cnt * len + idxes[ch] + 1) % MOD;
+  for (int i = 0; i < len; i++) idx[str[i]] = i;
+  for (char& ch : pwd) cnt = (cnt * len + idx[ch] + 1) % MOD;
   cout << cnt;
 }
