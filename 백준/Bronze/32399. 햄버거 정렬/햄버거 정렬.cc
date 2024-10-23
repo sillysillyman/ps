d440bce4ca3a6aaa8f9ec82c2e2ee1a58485e800
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-10-23 16:52:12 +0900

    [Bronze III] Title: 햄버거 정렬, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32399. 햄버거 정렬/햄버거 정렬.cc
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cstring>
+
+int main() {
+  char S[4];
+
+  scanf("%s", S);
+  printf("%d", !strcmp(S, "(1)") ? 0
+               : !strcmp(S, "1()") || !strcmp(S, "1)(") || !strcmp(S, "()1") ||
+                       !strcmp(S, ")(1")
+                   ? 1
+                   : 2);
+}
