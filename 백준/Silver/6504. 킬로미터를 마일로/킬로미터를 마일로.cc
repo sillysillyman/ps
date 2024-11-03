612bcb8e871da4f3fdfe0b99c923ce81931b9167
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-03 19:21:04 +0900

    [Silver V] Title: 킬로미터를 마일로, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/6504. 킬로미터를 마일로/킬로미터를 마일로.cc
+++ 백준/Silver/6504. 킬로미터를 마일로/킬로미터를 마일로.cc
@@ -8,17 +8,14 @@ int main() {
   scanf("%d", &t);
   while (t--) {
     int x, y = 0;
-    int base[22];
 
     scanf("%d", &x);
-    for (int i = 21; i >= 0; i--) {
-      base[21 - i] = 0;
+    for (int i = 21; i > 0; i--) {
       if (x >= fibo[i]) {
         x -= fibo[i];
-        base[21 - i] = 1;
+        y += fibo[i - 1];
       }
     }
-    for (int i = 0; i < 21; i++) y += base[i] * fibo[20 - i];
     printf("%d\n", y);
   }
 }
