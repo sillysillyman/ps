e9dcb1a2ddca1e7aba48c0b520028d470898c2a1
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-05 18:49:26 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -1,23 +1,19 @@
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
 
 int main() {
-    int a1, a0;
-    int c;
-    int n0;
-    bool success = true;
+  int a1, a0, c, n0;
+  bool success = true;
 
-    cin >> a1 >> a0 >> c >> n0;
-    if (c < a1) cout << 0;
-    else {
-        for (int i = n0; i <= 100; i++) {
-            if (a1*n0 + a0 > c*n0) {
-                success = false;
-                break;
-            }
-        }
-        if (success) cout << 1;
-        else cout << 0;
+  scanf("%d%d%d%d", &a1, &a0, &c, &n0);
+  if (c < a1)
+    puts("0");
+  else {
+    for (int i = n0; i <= 100; i++) {
+      if (a1 * n0 + a0 > c * n0) {
+        success = false;
+        break;
+      }
     }
+    printf("%d", success ? 1 : 0);
+  }
 }
