5924e07a4cffda4bbf6121856f56545fc93e927a
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-07 00:18:23 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cc
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cc
@@ -4,9 +4,11 @@ int main() {
   int N;
 
   scanf("%d", &N);
-  printf("%s", N == 1   ? "11\nA B C D E F G H J L M"
-               : N < 4  ? "9\nA C E F G H I L M"
-               : N == 4 ? "9\nA B C E F G H L M"
-               : N < 10 ? "8\nA C E F G H L M"
-                        : "8\nA B C F G H L M");
+  printf("%s%s",
+         N == 1   ? "11\nA B C D E F G H J "
+         : N < 4  ? "9\nA C E F G H I "
+         : N == 4 ? "9\nA B C E F G H "
+         : N < 10 ? "8\nA C E F G H "
+                  : "8\nA B C F G H ",
+         "L M");
 }
