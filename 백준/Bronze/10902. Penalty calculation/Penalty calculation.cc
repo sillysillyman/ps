9ae75fe3e9bce39bc2902df64886d2ce0646fb3d
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-07 17:24:30 +0900

    [Bronze III] Title: Penalty calculation, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10902. Penalty calculation/Penalty calculation.cc
+++ 백준/Bronze/10902. Penalty calculation/Penalty calculation.cc
@@ -1,7 +1,7 @@
 #include <cstdio>
 
 int main() {
-  int n, f, s_f = -1, t_f;
+  int n, f, t_f, s_f = -1;
 
   scanf("%d", &n);
   for (int i = 1; i <= n; i++) {
@@ -10,8 +10,8 @@ int main() {
     scanf("%d%d", &t, &s);
     if (s > s_f) {
       f = i;
-      s_f = s;
       t_f = t;
+      s_f = s;
     }
   }
   printf("%d", s_f ? t_f + (f - 1) * 20 : 0);
