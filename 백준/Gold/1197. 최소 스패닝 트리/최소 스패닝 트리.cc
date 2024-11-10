864725294c66e7a53406d6a8ead72afdccb0de96
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-11 00:50:05 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 32 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -6,47 +6,35 @@ using namespace std;
 
 struct Edge {
   int src, dest, weight;
+
+  bool operator<(const Edge& other) const { return weight < other.weight; }
 };
 
 const int MAX_V = 10'000;
-int parents[MAX_V + 1];
+int parents[MAX_V];
 vector<Edge> edges;
 
-int find(int x) {
-  if (x == parents[x]) return x;
-  return parents[x] = find(parents[x]);
-}
+int find(int x) { return x == parents[x] ? x : parents[x] = find(parents[x]); }
 
-void merge(int x, int y) {
-  x = find(x);
-  y = find(y);
-  if (x == y) return;
-  if (x < y)
-    parents[y] = x;
-  else
-    parents[x] = y;
-}
+void unite(int x, int y) { parents[find(x)] = find(y); }
 
 int main() {
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(nullptr);
 
   int V, E, weight = 0;
 
   cin >> V >> E;
-  for (int i = 1; i <= V; i++) parents[i] = i;
-  for (int A, B, C, i = 0; i < E; i++) {
-    Edge edge;
-
+  for (int i = 0; i < V; i++) parents[i] = i;
+  while (E--) {
+    int A, B, C;
     cin >> A >> B >> C;
-    edge = {A, B, C};
-    edges.push_back(edge);
+    edges.push_back({A, B, C});
   }
-  sort(edges.begin(), edges.end(),
-       [](Edge& u, Edge& v) { return u.weight < v.weight; });
+  sort(edges.begin(), edges.end());
   for (Edge& edge : edges) {
     if (find(edge.src) != find(edge.dest)) {
-      merge(edge.src, edge.dest);
+      unite(edge.src, edge.dest);
       weight += edge.weight;
     }
   }
