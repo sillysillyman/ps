e7c5b0236dfb430a8bf8b42da4ad4c06316503bd
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-16 01:26:31 +0900

    [Gold III] Title: MST 게임, Time: 12 ms, Memory: 1232 KB -BaekjoonHub

---
+++ 백준/Gold/16202. MST 게임/MST 게임.cc
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+struct Edge {
+  int u, v, w;
+
+  bool operator>(const Edge& other) const { return w > other.w; }
+};
+
+vector<int> p;
+vector<Edge> edges;
+
+int find(int x) { return x == p[x] ? x : p[x] = find(p[x]); }
+
+void unite(int x, int y) { p[find(x)] = find(y); }
+
+int main() {
+  int N, M, K;
+
+  scanf("%d%d%d", &N, &M, &K);
+  p.resize(N);
+  edges.resize(M);
+  for (int i = 0; i < N; i++) p[i] = i;
+  for (int i = 0; i < M; i++) {
+    int u, v;
+
+    scanf("%d%d", &u, &v);
+    --u, --v;
+    edges[i] = {u, v, i + 1};
+  }
+
+  bool flag = false;
+  sort(edges.begin(), edges.end(), greater());
+  while (K--) {
+    int score = 0;
+
+    if (!flag) {
+      for (int i = 0; i < N; i++) p[i] = i;
+      for (int i = M - 1; i >= 0; i--) {
+        auto [u, v, w] = edges[i];
+
+        if (find(u) == find(v)) continue;
+        unite(u, v);
+        score += w;
+      }
+
+      int root = find(0);
+      for (int i = 1; i < N; i++) {
+        if (find(i) != root) {
+          flag = true;
+          break;
+        }
+      }
+    }
+
+    if (flag) score = 0;
+
+    printf("%d ", score);
+    --M;
+  }
+}
