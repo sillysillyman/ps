f401b9fa7ac3e8d3651ef8156a8cada07ab5a99b
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-20 19:10:29 +0900

    [Bronze II] Title: Occupy parking, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/16018. Occupy parking/Occupy parking.cc
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+char yesterday[101], today[101];
+
+int main() {
+  int N, cnt = 0;
+
+  scanf("%d", &N);
+  for (int i = 0; i < N; i++) scanf(" %c", &yesterday[i]);
+  for (int i = 0; i < N; i++) scanf(" %c", &today[i]);
+  for (int i = 0; i < N; i++) cnt += (yesterday[i] == 'C') & (today[i] == 'C');
+  printf("%d", cnt);
+}
