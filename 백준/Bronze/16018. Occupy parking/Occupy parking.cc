c01f8df84e23e893aa72d340bd3b27224cd7d925
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-20 19:12:48 +0900

    [Bronze II] Title: Occupy parking, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/16018. Occupy parking/Occupy parking.cc
+++ 백준/Bronze/16018. Occupy parking/Occupy parking.cc
@@ -4,9 +4,7 @@ int N, cnt;
 char y[101], t[101];
 
 int main() {
-  scanf("%d", &N);
-  for (int i = 0; i < N; i++) scanf(" %c", &y[i]);
-  for (int i = 0; i < N; i++) scanf(" %c", &t[i]);
+  scanf("%d %s %s", &N, y, t);
   for (int i = 0; i < N; i++) cnt += (y[i] == 'C') & (t[i] == 'C');
   printf("%d", cnt);
 }
