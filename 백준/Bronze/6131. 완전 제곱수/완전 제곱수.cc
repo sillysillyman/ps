56e6fe4552d361593a76c3d08329753b892e5ea4
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-21 16:46:39 +0900

    [Bronze III] Title: 완전 제곱수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/6131. 완전 제곱수/완전 제곱수.cc
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.cc
@@ -4,7 +4,7 @@ int N, cnt;
 
 int main() {
   scanf("%d", &N);
-  for (int i = 1; i <= 1'000; i++)
+  for (int i = 1; i <= 500; i++)
     for (int j = 1; j < i; j++)
       if (i * i == j * j + N) ++cnt;
   printf("%d", cnt);
