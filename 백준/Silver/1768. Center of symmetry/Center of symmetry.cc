8958864c60c2040cf35628a45b9291cebbe07dd2
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-22 23:43:33 +0900

    [Silver II] Title: Center of symmetry, Time: 16 ms, Memory: 1704 KB -BaekjoonHub

---
+++ 백준/Silver/1768. Center of symmetry/Center of symmetry.cc
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <set>
+
+using namespace std;
+
+struct Point {
+  double x, y;
+
+  bool operator<(const Point& other) const {
+    return x == other.x ? y < other.y : x < other.x;
+  }
+
+  bool operator==(const Point& other) const {
+    return x == other.x && y == other.y;
+  }
+
+  Point operator-(const Point& other) const {
+    return {x - other.x, y - other.y};
+  }
+};
+
+int main() {
+  int c;
+
+  scanf("%d", &c);
+  while (c--) {
+    int n;
+    double x_sum = 0, y_sum = 0;
+    set<Point> points;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+      double x, y;
+
+      scanf("%lf%lf", &x, &y);
+      x_sum += x;
+      y_sum += y;
+      points.insert(Point{x, y});
+    }
+
+    bool flag = true;
+    Point p = {2 * x_sum / n, 2 * y_sum / n};
+    for (auto& point : points) {
+      if (points.find(p - point) == points.end()) {
+        flag = false;
+        break;
+      }
+    }
+    printf("%s\n", flag ? "yes" : "no");
+  }
+}
