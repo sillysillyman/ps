694d626c7220632962f7c01ee509a6d4e4122410
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-23 18:50:39 +0900

    [Silver V] Title: Shirts, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/5078. Shirts/Shirts.cc
+++ 백준/Silver/5078. Shirts/Shirts.cc
@@ -1,7 +1,5 @@
 #include <algorithm>
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
 
 struct Shirt {
   char size[3];
@@ -10,32 +8,19 @@ struct Shirt {
     return size[0] == other.size[0] ? size[1] < other.size[1]
                                     : size[0] > other.size[0];
   }
-
-  friend istream& operator>>(istream& in, Shirt& s) {
-    in >> s.size;
-    return in;
-  }
-
-  friend ostream& operator<<(ostream& out, const Shirt& s) {
-    out << s.size;
-    return out;
-  }
 };
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-
   int W, H;
 
-  while (cin >> W && W) {
+  while (scanf("%d", &W) && W) {
     Shirt shirts[60];
 
-    for (int i = 0; i < W; i++) cin >> shirts[i];
-    cin >> H;
-    for (int i = 0; i < H; i++) cin >> shirts[W + i];
-    sort(shirts, shirts + W + H);
-    for (int i = 0; i < W + H; i++) cout << shirts[i] << ' ';
-    cout << '\n';
+    for (int i = 0; i < W; i++) scanf("%s", shirts[i].size);
+    scanf("%d", &H);
+    for (int i = 0; i < H; i++) scanf("%s", shirts[W + i].size);
+    std::sort(shirts, shirts + W + H);
+    for (int i = 0; i < W + H; i++) printf("%s ", shirts[i].size);
+    puts("");
   }
 }
