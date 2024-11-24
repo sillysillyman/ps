0f4d54a245c680a5f20fb07d759b65ca7aec0033
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-24 16:34:04 +0900

    [Silver V] Title: Costume Party, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/6159. Costume Party/Costume Party.cc
+++ 백준/Silver/6159. Costume Party/Costume Party.cc
@@ -1,13 +1,18 @@
 #include <algorithm>
-#include <cstdio>
+#include <iostream>
+
+using namespace std;
 
 int N, S, cnt;
 int arr[20'000];
 
 int main() {
-  scanf("%d%d", &N, &S);
-  for (int i = 0; i < N; i++) scanf("%d", &arr[i]);
-  std::sort(arr, arr + N);
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> N >> S;
+  for (int i = 0; i < N; i++) cin >> arr[i];
+  sort(arr, arr + N);
 
   int left = 0, right = N - 1;
   while (left < right) {
@@ -17,5 +22,5 @@ int main() {
     } else
       --right;
   }
-  printf("%d", cnt);
+  cout << cnt;
 }
