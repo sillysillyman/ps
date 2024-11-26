152246fd522bff36cc4474683c5e24484b1c4828
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-26 12:01:10 +0900

    [Silver V] Title: 得点 (Score), Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/24155. 得点 （Score）/得点 （Score）.cc
+++ 백준/Silver/24155. 得点 （Score）/得点 （Score）.cc
@@ -1,14 +1,19 @@
-#include <cstdio>
+#include <iostream>
+
+using namespace std;
 
 int scores[101];
 int arr[100'000];
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
   int n;
 
-  scanf("%d", &n);
+  cin >> n;
   for (int i = 0; i < n; i++) {
-    scanf("%d", &arr[i]);
+    cin >> arr[i];
     ++scores[arr[i]];
   }
   for (int tmp, rank = 1, i = 100; i >= 0; i--) {
@@ -16,5 +21,5 @@ int main() {
     if (scores[i]) scores[i] = rank;
     rank += tmp;
   }
-  for (int i = 0; i < n; i++) printf("%d\n", scores[arr[i]]);
+  for (int i = 0; i < n; i++) cout << scores[arr[i]] << '\n';
 }
