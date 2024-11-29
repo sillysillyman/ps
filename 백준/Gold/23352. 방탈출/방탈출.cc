f920da2a1c62f492837380b59af0593ed6936364
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-11-30 00:49:04 +0900

    [Gold V] Title: 방탈출, Time: 12 ms, Memory: 1236 KB -BaekjoonHub

--- 백준/Gold/23352. 방탈출/방탈출.cc
+++ 백준/Gold/23352. 방탈출/방탈출.cc
@@ -8,13 +8,13 @@ using namespace std;
 int N, M;
 int dx[4] = {-1, 0, 0, 1};
 int dy[4] = {0, -1, 1, 0};
-bool visited[50][50];
 int graph[50][50];
 
 bool is_inside(int x, int y) { return 0 <= x && x < N && 0 <= y && y < M; }
 
 pair<int, int> bfs(int x, int y) {
   int src = graph[x][y], dest, max_dist = 0;
+  bool visited[50][50] = {};
   queue<tuple<int, int, int>> q;
 
   visited[x][y] = true;
@@ -45,10 +45,12 @@ int main() {
   for (int i = 0; i < N; i++)
     for (int j = 0; j < M; j++) scanf("%d", &graph[i][j]);
   for (int i = 0; i < N; i++) {
-    for (int j = 0; j < M; j++) {
-      fill(&visited[0][0], &visited[N - 1][M], false);
-      if (graph[i][j]) p = max(p, bfs(i, j));
-    }
+    if (graph[i][0]) p = max(p, bfs(i, 0));
+    if (graph[i][M - 1]) p = max(p, bfs(i, M - 1));
+  }
+  for (int j = 0; j < M; j++) {
+    if (graph[0][j]) p = max(p, bfs(0, j));
+    if (graph[N - 1][j]) p = max(p, bfs(N - 1, j));
   }
   printf("%d", p.second);
 }
