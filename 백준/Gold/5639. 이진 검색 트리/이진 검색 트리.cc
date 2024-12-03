8ef82712dfa46a97ca97d78f988947589fc1999b
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-12-03 11:42:27 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 4 ms, Memory: 1500 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -1,25 +1,21 @@
-#include <iostream>
-
-using namespace std;
+#include <algorithm>
+#include <cstdio>
 
 int tree[10'000];
 
 void postorder(int start, int end) {
   if (start >= end) return;
 
-  int right = start + 1;
+  int parent = tree[start];
+  int right = std::upper_bound(tree + start + 1, tree + end, parent) - tree;
 
-  while (right < end && tree[right] < tree[start]) right++;
   postorder(start + 1, right);
   postorder(right, end);
-  cout << tree[start] << '\n';
+  printf("%d\n", parent);
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-
   int num, size = 0;
-  while (cin >> num) tree[size++] = num;
+  while (~scanf("%d", &num)) tree[size++] = num;
   postorder(0, size);
 }
