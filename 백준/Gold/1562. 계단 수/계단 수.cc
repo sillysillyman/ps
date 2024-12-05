789cd8ed74df209bdf552fab88ec66cc9d32a14a
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-12-05 11:58:29 +0900

    [Gold I] Title: 계단 수, Time: 12 ms, Memory: 9192 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cc
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -1,9 +1,7 @@
 #include <cstdio>
 
-using ll = long long;
-
 const int MOD = 1e9;
-ll dp[101][10][1 << 10];
+long long dp[101][10][1 << 10];
 
 int main() {
   int N;
@@ -14,22 +12,17 @@ int main() {
   for (int i = 2; i <= N; i++) {
     for (int j = 0; j < 10; j++) {
       for (int k = 0; k < (1 << 10); k++) {
-        if (j > 0) {
-          dp[i][j][k | (1 << j)] += dp[i - 1][j - 1][k];
-          dp[i][j][k | (1 << j)] %= MOD;
-        }
-        if (j < 9) {
-          dp[i][j][k | (1 << j)] += dp[i - 1][j + 1][k];
-          dp[i][j][k | (1 << j)] %= MOD;
-        }
+        if (j > 0)
+          dp[i][j][k | (1 << j)] =
+              (dp[i][j][k | (1 << j)] + dp[i - 1][j - 1][k]) % MOD;
+        if (j < 9)
+          dp[i][j][k | (1 << j)] =
+              (dp[i][j][k | (1 << j)] + dp[i - 1][j + 1][k]) % MOD;
       }
     }
   }
 
-  ll cnt = 0;
-  for (int i = 0; i < 10; i++) {
-    cnt += dp[N][i][(1 << 10) - 1];
-    cnt %= MOD;
-  }
-  printf("%lld", cnt);
+  int cnt = 0;
+  for (int i = 0; i < 10; i++) cnt = (cnt + dp[N][i][(1 << 10) - 1]) % MOD;
+  printf("%d", cnt);
 }
