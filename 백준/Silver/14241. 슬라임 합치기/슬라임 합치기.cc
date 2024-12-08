297009652e6a2c248a518026496ac3cbc731a837
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-12-08 23:55:42 +0900

    [Silver III] Title: 슬라임 합치기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/14241. 슬라임 합치기/슬라임 합치기.cc
+++ 백준/Silver/14241. 슬라임 합치기/슬라임 합치기.cc
@@ -1,16 +1,14 @@
 #include <algorithm>
 #include <cstdio>
 
-int N, size, score;
+int N, score;
 int arr[100];
 
 int main() {
   scanf("%d", &N);
   for (int i = 0; i < N; i++) scanf("%d", &arr[i]);
   std::sort(arr, arr + N);
-  size = arr[0] + arr[1];
-  score += arr[0] * arr[1];
-  for (int i = 2; i < N; i++) {
+  for (int size = arr[0], i = 1; i < N; i++) {
     score += size * arr[i];
     size += arr[i];
   }
