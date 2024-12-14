47517a48b5c5e459194843494eb5f8bb7038a2d7
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-12-14 19:12:28 +0900

    [Silver V] Title: 슈퍼 소수, Time: 0 ms, Memory: 1432 KB -BaekjoonHub

--- 백준/Silver/31216. 슈퍼 소수/슈퍼 소수.cc
+++ 백준/Silver/31216. 슈퍼 소수/슈퍼 소수.cc
@@ -3,20 +3,20 @@
 
 const int MAX_N = 318'138;
 int p[3'001];
-bool sieve[MAX_N];
+bool dp[MAX_N];
 
 int main() {
   int T, n;
 
-  std::fill(sieve + 2, sieve + MAX_N, true);
+  std::fill(dp + 2, dp + MAX_N, true);
   for (int i = 2; i < MAX_N; i++) {
-    if (!sieve[i]) continue;
-    for (int j = 2; i * j < MAX_N; j++) sieve[i * j] = false;
+    if (!dp[i]) continue;
+    for (int j = 2; i * j < MAX_N; j++) dp[i * j] = false;
   }
 
   for (int k = 0, n = 0, i = 2; i < MAX_N; i++) {
-    if (!sieve[i]) continue;
-    if (sieve[++k]) p[++n] = i;
+    if (!dp[i]) continue;
+    if (dp[++k]) p[++n] = i;
   }
 
   scanf("%d", &T);
