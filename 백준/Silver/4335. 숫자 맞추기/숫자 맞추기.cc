d579cbea0b3c171546f18e0073db46244a2d5ab9
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-12-20 00:42:41 +0900

    [Silver V] Title: 숫자 맞추기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/4335. 숫자 맞추기/숫자 맞추기.cc
+++ 백준/Silver/4335. 숫자 맞추기/숫자 맞추기.cc
@@ -1,10 +1,9 @@
 #include <cstdio>
 
+int arr[11];
+
 int main() {
   int n;
-  int arr[11];
-
-  for (int i = 1; i < 11; i++) arr[i] = 1;
 
   while (scanf("%d", &n) && n) {
     char c;
@@ -12,11 +11,11 @@ int main() {
     scanf("%*s %c%*s", &c);
 
     if (c == 'o') {
-      printf("%s\n", arr[n] ? "Stan may be honest" : "Stan is dishonest");
-      for (int i = 1; i < 11; i++) arr[i] = 1;
+      printf("%s\n", !arr[n] ? "Stan may be honest" : "Stan is dishonest");
+      for (int i = 1; i < 11; i++) arr[i] = 0;
     } else if (c == 'l')
-      for (int i = 1; i <= n; i++) arr[i] = 0;
+      for (int i = 1; i <= n; i++) arr[i] = 1;
     else
-      for (int i = n; i <= 10; i++) arr[i] = 0;
+      for (int i = n; i <= 10; i++) arr[i] = 1;
   }
 }
