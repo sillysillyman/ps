1538cc4b79ee9ff217e92b9bb2be5d9bbf261634
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-12-23 03:29:19 +0900

    [Bronze I] Title: 반올림, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2033. 반올림/반올림.cc
+++ 백준/Bronze/2033. 반올림/반올림.cc
@@ -3,28 +3,15 @@
 
 int main() {
   char N[9];
-
   scanf("%s", N);
 
   int carry = 0;
   for (int i = strlen(N) - 1; i >= 0; i--) {
     int digit = N[i] - '0' + carry;
 
-    if (!i) {
-      if (digit > 9) {
-        carry = 1;
-        N[i] = '0';
-      } else {
-        carry = 0;
-        N[i] = digit + '0';
-      }
-    } else {
-      if (digit < 5)
-        carry = 0;
-      else
-        carry = 1;
-      N[i] = '0';
-    }
+    carry = i ? (digit >= 5) : (digit > 9);
+    N[i] = i ? '0' : (digit % 10 + '0');
   }
-  carry ? printf("1%s", N) : printf("%s", N);
+  if (carry) printf("1");
+  printf("%s", N);
 }
