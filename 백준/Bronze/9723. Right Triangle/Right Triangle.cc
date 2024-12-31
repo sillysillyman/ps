c4c3b76b5e66cb6d7f187a9b4816a5b63200e972
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2024-12-31 19:15:29 +0900

    [Bronze III] Title: Right Triangle, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/9723. Right Triangle/Right Triangle.cc
+++ 백준/Bronze/9723. Right Triangle/Right Triangle.cc
@@ -1,24 +1,18 @@
 #include <algorithm>
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
 
 int sides[3];
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-
   int T;
-  cin >> T;
+  scanf("%d", &T);
 
   for (int t = 1; t <= T; t++) {
-    for (int i = 0; i < 3; i++) cin >> sides[i];
-    sort(sides, sides + 3);
-    cout << "Case #" << t << ": "
-         << (sides[2] * sides[2] == sides[0] * sides[0] + sides[1] * sides[1]
-                 ? "YES"
-                 : "NO")
-         << '\n';
+    for (int i = 0; i < 3; i++) scanf("%d", &sides[i]);
+    std::sort(sides, sides + 3);
+    printf("Case #%d: %s\n", t,
+           sides[2] * sides[2] == sides[0] * sides[0] + sides[1] * sides[1]
+               ? "YES"
+               : "NO");
   }
 }
