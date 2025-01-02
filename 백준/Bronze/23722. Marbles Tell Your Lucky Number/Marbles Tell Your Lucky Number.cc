ef0d231ccc742d1537ab69e9dbb9a7d39bbd682f
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-02 22:52:12 +0900

    [Bronze I] Title: Marbles Tell Your Lucky Number, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/23722. Marbles Tell Your Lucky Number/Marbles Tell Your Lucky Number.cc
+++ 백준/Bronze/23722. Marbles Tell Your Lucky Number/Marbles Tell Your Lucky Number.cc
@@ -1,32 +1,20 @@
-#include <algorithm>
 #include <cstdio>
+#include <algorithm>
 
 int arr[4];
-
-bool is_terminated() {
-  return (arr[0] && !arr[1] && !arr[2] && !arr[3]) ||
-         (!arr[0] && arr[1] && !arr[2] && !arr[3]) ||
-         (!arr[0] && !arr[1] && arr[2] && !arr[3]) ||
-         (!arr[0] && !arr[1] && !arr[2] && arr[3]);
-}
-
-std::pair<int, int> calc_min() {
-  int min_idx = -1;
-  int min_val = 101;
-
-  for (int i = 0; i < 4; i++)
-    if (arr[i] && arr[i] < min_val) min_idx = i, min_val = arr[i];
-
-  return {min_idx, min_val};
-}
 
 int main() {
   while (scanf("%d%d%d%d", &arr[0], &arr[1], &arr[2], &arr[3]) &&
          (arr[0] || arr[1] || arr[2] || arr[3])) {
-    while (!is_terminated()) {
-      auto [min_idx, min_val] = calc_min();
+    int min_idx = -1;
+    int min_val = 101;
+
+    while ((arr[0] || arr[1] || arr[2]) && (arr[0] || arr[1] || arr[3]) &&
+           (arr[0] || arr[2] || arr[3]) && (arr[1] || arr[2] || arr[3])) {
       for (int i = 0; i < 4; i++)
-        if (arr[i] && i != min_idx) arr[i] -= min_val;
+        if (arr[i] && arr[i] < min_val) min_idx = i, min_val = arr[i];
+      for (int i = 0; i < 4; i++)
+        if (arr[i] && min_idx != i) arr[i] -= min_val;
     }
 
     printf("%d\n", *std::max_element(arr, arr + 4));
