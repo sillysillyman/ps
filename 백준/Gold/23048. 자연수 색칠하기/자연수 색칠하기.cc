9f5e611a43e44589b21b3df24d393f6541d96f31
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-03 01:05:02 +0900

    [Gold V] Title: 자연수 색칠하기, Time: 40 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Gold/23048. 자연수 색칠하기/자연수 색칠하기.cc
+++ 백준/Gold/23048. 자연수 색칠하기/자연수 색칠하기.cc
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-bool sieve[500'001];
 int colors[500'001];
 
 int main() {
@@ -14,14 +13,10 @@ int main() {
 
   int color = 1;
   colors[1] = color;
-  for (int i = 2; i <= N; i++) sieve[i] = true;
   for (int i = 2; i <= N; i++) {
-    if (!sieve[i]) continue;
+    if (colors[i]) continue;
     colors[i] = ++color;
-    for (int j = 2; i * j <= N; j++) {
-      sieve[i * j] = false;
-      colors[i * j] = color;
-    }
+    for (int j = 2; i * j <= N; j++) colors[i * j] = color;
   }
   cout << color << '\n';
   for (int i = 1; i <= N; i++) cout << colors[i] << ' ';
