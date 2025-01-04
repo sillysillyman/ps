e6d546179ec79994a32e63e9a193e8acaeeac74b
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-04 17:17:14 +0900

    [Silver V] Title: 삼각형만들기, Time: 204 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2622. 삼각형만들기/삼각형만들기.cc
+++ 백준/Silver/2622. 삼각형만들기/삼각형만들기.cc
@@ -5,9 +5,9 @@ int n, cnt;
 int main() {
   scanf("%d", &n);
   for (int i = 1; i < n / 3 + 1; i++) {
-    for (int j = i; j < (n - 1) / 2 + 1; j++) {
+    for (int j = i; j < (n - i) / 2 + 1; j++) {
       int k = n - i - j;
-      if (k >= i && k >= j && k < i + j) ++cnt;
+      if (i + j > k) ++cnt;
     }
   }
   printf("%d", cnt);
