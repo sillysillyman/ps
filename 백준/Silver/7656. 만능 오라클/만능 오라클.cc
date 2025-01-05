1d9139c833c86e0130ab7a91c595953636857d16
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-05 23:35:34 +0900

    [Silver V] Title: 만능 오라클, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Silver/7656. 만능 오라클/만능 오라클.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <regex>
+
+using namespace std;
+
+int main() {
+  string s;
+  getline(cin, s);
+
+  regex pattern("What is([^?.]*)\\?");
+  for (sregex_iterator it(s.begin(), s.end(), pattern), end; it != end; it++)
+    cout << "Forty-two is" << it->str(1) << ".\n";
+}
