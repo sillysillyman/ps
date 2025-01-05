4da11993ff890cfb68096a40bf4bc02ea0cce881
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-05 17:10:51 +0900

    [Silver V] Title: 부분 문자열, Time: 0 ms, Memory: 1212 KB -BaekjoonHub

--- 백준/Silver/6550. 부분 문자열/부분 문자열.cc
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.cc
@@ -1,13 +1,9 @@
 #include <cstdio>
-#include <cstring>
 
-char s[100'001], t[100'001];
+char s[100'001];
 
 int main() {
-  while (~scanf(" %s %s", s, t)) {
-    int j = 0;
-    for (int i = 0; t[i]; i++)
-      if (s[j] == t[i]) ++j;
-    printf("%s\n", j == strlen(s) ? "Yes" : "No");
-  }
+  for (char *p; ~scanf("%s", p = s); puts(*p ? "No" : "Yes"))
+    for (char c; (c = getchar()) ^ '\n'; p += *p == c)
+      ;
 }
