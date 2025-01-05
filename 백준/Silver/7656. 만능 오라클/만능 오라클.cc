87cd42250404f08df6069ba3eb9ed9560570fcff
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-05 23:35:53 +0900

    [Silver V] Title: 만능 오라클, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Silver/7656. 만능 오라클/만능 오라클.cc
+++ 백준/Silver/7656. 만능 오라클/만능 오라클.cc
@@ -7,7 +7,7 @@ int main() {
   string s;
   getline(cin, s);
 
-  regex pattern("What is([^?.]*)\\?");
+  regex pattern("What is([^.?]*)\\?");
   for (sregex_iterator it(s.begin(), s.end(), pattern), end; it != end; it++)
     cout << "Forty-two is" << it->str(1) << ".\n";
 }
