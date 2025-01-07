bc64e493915e5d91c0733486ab01cf99e88c5b52
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-07 13:00:42 +0900

    [Bronze IV] Title: 바코드 닉네임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/32288. 바코드 닉네임/바코드 닉네임.cc
+++ 백준/Bronze/32288. 바코드 닉네임/바코드 닉네임.cc
@@ -3,7 +3,6 @@
 char s[501];
 
 int main() {
-  int n;
-  scanf("%d %s", &n, s);
-  for (int i = 0; i < n; i++) printf("%c", s[i] == 'I' ? 'i' : 'L');
+  scanf("%*d %s", s);
+  for (int i = 0; s[i]; i++) printf("%c", s[i] == 'I' ? 'i' : 'L');
 }
