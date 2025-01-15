918af09be648c31f2cbb2f609e523352263361ee
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-16 01:57:16 +0900

    [Silver V] Title: 수들의 합 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
@@ -4,8 +4,7 @@ int main() {
   int N, cnt = 0;
 
   scanf("%d", &N);
-  for (int i = 1; i * (i + 1) / 2 <= N; i++) {
-    if (!((N - i * (i + 1) / 2) % i)) ++cnt;
-  }
+  for (int i = 1; i * (i - 1) / 2 < N; i++)
+    if (!((N - i * (i - 1) / 2) % i)) ++cnt;
   printf("%d", cnt);
 }
