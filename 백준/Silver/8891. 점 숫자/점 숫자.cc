46b123014f033c171184473b7cc062af9a495ef8
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-15 18:22:15 +0900

    [Silver IV] Title: 점 숫자, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/8891. 점 숫자/점 숫자.cc
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <utility>
+
+using namespace std;
+
+struct Point {
+  int x, y;
+
+  Point operator+(const Point& other) const {
+    return {x + other.x, y + other.y};
+  }
+};
+
+int pairing(Point p) { return (p.x + p.y - 2) * (p.x + p.y - 1) / 2 + p.x; }
+
+Point unpairing(int z) {
+  int s = 2;
+  while ((s - 2) * (s - 1) / 2 < z) s++;
+  --s;
+  int x = z - (s - 2) * (s - 1) / 2;
+  return {x, s - x};
+}
+
+int main() {
+  int T;
+  scanf("%d", &T);
+  while (T--) {
+    int p, q;
+    scanf("%d%d", &p, &q);
+    printf("%d\n", pairing(unpairing(p) + unpairing(q)));
+  }
+}
