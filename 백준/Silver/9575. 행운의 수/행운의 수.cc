2f5c8abd50461dc8682544b9b30b932f214186e0
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-17 17:31:37 +0900

    [Silver IV] Title: 행운의 수, Time: 84 ms, Memory: 2232 KB -BaekjoonHub

---
+++ 백준/Silver/9575. 행운의 수/행운의 수.cc
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <map>
+
+int A[50], B[50], C[50];
+
+bool is_lucky_number(int num) {
+  while (num) {
+    if (int digit = num % 10; digit != 5 && digit != 8) return false;
+    num /= 10;
+  }
+  return true;
+}
+
+int main() {
+  int T;
+  scanf("%d", &T);
+
+  while (T--) {
+    int N, M, K;
+
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) scanf("%d", &A[i]);
+    scanf("%d", &M);
+    for (int i = 0; i < M; i++) scanf("%d", &B[i]);
+    scanf("%d", &K);
+    for (int i = 0; i < K; i++) scanf("%d", &C[i]);
+
+    std::map<int, int> cnts;
+    for (int i = 0; i < N; i++)
+      for (int j = 0; j < M; j++)
+        for (int k = 0; k < K; k++) ++cnts[A[i] + B[j] + C[k]];
+
+    int cnt = 0;
+    for (auto [k, v] : cnts)
+      if (is_lucky_number(k)) ++cnt;
+    printf("%d\n", cnt);
+  }
+}
