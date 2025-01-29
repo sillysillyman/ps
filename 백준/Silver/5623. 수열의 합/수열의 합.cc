5289fbf3417052d22980cb9a05cb3e3ea9103e15
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-30 00:47:48 +0900

    [Silver IV] Title: 수열의 합, Time: 136 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/5623. 수열의 합/수열의 합.cc
+++ 백준/Silver/5623. 수열의 합/수열의 합.cc
@@ -1,19 +1,21 @@
 #include <cstdio>
 
 int N;
-long long total;
-int mat[1'000][1'000];
+long long sum;
+int arr[1'000];
 
 int main() {
   scanf("%d", &N);
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < N; j++) scanf("%d", &mat[i][j]), total += mat[i][j];
-
-  if (N == 2) return printf("%d %d", mat[0][1] / 2, mat[0][1] / 2), 0;
-  total /= 2 * (N - 1);
   for (int i = 0; i < N; i++) {
-    int sum = 0;
-    for (int j = 0; j < N; j++) sum += mat[i][j];
-    printf("%d ", (sum - total) / (N - 2));
+    for (int num, j = 0; j < N; j++) {
+      scanf("%d", &num);
+      arr[i] += num;
+    }
+    sum += arr[i];
   }
+
+  if (N == 2) return printf("%d %d", arr[0] / 2, arr[0] / 2), 0;
+
+  sum /= 2 * (N - 1);
+  for (int i = 0; i < N; i++) printf("%lld ", (arr[i] - sum) / (N - 2));
 }
