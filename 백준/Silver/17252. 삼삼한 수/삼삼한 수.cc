388ed0d3a14d960b30197d548cbbebe86e26ee45
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-30 15:36:23 +0900

    [Silver IV] Title: 삼삼한 수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/17252. 삼삼한 수/삼삼한 수.cc
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main() {
+  int N;
+  scanf("%d", &N);
+  if (!N) return puts("NO"), 0;
+
+  long long n = 1;
+  while (3 * n <= N) n *= 3;
+  while (n && N > 0) {
+    if (N >= n) N -= n;
+    n /= 3;
+  }
+  printf("%s", N ? "NO" : "YES");
+}
