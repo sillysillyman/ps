c42396065eaa9a2490e7378dc4d313c634f9f897
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-31 02:29:43 +0900

    [Silver IV] Title: Dome Construction, Time: 88 ms, Memory: 4380 KB -BaekjoonHub

---
+++ 백준/Silver/17797. Dome Construction/Dome Construction.cc
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+struct Point {
+  double x, y, z;
+};
+
+Point points[100'000];
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  for (int i = 0; i < n; i++) cin >> points[i].x >> points[i].y >> points[i].z;
+  sort(points, points + n, [](const Point& p1, const Point& p2) {
+    return p1.x * p1.x + p1.y * p1.y + p1.z * p1.z <
+           p2.x * p2.x + p2.y * p2.y + p2.z * p2.z;
+  });
+
+  Point p = points[k - 1];
+  cout << fixed;
+  cout.precision(6);
+  cout << sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+}
