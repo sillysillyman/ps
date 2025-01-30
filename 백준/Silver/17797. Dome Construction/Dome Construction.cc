a3a4a1f6b63ed6f8890ae8606f0d3977ed9e2414
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-01-31 02:32:39 +0900

    [Silver IV] Title: Dome Construction, Time: 80 ms, Memory: 4380 KB -BaekjoonHub

--- 백준/Silver/17797. Dome Construction/Dome Construction.cc
+++ 백준/Silver/17797. Dome Construction/Dome Construction.cc
@@ -17,10 +17,11 @@ int main() {
   int n, k;
   cin >> n >> k;
   for (int i = 0; i < n; i++) cin >> points[i].x >> points[i].y >> points[i].z;
-  sort(points, points + n, [](const Point& p1, const Point& p2) {
-    return p1.x * p1.x + p1.y * p1.y + p1.z * p1.z <
-           p2.x * p2.x + p2.y * p2.y + p2.z * p2.z;
-  });
+  nth_element(points, points + k - 1, points + n,
+              [](const Point& p1, const Point& p2) {
+                return p1.x * p1.x + p1.y * p1.y + p1.z * p1.z <
+                       p2.x * p2.x + p2.y * p2.y + p2.z * p2.z;
+              });
 
   Point p = points[k - 1];
   cout << fixed;
