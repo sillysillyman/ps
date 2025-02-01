c713fba497938914a05a87e7760e923d542db987
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-02-01 20:49:43 +0900

    [Gold III] Title: 소수 4개의 합, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Gold/14905. 소수 4개의 합/소수 4개의 합.cc
@@ -0,0 +1,34 @@
+#include <cstdio>
+
+bool is_prime(int n) {
+  if (n <= 1) return false;
+  for (int i = 2; i * i <= n; i++)
+    if (!(n % i)) return false;
+  return true;
+}
+
+void solve(int n) {
+  if (n < 8) {
+    printf("Impossible.\n");
+    return;
+  }
+  if (n & 1) {
+    for (int i = 2; i < n / 2; i++)
+      if (is_prime(i) && is_prime(n - i - 5)) {
+        printf("2 3 %d %d\n", i, n - i - 5);
+        return;
+      }
+  } else {
+    for (int i = 2; i < n / 2; i++)
+      if (is_prime(i) && is_prime(n - i - 4)) {
+        printf("2 2 %d %d\n", i, n - i - 4);
+        return;
+      }
+  }
+  printf("Impossible.\n");
+}
+
+int main() {
+  int N;
+  while (~scanf("%d", &N)) solve(N);
+}
