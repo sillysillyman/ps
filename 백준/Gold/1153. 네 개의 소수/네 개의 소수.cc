3afe30159f1d914632d5513be792ce4489dc9275
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-02-01 20:56:05 +0900

    [Gold III] Title: 네 개의 소수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/1153. 네 개의 소수/네 개의 소수.cc
+++ 백준/Gold/1153. 네 개의 소수/네 개의 소수.cc
@@ -11,14 +11,12 @@ int main() {
   int N;
   scanf("%d", &N);
 
-  if (N & 1) {
-    for (int i = 2; i < N / 2; i++)
-      if (is_prime(i) && is_prime(N - i - 5))
-        return printf("2 3 %d %d", i, N - i - 5), 0;
-  } else {
-    for (int i = 2; i < N / 2; i++)
-      if (is_prime(i) && is_prime(N - i - 4))
-        return printf("2 2 %d %d", i, N - i - 4), 0;
+  for (int i = 2; i < N / 2; i++) {
+    if (!is_prime(i)) continue;
+    if (N & 1 && is_prime(N - i - 5))
+      return printf("2 3 %d %d", i, N - i - 5), 0;
+    else if (!(N & 1) && is_prime(N - i - 4))
+      return printf("2 2 %d %d", i, N - i - 4), 0;
   }
   printf("-1");
 }
