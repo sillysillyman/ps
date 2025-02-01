d06e64da4360208749a06fc8bcc045eaf8b42305
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-02-01 20:43:26 +0900

    [Gold III] Title: 네 개의 소수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/1153. 네 개의 소수/네 개의 소수.cc
+++ 백준/Gold/1153. 네 개의 소수/네 개의 소수.cc
@@ -1,27 +1,26 @@
 #include <cstdio>
 
-const int MAX_N = 1e6;
-bool sieve[MAX_N + 1];
-int primes[78'498];
+bool is_prime(int n) {
+  if (n <= 1) return false;
+  for (int i = 2; i * i <= n; i++)
+    if (!(n % i)) return false;
+  return true;
+}
 
 int main() {
   int N;
   scanf("%d", &N);
 
-  for (int i = 2; i < MAX_N + 1; i++) sieve[i] = true;
-  for (int i = 2; i < MAX_N + 1; i++)
-    if (sieve[i])
-      for (int j = 2; i * j < MAX_N + 1; j++) sieve[i * j] = false;
-
-  int idx = 0;
-  for (int i = 0; i <= N; i++)
-    if (sieve[i]) primes[idx++] = i;
-
-  for (int i = 0; i < idx; i++)
-    for (int j = i; j < idx; j++)
-      for (int k = j; k < idx; k++)
-        if (int r = N - (primes[i] + primes[j] + primes[k]); sieve[r])
-          return printf("%d %d %d %d", primes[i], primes[j], primes[k], r), 0;
-
+  if (N < 8)
+    return printf("-1"), 0;
+  else if (N & 1) {
+    for (int i = 2; i < N / 2; i++)
+      if (is_prime(i) && is_prime(N - i - 5))
+        return printf("2 3 %d %d", i, N - i - 5), 0;
+  } else {
+    for (int i = 2; i < N / 2; i++)
+      if (is_prime(i) && is_prime(N - i - 4))
+        return printf("2 2 %d %d", i, N - i - 4), 0;
+  }
   printf("-1");
 }
