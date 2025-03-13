242ac77d2ad94584fa3f855709bb48c86ad94abb
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-03-13 17:09:01 +0900

    [Gold II] Title: 성냥개비, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.cc
+++ 백준/Gold/3687. 성냥개비/성냥개비.cc
@@ -11,8 +11,8 @@ int main() {
   for (int i = 1; i < 9; i++) dp[i] = arr[i];
   dp[6] = 6;
   for (int i = 9; i < 101; i++) {
-    dp[i] = dp[i - 2] * 10 + 1;
-    for (int j = 3; j < 8; j++)
+    dp[i] = ~(1LL << 63);
+    for (int j = 2; j < 8; j++)
       dp[i] = std::min(dp[i], 10 * dp[i - j] + arr[j]);
   }
 
