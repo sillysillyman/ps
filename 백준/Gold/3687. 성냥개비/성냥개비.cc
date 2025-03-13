03148e0927a5748b8cfa4e85ca41ab1acc81f164
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-03-13 16:52:25 +0900

    [Gold II] Title: 성냥개비, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Gold/3687. 성냥개비/성냥개비.cc
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cstdio>
+
+int arr[9] = {0, 0, 1, 7, 4, 2, 0, 8, 10};
+long long dp[101];
+
+int main() {
+  int t;
+  scanf("%d", &t);
+
+  for (int i = 1; i < 9; i++) dp[i] = arr[i];
+  dp[6] = 6;
+  for (int i = 9; i < 101; i++) {
+    dp[i] = dp[i - 2] * 10 + 1;
+    for (int j = 3; j < 8; j++)
+      dp[i] = std::min(dp[i], 10 * dp[i - j] + arr[j]);
+  }
+
+  while (t--) {
+    int n;
+    scanf("%d", &n);
+
+    printf("%lld ", dp[n]);
+    if (n & 1) {
+      printf("7");
+      for (int i = 0; i < (n - 3) / 2; i++) printf("1");
+    } else {
+      for (int i = 0; i < n / 2; i++) printf("1");
+    }
+    printf("\n");
+  }
+}
