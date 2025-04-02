eb961cfd7f85889eea59a073126b1196a9d98166
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-04-02 18:50:17 +0900

    [Bronze II] Title: 시험 감독, Time: 108 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+long long arr[(int)1e6];
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, B, C;
+  long long cnt = 0;
+  cin >> N;
+  for (int i = 0; i < N; i++) cin >> arr[i];
+  cin >> B >> C;
+
+  for (int i = 0; i < N; i++) {
+    arr[i] -= arr[i] < B ? arr[i] : B;
+    cnt += arr[i] / C + !!(arr[i] % C) + 1;
+  }
+
+  cout << cnt;
+}
