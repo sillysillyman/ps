5f93e22d469233466e5436fc93d0ef1921dce899
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-04-07 00:40:26 +0900

    [Bronze III] Title: Centroid of Point Masses, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9945. Centroid of Point Masses/Centroid of Point Masses.cc
+++ 백준/Bronze/9945. Centroid of Point Masses/Centroid of Point Masses.cc
@@ -1,19 +1,27 @@
-#include <cstdio>
+#include <iostream>
+
+using namespace std;
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
   for (int t = 1;; t++) {
     int n;
-    scanf("%d", &n);
+    cin >> n;
     if (n < 0) break;
 
     long long X = 0, Y = 0, M = 0;
     for (int x, y, m, i = 0; i < n; i++) {
-      scanf("%d%d%d", &x, &y, &m);
+      cin >> x >> y >> m;
       X += x * m;
       Y += y * m;
       M += m;
     }
 
-    printf("Case %d: %.2lf %.2lf\n", t, (double)X / M, (double)Y / M);
+    cout << fixed;
+    cout.precision(2);
+    cout << "Case " << t << ": " << (double)X / M << ' ' << (double)Y / M
+         << '\n';
   }
 }
