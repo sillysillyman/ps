50828e4946d133aedbf1f85ccb91368dcc094cc2
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-04-12 01:14:07 +0900

    [Bronze III] Title: 꼬리를 무는 숫자 나열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.cc
+++ 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.cc
@@ -4,6 +4,6 @@
 int main() {
   int a, b;
   scanf("%d%d", &a, &b);
-  printf("%d", abs(a / 4 + !!(a % 4) - b / 4 - !!(b % 4)) +
-                   abs((a - 1) % 4 - (b - 1) % 4));
+  printf("%d",
+         abs(a / 4 + !!(a % 4) - b / 4 - !!(b % 4)) + abs(--a % 4 - --b % 4));
 }
