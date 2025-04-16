89bfb83c577b154896b9d13195b11bf126749d83
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-04-16 20:05:37 +0900

    [Silver V] Title: 매직스퀘어, Time: 0 ms, Memory: 1152 KB -BaekjoonHub

--- 백준/Silver/15739. 매직스퀘어/매직스퀘어.cc
+++ 백준/Silver/15739. 매직스퀘어/매직스퀘어.cc
@@ -1,39 +1,32 @@
 #include <cstdio>
 
+int d1, d2;
 int arr[(int)1e4 + 1];
-int mat[100][100];
+int row_sums[100];
+int col_sums[100];
 
 int main() {
   int N;
   scanf("%d", &N);
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
-      scanf("%d", &mat[i][j]);
-      ++arr[mat[i][j]];
+      int x;
+      scanf("%d", &x);
+      ++arr[x];
+      row_sums[i] += x;
+      col_sums[j] += x;
+      if (i == j) d1 += x;
+      if (i + j == N - 1) d2 += x;
     }
   }
+
   for (int i = 1; i <= N * N; i++)
     if (arr[i] != 1) return printf("FALSE"), 0;
 
-  int x, y, n = N * (N * N + 1) / 2;
-  for (int i = 0; i < N; i++) {
-    x = 0;
-    for (int j = 0; j < N; j++) x += mat[i][j];
-    if (x != n) return printf("FALSE"), 0;
-  }
-
-  for (int j = 0; j < N; j++) {
-    x = 0;
-    for (int i = 0; i < N; i++) x += mat[i][j];
-    if (x != n) return printf("FALSE"), 0;
-  }
-
-  x = 0, y = 0;
-  for (int i = 0; i < N; i++) {
-    x += mat[i][i];
-    y += mat[N - 1 - i][i];
-  }
-  if (x != n || y != n) return printf("FALSE"), 0;
+  int n = N * (N * N + 1) / 2;
+  if (d1 != n || d2 != n) return printf("FALSE"), 0;
+  for (int i = 0; i < N; i++)
+    if (row_sums[i] != n || col_sums[i] != n) return printf("FALSE"), 0;
 
   printf("TRUE");
 }
