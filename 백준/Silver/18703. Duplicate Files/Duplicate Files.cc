677abd8e837cc45727d9e8f0cb07a39408f22c6a
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-04-19 23:25:59 +0900

    [Silver V] Title: Duplicate Files, Time: 460 ms, Memory: 10780 KB -BaekjoonHub

--- 백준/Silver/18703. Duplicate Files/Duplicate Files.cc
+++ 백준/Silver/18703. Duplicate Files/Duplicate Files.cc
@@ -1,9 +1,12 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
+
+vector<int> ids;
+unordered_map<string, int> um;
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -12,19 +15,20 @@ int main() {
   int T;
   cin >> T;
   while (T--) {
+    ids.clear();
+    um.clear();
+
     int N;
-    map<string, int> m;
 
     cin >> N;
     while (N--) {
       int id;
       string s;
       cin >> s >> id;
-      if (m.find(s) == m.end() || m[s] > id) m[s] = id;
+      if (um.find(s) == um.end() || um[s] > id) um[s] = id;
     }
 
-    vector<int> ids;
-    for (auto [k, v] : m) ids.push_back(v);
+    for (auto [k, v] : um) ids.push_back(v);
     sort(ids.begin(), ids.end());
 
     for (int e : ids) cout << e << ' ';
