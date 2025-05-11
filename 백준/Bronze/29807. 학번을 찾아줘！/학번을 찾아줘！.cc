6b744bea7391e149dd1ea4710ed0dd923b3de2af
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-05-11 21:18:53 +0900

    [Bronze IV] Title: 학번을 찾아줘!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/29807. 학번을 찾아줘！/학번을 찾아줘！.cc
+++ 백준/Bronze/29807. 학번을 찾아줘！/학번을 찾아줘！.cc
@@ -7,7 +7,5 @@ int main() {
   int T;
   scanf("%d", &T);
   for (int i = 0; i < T; i++) scanf("%d", &arr[i]);
-  printf("%d", 4763 * (abs((arr[0] - arr[2])) * (arr[0] > arr[2] ? 508 : 108) +
-                       abs(arr[1] - arr[3]) * (arr[1] > arr[3] ? 212 : 305) +
-                       707 * arr[4]));
+  printf("%d", 4763 * (abs((arr[0] - arr[2])) * (arr[0] > arr[2] ? 508 : 108) + abs(arr[1] - arr[3]) * (arr[1] > arr[3] ? 212 : 305) + 707 * arr[4]));
 }
