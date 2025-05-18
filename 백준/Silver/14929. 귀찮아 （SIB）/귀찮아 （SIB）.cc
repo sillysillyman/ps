b684a94d6608ddf4a8de51678424bb7e9c27becf
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-05-18 12:28:03 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.cc
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.cc
@@ -2,19 +2,17 @@
 
 using namespace std;
 
-int arr[(int)1e5 + 1];
-
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n;
+  int n, prefix = 0;
   long long s = 0;
   cin >> n;
-  for (int i = 1; i <= n; i++) {
-    cin >> arr[i];
-    arr[i] += arr[i - 1];
+  for (int x, i = 0; i < n; i++) {
+    cin >> x;
+    s += prefix * x;
+    prefix += x;
   }
-  for (int i = 1; i < n; i++) s += (arr[i] - arr[i - 1]) * (arr[n] - arr[i]);
   cout << s;
 }
