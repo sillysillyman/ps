3fbff573b646f4e339597863594f0c44a2f856b4
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-05-20 22:55:34 +0900

    [Bronze III] Title: 369, Time: 12 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.cc
+++ 백준/Bronze/17614. 369/369.cc
@@ -3,7 +3,7 @@
 int main() {
   int N, cnt = 0;
   scanf("%d", &N);
-  for (int n, i = 0; i <= N; i += 3) {
+  for (int n, i = 0; i <= N; i++) {
     n = i;
     while (n) {
       if (n % 10 == 3 || n % 10 == 6 || n % 10 == 9) ++cnt;
