73c6bfedeabcfec1e1ba72dfbf6f1beab08440f1
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-06-03 20:19:02 +0900

    [Bronze II] Title: 거리의 합, Time: 0 ms, Memory: 1192 KB -BaekjoonHub

--- 백준/Bronze/2399. 거리의 합/거리의 합.cc
+++ 백준/Bronze/2399. 거리의 합/거리의 합.cc
@@ -1,14 +1,15 @@
+#include <algorithm>
 #include <cmath>
 #include <cstdio>
 
-int arr[10'000];
+long long arr[10'000];
 
 int main() {
   int n;
   long long sum =0;
   scanf("%d", &n);
-  for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++) sum += abs(arr[i] - arr[j]);
-  printf("%lld", sum);
+  for (int i = 0; i < n; i++) scanf("%lld", &arr[i]);
+  std::sort(arr, arr + n, std::greater());
+  for (int i = 0; i < n; i++) sum += (n - 2 * i - 1) * arr[i];
+  printf("%lld", 2 * sum);
 }
