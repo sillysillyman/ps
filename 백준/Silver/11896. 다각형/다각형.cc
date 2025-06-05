3228c1c2d91a449855fd69819a08f17b7c2f79ec
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-06-05 23:29:36 +0900

    [Silver V] Title: 다각형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/11896. 다각형/다각형.cc
+++ 백준/Silver/11896. 다각형/다각형.cc
@@ -6,7 +6,6 @@ int main() {
   if (a < 3) a = 4;
   if (b < 3) return printf("0"), 0;
   a = (++a / 2) * 2;
-  b = (b / 2) * 2;
-  b = (b - a) / 2 + 1;
+  b = ((b / 2) * 2 - a) / 2 + 1;
   printf("%lld", b * (a + b - 1));
 }
