d0f7116b39632be1339cfef38c3c72cfe794b03d
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-06-15 13:03:12 +0900

    [Silver I] Title: 행렬, Time: 0 ms, Memory: 1120 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.cc
+++ 백준/Silver/1080. 행렬/행렬.cc
@@ -1,26 +1,21 @@
 #include <cstdio>
 
 int A[50][50];
-int B[50][50];
 
 int main() {
   int N, M, cnt = 0;
   scanf("%d%d", &N, &M);
   for (int i = 0; i < N; i++)
     for (int j = 0; j < M; j++) scanf("%1d", &A[i][j]);
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < M; j++) scanf("%1d", &B[i][j]);
-
-  for (int i = 0; i < N - 2; i++) {
-    for (int j = 0; j < M - 2; j++) {
-      if (A[i][j] == B[i][j]) continue;
+  for (int i = 0; i < N; i++) {
+    for (int x, j = 0; j < M; j++) {
+      scanf("%1d", &x);
+      if (x == A[i][j]) continue;
+      if (i > N - 3 || j > M - 3) return printf("-1"), 0;
       ++cnt;
       for (int r = i; r < i + 3; r++)
         for (int c = j; c < j + 3; c++) A[r][c] ^= 1;
     }
   }
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < M; j++)
-      if (A[i][j] != B[i][j]) return printf("-1"), 0;
   printf("%d", cnt);
 }
