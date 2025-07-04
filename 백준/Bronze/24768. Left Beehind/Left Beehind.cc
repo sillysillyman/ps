c2488ad2693a8f06b0d28294f2d5c468844d4ff5
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-07-05 01:55:55 +0900

    [Bronze IV] Title: Left Beehind, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24768. Left Beehind/Left Beehind.cc
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main() {
+  int x, y;
+  while (scanf("%d%d", &x, &y) && (x || y))
+    printf("%s.\n", x + y == 13 ? "Never speak again" : x > y ? "To the convention" : x < y ? "Left beehind" : "Undecided");
+}
