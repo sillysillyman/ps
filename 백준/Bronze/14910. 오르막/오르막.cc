dee9d2648760d27366e64c850c3c2b68bc0b7d63
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-07-06 16:35:40 +0900

    [Bronze II] Title: 오르막, Time: 92 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Bronze/14910. 오르막/오르막.cc
+++ 백준/Bronze/14910. 오르막/오르막.cc
@@ -8,10 +8,10 @@ int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int k, flag = 0, n = 1;
+  int k, n = 1;
   while (cin >> k) {
     arr[n] = k;
-    if (arr[n - 1] > arr[n++]) flag = 1;
+    if (arr[n - 1] > arr[n++]) return cout << "Bad", 0;
   }
-  cout << (flag ? "Bad" : "Good");
+  cout << "Good";
 }
