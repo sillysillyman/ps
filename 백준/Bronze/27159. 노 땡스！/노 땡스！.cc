a69626990c9e531eb10876bce083fec33dd923d0
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-07-06 16:12:21 +0900

    [Bronze III] Title: 노 땡스!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27159. 노 땡스！/노 땡스！.cc
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int N, n;
+int arr[34];
+
+int main() {
+  scanf("%d", &N);
+  for (int i = 1; i <= N; i++) scanf("%d", &arr[i]);
+  for (int i = 1; i <= N; i++)
+    if (arr[i] - arr[i - 1] != 1) n += arr[i];
+  printf("%d", n);
+}
