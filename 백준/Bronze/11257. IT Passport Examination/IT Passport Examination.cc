d6ebd82926b499509b5f97c3e32bf8d0dca1653a
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-07-13 16:21:46 +0900

    [Bronze IV] Title: IT Passport Examination, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11257. IT Passport Examination/IT Passport Examination.cc
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main() {
+  int N;
+  scanf("%d", &N);
+  while (N--) {
+    int n, a, b, c;
+    scanf("%d%d%d%d", &n, &a, &b, &c);
+    printf("%d %d %s\n", n, a + b + c, a + b + c > 54 && a > 10 && b > 7 && c > 11 ? "PASS" : "FAIL");
+  }
+}
