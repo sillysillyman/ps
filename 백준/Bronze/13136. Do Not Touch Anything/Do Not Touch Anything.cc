acd0bdc3d460159ed1a79b02e8875978888fe38c
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-08-03 19:55:39 +0900

    [Bronze IV] Title: Do Not Touch Anything, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/13136. Do Not Touch Anything/Do Not Touch Anything.cc
+++ 백준/Bronze/13136. Do Not Touch Anything/Do Not Touch Anything.cc
@@ -3,7 +3,5 @@
 int main() {
   long long R, C, N;
   scanf("%lld%lld%lld", &R, &C, &N);
-  R = ((R + N - 1) / N) * N;
-  C = ((C + N - 1) / N) * N;
-  printf("%lld", R * C / (N * N));
+  printf("%lld", ((R + N - 1) / N) * ((C + N - 1) / N));
 }
