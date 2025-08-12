9618a9f0a4ada17d99ec76f02a5a25267dcc9d23
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-08-13 00:39:24 +0900

    [Bronze IV] Title: DORO, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/34073. DORO/DORO.cc
+++ 백준/Bronze/34073. DORO/DORO.cc
@@ -3,9 +3,6 @@
 int main() {
   char c;
   scanf("%*d\n");
-  while (scanf("%c", &c) == 1 && c != '\n') {
-    if (c == ' ') printf("DORO ");
-    else printf("%c", c);
-  }
+  while (scanf("%c", &c) == 1 && c != '\n') c == ' ' ? printf("DORO ") : printf("%c", c);
   printf("DORO");
 }
