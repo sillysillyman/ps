79f3c891fad812267231470380ce0673bbbe373d
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-08-21 22:34:20 +0900

    [Bronze IV] Title: Basketball Score, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/32306. Basketball Score/Basketball Score.cc
+++ 백준/Bronze/32306. Basketball Score/Basketball Score.cc
@@ -1,8 +1,11 @@
 #include <cstdio>
 
+int n;
+
 int main() {
-  int a, b, c, d, e, f;
-  scanf("%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f);
-  int A = a + 2 * b + 3 * c, B = d + 2 * e + 3 * f;
-  printf("%d", A == B ? 0 : A > B ? 1 : 2);
+  for (int x, i = 0; i < 6; i++) {
+    scanf("%d", &x);
+    n += (i > 2 ? -1 : 1) * (i % 3 + 1) * x;
+  }
+  printf("%d", !n ? 0 : n > 0 ? 1 : 2);
 }
