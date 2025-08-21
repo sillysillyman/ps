e9b5c47d51420bb21ffe03e6519e3b7e7e024c49
sillysillyman <sillysillyman.cs@gmail.com>
2025-08-21 02:22:52 +0000

    Time: 0 ms (100%), Space: 56 MB (83.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++
@@ -1,7 +0,0 @@
-function twoSum(nums: number[], target: number): number[] {
-  const map = new Map<number, number>();
-  for (let i = 0; i < nums.length; i++) {
-    if (map.has(target - nums[i])) return [map.get(target - nums[i]), i];
-    map.set(nums[i], i);
-  }
-};
