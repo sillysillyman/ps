bfa7eff29fba2027f7a969429c4e19194ffe59a4
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-08-22 15:44:45 +0900

    [Bronze IV] Title: Juta teekond, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/32722. Juta teekond/Juta teekond.cc
+++ 백준/Bronze/32722. Juta teekond/Juta teekond.cc
@@ -3,5 +3,5 @@
 int main() {
   int a, b, c;
   scanf("%d%d%d", &a, &b, &c);
-  printf("%s", (a & 1 && a < 4) && (!(b & 1) && b > 5) && (c == 2 || c == 5) ? "JAH" : "EI");
+  printf((a & 1 && a < 4) && (!(b & 1) && b > 5) && (c == 2 || c == 5) ? "JAH" : "EI");
 }
