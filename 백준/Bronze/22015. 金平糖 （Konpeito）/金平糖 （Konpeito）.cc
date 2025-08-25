7988d2d38eb4e7085f5a90803a5b25ee31126a25
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-08-25 15:56:58 +0900

    [Bronze IV] Title: 金平糖 (Konpeito), Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22015. 金平糖 （Konpeito）/金平糖 （Konpeito）.cc
+++ 백준/Bronze/22015. 金平糖 （Konpeito）/金平糖 （Konpeito）.cc
@@ -4,6 +4,6 @@ int A, B, C, max;
 
 int main() {
   scanf("%d%d%d", &A, &B, &C);
-  max = A < C && B < C ? C : A < B && C < B ? B : A;
+  max = A <= C && B <= C ? C : A <= B && C <= B ? B : A;
   printf("%d", 3 * max - A - B - C);
 }
