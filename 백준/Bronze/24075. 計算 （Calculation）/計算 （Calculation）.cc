0e546f68d01502f7b85c1bdd2a5eebc651dab881
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-08-26 13:30:15 +0900

    [Bronze IV] Title: 計算 (Calculation), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/24075. 計算 （Calculation）/計算 （Calculation）.cc
+++ 백준/Bronze/24075. 計算 （Calculation）/計算 （Calculation）.cc
@@ -3,6 +3,6 @@
 int main() {
   int A, B;
   scanf("%d%d", &A, &B);
-  B > 0 ? (A += B, B = A - 2 * B) : (A -= B, B = A + 2 * B);
-  printf("%d\n%d", A, B);
+  B < 0 && (B = -B);
+  printf("%d\n%d", A + B, A - B);
 }
