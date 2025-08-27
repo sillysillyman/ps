8263fab1b15e4ec1e0c4b8d5b0e506e0a2997fff
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-08-27 12:08:19 +0900

    [Bronze IV] Title: Hurra!, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/26767. Hurra！/Hurra！.cc
+++ 백준/Bronze/26767. Hurra！/Hurra！.cc
@@ -5,9 +5,10 @@ using namespace std;
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
+  cout.tie(nullptr);
 
   int N;
   cin >> N;
   for (int n = 1; n <= N; n++)
-    cout << (!(n % 77)   ? "Wiwat!" : !(n % 11) ? "Super!" : !(n % 7)  ? "Hurra!" : to_string(n)) << '\n';
+    cout << (!(n % 77) ? "Wiwat!" : !(n % 11) ? "Super!" : !(n % 7)  ? "Hurra!" : to_string(n)) << '\n';
 }
