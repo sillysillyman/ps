e6611597bb143bff5dd4334ff47760230068b110
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-08-28 11:37:18 +0900

    [Bronze IV] Title: 준영이의 등급, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/30008. 준영이의 등급/준영이의 등급.cc
+++ 백준/Bronze/30008. 준영이의 등급/준영이의 등급.cc
@@ -1,18 +1,11 @@
 #include <cstdio>
 
-int N, K;
-int arr[10] = {0, 4, 11, 23, 40, 60, 77, 89, 96, 100};
-
 int main() {
+  int N, K;
   scanf("%d%d", &N, &K);
-  for (int G, i = 0; i < K; i++) {
+  for (int G, p, i = 0; i < K; i++) {
     scanf("%d", &G);
-    int p = G * 100 / N;
-    for (int d = 1; d < 10; d++) {
-      if (p <= arr[d]) {
-        printf("%d ", d);
-        break;
-      }
-    }
+    p = G * 100 / N;
+    printf("%d ", p < 5 ? 1 : p < 12 ? 2 : p < 24 ? 3 : p < 41 ? 4 : p < 61 ? 5 : p < 78 ? 6 : p < 90 ? 7 : p < 97 ? 8 : 9);
   }
 }
