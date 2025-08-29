648bf721416ebf8396a968ae5917cb6f42151479
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-08-29 16:33:58 +0900

    [Bronze IV] Title: 揃った文字 (Matched Letters), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/31616. 揃った文字 （Matched Letters）/揃った文字 （Matched Letters）.cc
+++ 백준/Bronze/31616. 揃った文字 （Matched Letters）/揃った文字 （Matched Letters）.cc
@@ -4,6 +4,6 @@ int main() {
   int N;
   char S[100];
   scanf("%d %s", &N, S);
-  for (int i = 1; i < N; i++) if (S[i] != S[i - 1]) return printf("No"), 0;
+  for (int i = 1; S[i]; i++) if (S[i] != S[i - 1]) return printf("No"), 0;
   printf("Yes");
 }
