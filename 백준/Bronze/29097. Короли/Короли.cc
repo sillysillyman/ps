607b887eabe2d59a470d971d5d1fde206d81cc05
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-09-02 13:55:40 +0900

    [Bronze IV] Title: Короли, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/29097. Короли/Короли.cc
+++ 백준/Bronze/29097. Короли/Короли.cc
@@ -4,8 +4,7 @@ int main() {
   int n, m, k, a, b, c, M;
   scanf("%d%d%d%d%d%d", &n, &m, &k, &a, &b, &c);
   n *= a, m *= b, k *= c;
-  M = n > m && n > k ? n : m > k ? m : k;
-  n == M && printf("Joffrey ");
-  m == M && printf("Robb ");
-  k == M && printf("Stannis");
+  n >= m && n >= k && printf("Joffrey ");
+  m >= n && m >= k && printf("Robb ");
+  k >= n && k >= m && printf("Stannis");
 }
