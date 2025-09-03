77f5ddf65e910ab6c6cb151c34a606d060dfbbf7
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-09-03 22:11:56 +0900

    [Bronze IV] Title: 선택 가능성이 가장 높은 자료형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/32278. 선택 가능성이 가장 높은 자료형/선택 가능성이 가장 높은 자료형.cc
+++ 백준/Bronze/32278. 선택 가능성이 가장 높은 자료형/선택 가능성이 가장 높은 자료형.cc
@@ -5,5 +5,5 @@ int main() {
   long long N;
   scanf("%lld", &N);
   if (N < 0) N  = ~N;
-  printf("%s", N <= SHRT_MAX ? "short" : N <= INT_MAX ? "int" : "long long");
+  printf(N <= SHRT_MAX ? "short" : N <= INT_MAX ? "int" : "long long");
 }
