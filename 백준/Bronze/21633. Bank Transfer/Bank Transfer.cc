cf6104bdd828f8a9438cbb7cebc498a06cacd4a0
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-09-04 11:03:50 +0900

    [Bronze IV] Title: Bank Transfer, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/21633. Bank Transfer/Bank Transfer.cc
+++ 백준/Bronze/21633. Bank Transfer/Bank Transfer.cc
@@ -1,8 +1,8 @@
 #include <cstdio>
 
 int main() {
-  int k;
-  scanf("%d", &k);
-  float c = 25 + k / 100.;
+  float k, c;
+  scanf("%f", &k);
+  c = 25 + k / 100;
   printf("%.2f", c < 100 ? 100 : c > 2000 ? 2000 : c);
 }
