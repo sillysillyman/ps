50edac97aacf014f3246c6a5c652273112d75c59
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-09-07 14:13:16 +0900

    [Bronze III] Title: Count Vowels, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10203. Count Vowels/Count Vowels.cc
+++ 백준/Bronze/10203. Count Vowels/Count Vowels.cc
@@ -1,15 +1,13 @@
 #include <cstdio>
 
 int main() {
-  int T;
+  int T, c;
   char s[30];
   scanf("%d", &T);
   while (T--) {
-    int c = 0;
+    c = 0;
     scanf("%s", s);
-    for (int i = 0; s[i]; i++)
-      c += s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
-           s[i] == 'u';
+    for (int i = 0; s[i]; i++) c += s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u';
     printf("The number of vowels in %s is %d.\n", s, c);
   }
 }
