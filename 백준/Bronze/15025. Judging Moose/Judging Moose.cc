4315b59f907b3b1bd374f9a715c66af6ad611b82
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-09-14 20:55:40 +0900

    [Bronze IV] Title: Judging Moose, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15025. Judging Moose/Judging Moose.cc
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main() {
+  int l, r;
+  scanf("%d%d", &l, &r);
+  if (!l && !r) return printf("Not a moose"), 0;
+  printf("%s %d", l == r ? "Even" : "Odd", l > r ? 2 * l : 2 * r);
+}
