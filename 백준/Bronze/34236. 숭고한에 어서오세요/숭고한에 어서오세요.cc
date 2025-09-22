536783504490027622e24aa85194e5ebad36af8d
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-09-22 13:32:07 +0900

    [Bronze IV] Title: 숭고한에 어서오세요, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/34236. 숭고한에 어서오세요/숭고한에 어서오세요.cc
+++ 백준/Bronze/34236. 숭고한에 어서오세요/숭고한에 어서오세요.cc
@@ -1,10 +1,9 @@
 #include <cstdio>
 
 int main() {
-  int N, a, b, c;
-  scanf("%d%d%d", &N, &a, &b);
-  if (N == 2) return printf("%d", 2 * b - a), 0;
-  for (int i = 0; i < N - 3; i++) scanf("%*d");
-  scanf("%d", &c);
-  printf("%d", c + b - a);
+  int N;
+  int arr[100];
+  scanf("%d", &N);
+  for (int i = 0; i < N; i++) scanf("%d", &arr[i]);
+  printf("%d", arr[N - 1] + arr[1] - arr[0]);
 }
