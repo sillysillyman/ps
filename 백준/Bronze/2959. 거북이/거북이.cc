8cfe8e2d1e4190b187b8f6ebd3ab3b71cebb7770
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-09-25 11:35:43 +0900

    [Bronze III] Title: 거북이, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2959. 거북이/거북이.cc
@@ -0,0 +1,9 @@
+#include <algorithm>
+#include <cstdio>
+
+int main() {
+  int arr[4];
+  for (int i = 0; i < 4; i++) scanf("%d", &arr[i]);
+  std::sort(arr, arr + 4);
+  printf("%d", arr[0] * arr[2]);
+}
