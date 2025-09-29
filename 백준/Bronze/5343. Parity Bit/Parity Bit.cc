d3386b1021573de82dcd7ce7bfeee6c9636d94d7
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-09-29 11:23:24 +0900

    [Bronze III] Title: Parity Bit, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5343. Parity Bit/Parity Bit.cc
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int N;
+char b[64];
+
+int main() {
+  scanf("%d", &N);
+  while (N--) {
+    int e = 0;
+    scanf("%s", b);
+    for (int c = 0, i = 0; b[i]; i++) {
+      if ((i + 1) % 8) c += b[i] == '1';
+      else {
+        e += (c & 1 && b[i] == '0') || (!(c & 1) && b[i] == '1');
+        c = 0;
+      }
+    }
+    printf("%d\n", e);
+  }
+}
