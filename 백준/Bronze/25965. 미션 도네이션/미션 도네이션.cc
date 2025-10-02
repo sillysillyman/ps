1af31c5870f89785d7c6e82a75cca52d8998e482
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-10-02 21:08:32 +0900

    [Bronze III] Title: 미션 도네이션, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/25965. 미션 도네이션/미션 도네이션.cc
+++ 백준/Bronze/25965. 미션 도네이션/미션 도네이션.cc
@@ -8,7 +8,7 @@ int main() {
   while (N--) {
     long long n = 0;
     scanf("%d", &M);
-    for (int i = 0; i < M; i++) scanf("%lld%lld%lld", &K[i], &D[i], &A[i]);
+    for (int i = 0; i < M; i++) scanf("%lld%lld%lld", K + i, D + i, A + i);
     scanf("%d%d%d", &k, &d, &a);
     for (int i = 0; i < M; i++)
       if (long long c = k * K[i] - d * D[i] + a * A[i]; c > 0) n += c;
