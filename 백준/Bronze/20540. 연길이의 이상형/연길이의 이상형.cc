8f450ca79195ae6786a14317b8d3bdf1cf2f73d7
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-10-10 12:17:13 +0900

    [Bronze III] Title: 연길이의 이상형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/20540. 연길이의 이상형/연길이의 이상형.cc
+++ 백준/Bronze/20540. 연길이의 이상형/연길이의 이상형.cc
@@ -3,5 +3,5 @@
 int main() {
   char s[4];
   scanf("%s", s);
-  printf("%c%c%c%c", s[0] == 'E' ? 'I' : 'E', s[1] == 'N' ? 'S' : 'N', s[2] == 'F' ? 'T' : 'F', s[3] == 'J' ? 'P' : 'J');
+  printf("%c%c%c%c", 'E' ^ 'I' ^ s[0], 'N' ^ 'S' ^ s[1], 'F' ^ 'T' ^ s[2], 'J' ^ 'P' ^ s[3]);
 }
