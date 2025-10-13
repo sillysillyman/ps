459168755f89d25ff4be57fec2dadea234cff6a7
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-10-13 11:39:32 +0900

    [Bronze III] Title: Win Streak, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/34302. Win Streak/Win Streak.cc
+++ 백준/Bronze/34302. Win Streak/Win Streak.cc
@@ -7,13 +7,7 @@ int main() {
   scanf("%d", &N);
   while (N--) {
     scanf("%d%d", &S, &T);
-    if (S > T) {
-      w = flag ? w + 1 : 1;
-      flag = 1;
-    } else {
-      w = 0;
-      flag = 0;
-    }
+    S > T ? (w = flag ? w + 1 : 1, flag = 1) : (w = flag = 0);
     if (w > W) W = w;
   }
   printf("%d", W);
