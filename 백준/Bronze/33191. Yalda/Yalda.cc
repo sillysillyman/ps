158c31ac93fbabab312e89a69378a0200283c24e
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-10-16 00:10:24 +0900

    [Bronze IV] Title: Yalda, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/33191. Yalda/Yalda.cc
+++ 백준/Bronze/33191. Yalda/Yalda.cc
@@ -3,5 +3,5 @@
 int main() {
   int b, w, p, n;
   scanf("%d%d%d%d", &b, &w, &p, &n);
-  printf("%s", w < ++b ? "Watermelon" : p < b ? "Pomegranates" : n < b ? "Nuts" : "Nothing");
+  printf(w < ++b ? "Watermelon" : p < b ? "Pomegranates" : n < b ? "Nuts" : "Nothing");
 }
