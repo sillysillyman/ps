7e4a5ee1a8df3ab8252a46b82f04661c590c5f2a
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-10-17 14:48:32 +0900

    [Bronze III] Title: 단원평가, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/31798. 단원평가/단원평가.cc
+++ 백준/Bronze/31798. 단원평가/단원평가.cc
@@ -1,8 +1,11 @@
-#include <cmath>
 #include <cstdio>
 
 int main() {
   int a, b, c;
   scanf("%d%d%d", &a, &b, &c);
-  printf("%d", !a || !b ? c * c - (!a ? b : a) : (int)sqrt(a + b));
+  if (!a || !b)
+    printf("%d", c * c - (!a ? b : a));
+  else
+    for (int i = 1;; i++)
+      if (i * i == a + b) return printf("%d", i), 0;
 }
