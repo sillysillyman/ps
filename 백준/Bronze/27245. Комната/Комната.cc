0514d77ee25a3db181f8ed85dfa3c0eef86365ce
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-10-28 18:05:27 +0900

    [Bronze IV] Title: Комната, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/27245. Комната/Комната.cc
+++ 백준/Bronze/27245. Комната/Комната.cc
@@ -3,6 +3,6 @@
 int main() {
   int w, l, h;
   scanf("%d%d%d", &w, &l, &h);
-  if (l < w) w ^= l ^= w ^= l;
+  l < w ? w = l, l = w : 0;
   printf(1. * w / h >= 2 && 1. * l / w <= 2 ? "good" : "bad");
 }
