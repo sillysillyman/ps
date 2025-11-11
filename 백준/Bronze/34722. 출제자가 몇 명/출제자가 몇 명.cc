8ce7d0517804db00478a004b8cccd5885b81a49e
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-11-11 18:22:28 +0900

    [Bronze IV] Title: 출제자가 몇 명, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/34722. 출제자가 몇 명/출제자가 몇 명.cc
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int n, N, s, c, a, r;
+
+int main() {
+  scanf("%d", &N);
+  while (N--) {
+    scanf("%d%d%d%d", &s, &c, &a, &r);
+    n += s > 999 || c > 1599 || a > 1499 || (r != -1 && r < 31);
+  }
+  printf("%d", n);
+}
