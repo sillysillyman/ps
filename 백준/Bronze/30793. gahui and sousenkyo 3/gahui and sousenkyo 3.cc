dd501a66bc63f895a3ed81c259b9fb1719154ae9
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-11-12 16:04:24 +0900

    [Bronze IV] Title: gahui and sousenkyo 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/30793. gahui and sousenkyo 3/gahui and sousenkyo 3.cc
+++ 백준/Bronze/30793. gahui and sousenkyo 3/gahui and sousenkyo 3.cc
@@ -4,5 +4,5 @@ int main() {
   int p, r;
   scanf("%d%d", &p, &r);
   double v = 1. * p / r;
-  printf(v < 0.2 ? "weak" : v < 0.4 ? "normal" : v < 0.6 ? "strong" : "very strong");
+  printf(v < .2 ? "weak" : v < .4 ? "normal" : v < .6 ? "strong" : "very strong");
 }
