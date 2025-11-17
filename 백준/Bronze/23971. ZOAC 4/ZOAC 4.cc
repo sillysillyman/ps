61ea07c068ea89f66144a287c2a5a910cc1e5e95
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-11-17 13:52:58 +0900

    [Bronze III] Title: ZOAC 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.cc
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.cc
@@ -3,5 +3,5 @@
 int main() {
   int H, W, N, M;
   scanf("%d%d%d%d", &H, &W, &N, &M);
-  printf("%d", (H / (N + 1) + !!(H % (N + 1))) * (W / (M + 1) + !!(W % (M + 1))));
+  printf("%d", (H / ++N + !!(H % N)) * (W / ++M + !!(W % M)));
 }
