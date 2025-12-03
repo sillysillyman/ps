10b5f8c820405a94ac4a20267020f33fb9f8dd18
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-12-03 18:26:35 +0900

    [Bronze III] Title: 통학의 신, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.cc
+++ 백준/Bronze/17945. 통학의 신/통학의 신.cc
@@ -1,8 +1,9 @@
 #include <cstdio>
 
+int A, B, d;
+
 int main() {
-  int A, B, d;
   scanf("%d%d", &A, &B);
-  for (d = 0; d * d < A * A - B; d++) ;
+  while (d * d < A * A - B) d++;
   printf(d ? "%d %d" : "%d", -A - d, -A + d);
 }
