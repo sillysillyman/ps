9895fc0d9e823550efb3d9af2c4374b9d7d2b9a3
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-12-19 18:38:50 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -3,8 +3,5 @@
 int main() {
   char s[21];
   scanf("%s", s);
-  for (int i = 0; i < 9; i++) {
-    printf(":%s:", i == 4 ? s : "fan");
-    if (!((i + 1) % 3)) printf("\n");
-  }
+  printf(":fan::fan::fan:\n:fan::%s::fan:\n:fan::fan::fan:", s);
 }
