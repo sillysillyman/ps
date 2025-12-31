4d6d646e7685d2317f0555032e8c55a42e8df852
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-12-31 18:27:30 +0900

    [Bronze IV] Title: 안밖? 밖안? 계단? 역계단?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/28290. 안밖？ 밖안？ 계단？ 역계단？/안밖？ 밖안？ 계단？ 역계단？.cc
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <cstring>
+
+int main() {
+  char S[9];
+  scanf("%s", S);
+  printf(!strcmp(S, ";lkjfdsa")                             ? "reverse"
+         : !strcmp(S, "asdfjkl;")                           ? "stairs"
+         : !strcmp(S, "asdf;lkj") || !strcmp(S, ";lkjasdf") ? "out-in"
+         : !strcmp(S, "fdsajkl;") || !strcmp(S, "jkl;fdsa") ? "in-out"
+                                                            : "molu");
+}
