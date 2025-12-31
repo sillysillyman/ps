df6c3afd0219bb1e2792ebd8553f199a02f889b6
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2025-12-31 18:22:18 +0900

    [Bronze IV] Title: DKSH 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/29766. DKSH 찾기/DKSH 찾기.cc
+++ 백준/Bronze/29766. DKSH 찾기/DKSH 찾기.cc
@@ -7,6 +7,6 @@ char s[1'001];
 int main() {
   scanf("%s", s);
   for (int i = 0; s[i + 3]; i++)
-    if (!strncmp(s + i, "DKSH", 4)) ++cnt;
+    cnt += !strncmp(s + i, "DKSH", 4);
   printf("%d", cnt);
 }
