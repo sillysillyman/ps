fcc08cf36293ebbc218eaf0ac9ad388e7066ecab
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-01-06 18:00:08 +0900

    [Bronze IV] Title: Generalized FizzBuzz, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/32801. Generalized FizzBuzz/Generalized FizzBuzz.cc
+++ 백준/Bronze/32801. Generalized FizzBuzz/Generalized FizzBuzz.cc
@@ -4,7 +4,6 @@
 int main() {
   int n, a, b;
   scanf("%d%d%d", &n, &a, &b);
-  long long x = std::lcm(a, b);
-  x = n / x;
+  long long x = n / std::lcm(a, b);
   printf("%lld %lld %lld", n / a - x, n / b - x, x);
 }
