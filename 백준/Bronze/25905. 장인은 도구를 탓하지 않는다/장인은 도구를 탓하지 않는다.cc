c1dffae7f2555ab442c15e91423332903859c9ee
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-01-12 17:46:08 +0900

    [Bronze II] Title: 장인은 도구를 탓하지 않는다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/25905. 장인은 도구를 탓하지 않는다/장인은 도구를 탓하지 않는다.cc
+++ 백준/Bronze/25905. 장인은 도구를 탓하지 않는다/장인은 도구를 탓하지 않는다.cc
@@ -1,11 +1,12 @@
-#include <algorithm>
 #include <cstdio>
 
 int main() {
-  double arr[10];
-  for (int i = 0; i < 10; i++) scanf("%lf", &arr[i]);
-  std::sort(arr, arr + 10);
-  double p = 1e9 / 362880;
-  for (int i = 1; i < 10; i++) p *= arr[i];
-  printf("%lf", p);
+  double m = 1, p, P = 1e9 / 362880;
+  for (int i = 0; i < 10; i++) {
+    scanf("%lf", &p);
+    P *= p;
+    if (p < m) m = p;
+  }
+  P /= m;
+  printf("%lf", P);
 }
