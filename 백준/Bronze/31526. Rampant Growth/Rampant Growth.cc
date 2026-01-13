37baee25605691236db7073351441088fd0f0682
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-01-13 14:00:44 +0900

    [Bronze II] Title: Rampant Growth, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/31526. Rampant Growth/Rampant Growth.cc
+++ 백준/Bronze/31526. Rampant Growth/Rampant Growth.cc
@@ -3,9 +3,9 @@
 int main() {
   int r, c;
   scanf("%d%d", &r, &c);
-  long long v = r;
+  long long v = r--;
   for (int i = 1; i < c; i++) {
-    v *= r - 1;
+    v *= r;
     v %= 998244353;
   }
   printf("%lld", v);
