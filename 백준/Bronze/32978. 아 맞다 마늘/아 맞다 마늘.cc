f84af55697d54ddc73c3950d7f1995fcb15ee0ac
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-01-18 17:09:41 +0900

    [Bronze III] Title: 아 맞다 마늘, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Bronze/32978. 아 맞다 마늘/아 맞다 마늘.cc
+++ 백준/Bronze/32978. 아 맞다 마늘/아 맞다 마늘.cc
@@ -4,17 +4,16 @@
 using namespace std;
 
 int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
   int N;
   string s;
   set<string> S;
   cin >> N;
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < 2 * N - 1; i++) {
     cin >> s;
-    S.insert(s);
-  }
-  for (int i = 1; i < N; i++) {
-    cin >> s;
-    S.erase(s);
+    if (i < N) S.insert(s);
+    else S.erase(s);
   }
   cout << *S.begin();
 }
