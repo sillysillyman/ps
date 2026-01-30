d37be0e6afc1aa9aad83a66f474a7d929879565c
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-01-30 11:11:35 +0900

    [Bronze III] Title: ID, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/9907. ID/ID.cc
+++ 백준/Bronze/9907. ID/ID.cc
@@ -3,7 +3,7 @@
 int main() {
   int n = 0, N;
   scanf("%d", &N);
-  for (int i = 0; i < 7; i++, N /= 10) n += (i > 5 ? 2 : 2 + i) * (N % 10);
+  for (int i = 0; N; i++, N /= 10) n += (2 + i * (i < 6)) * (N % 10);
   n %= 11;
   printf("%c", !n ? 'J' : n > 9 ? 'Z' : 'A' + --n);
 }
