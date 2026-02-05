5d5c225a2b170bb9b9f52ff953f56574bf1cdf7a
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-02-05 14:41:50 +0900

    [Bronze III] Title: MIT Time, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/34459. MIT Time/MIT Time.cc
+++ 백준/Bronze/34459. MIT Time/MIT Time.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
@@ -5,18 +6,13 @@ using namespace std;
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
+  cout.tie(0);
   int T, N, k;
   int arr[] = {5, 25, 125, 625, 3125, 15625, 78125, 390625, 1953125, 9765625, 48828125, 244140625};
   cin >> T;
   while (T--) {
-    k = 13;
     cin >> N;
-    for (int i = 0; i < 12; i++) {
-      if (N <= arr[i]) {
-        k = i + 1;
-        break;
-      }
-    }
+    k = lower_bound(arr, arr + 12, N) - arr + 1;
     cout << "MIT";
     if (k - 1) cout << "^" << k;
     cout << " time\n";
