802ded6e085d43d8408b43dea93895a3c0de6e96
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-02-09 18:15:08 +0900

    [Bronze IV] Title: 2033년 밈 투표, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.cc
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.cc
@@ -1,13 +1,7 @@
 #include <cstdio>
 #include <cstring>
 
-const char *s[] = {"Never gonna give you up",
-                   "Never gonna let you down",
-                   "Never gonna run around and desert you",
-                   "Never gonna make you cry",
-                   "Never gonna say goodbye",
-                   "Never gonna tell a lie and hurt you",
-                   "Never gonna stop"};
+const char *s[] = {"Never gonna give you up", "Never gonna let you down", "Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye", "Never gonna tell a lie and hurt you", "Never gonna stop"};
 
 int main() {
   int N, f;
