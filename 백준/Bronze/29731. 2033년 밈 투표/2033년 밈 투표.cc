cd4b3523c9c47f78dc1904d9201cd6144cc95369
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-02-09 18:14:41 +0900

    [Bronze IV] Title: 2033년 밈 투표, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.cc
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <cstring>
+
+const char *s[] = {"Never gonna give you up",
+                   "Never gonna let you down",
+                   "Never gonna run around and desert you",
+                   "Never gonna make you cry",
+                   "Never gonna say goodbye",
+                   "Never gonna tell a lie and hurt you",
+                   "Never gonna stop"};
+
+int main() {
+  int N, f;
+  char S[51];
+  scanf("%d", &N);
+  while (N--) {
+    f = 0;
+    scanf(" %[^\n]", S);
+    for (int i = 0; i < 7; i++)
+      if (!strcmp(S, s[i])) f = 1;
+    if (!f) return puts("Yes"), 0;
+  }
+  puts("No");
+}
