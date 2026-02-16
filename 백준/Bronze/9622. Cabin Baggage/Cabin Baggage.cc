3f1547b225c29cf57ce41b2ebeb913599b7d56fe
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-02-16 15:35:56 +0900

    [Bronze III] Title: Cabin Baggage, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/9622. Cabin Baggage/Cabin Baggage.cc
+++ 백준/Bronze/9622. Cabin Baggage/Cabin Baggage.cc
@@ -6,7 +6,7 @@ int main() {
   scanf("%d", &t);
   while (t--) {
     scanf("%lf%lf%lf%lf", &l, &w, &d, &m);
-    f = ((l <= 56 && w <= 45 && d <= 25) || l + w + d <= 125) && m <= 7;
+    f = (l <= 56 && w <= 45 && d <= 25 || l + w + d <= 125) && m <= 7;
     c += f;
     printf("%d\n", f);
   }
