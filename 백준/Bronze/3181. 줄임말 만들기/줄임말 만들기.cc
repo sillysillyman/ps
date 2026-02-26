02523eb506fe155bf42764588088eaaab485c0e1
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-02-26 19:10:43 +0900

    [Bronze II] Title: 줄임말 만들기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/3181. 줄임말 만들기/줄임말 만들기.cc
+++ 백준/Bronze/3181. 줄임말 만들기/줄임말 만들기.cc
@@ -2,14 +2,12 @@
 #include <cstring>
 
 int main() {
-  int F = 1, f;
+  int F = 1, i;
   char s[101];
   const char *t[10] = {"i", "pa", "te", "ni", "niti", "a", "ali", "nego", "no", "ili"};
   while (~scanf("%s", s)) {
-    f = 0;
-    for (int i = 0; i < 10; i++) if (!strcmp(s, t[i])) f = 1;
-    if (!F && f) continue;
-    printf("%c", s[0] - 32);
+    for (i = 0; i < 10 && strcmp(s, t[i]); i++);
+    if (F || i == 10) printf("%c", s[0] - 32);
     F = 0;
   }
 }
