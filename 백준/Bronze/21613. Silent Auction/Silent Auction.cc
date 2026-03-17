a6c1126a3550f3072cad6199b50ef6101c3274c0
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-03-17 17:53:14 +0900

    [Bronze II] Title: Silent Auction, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/21613. Silent Auction/Silent Auction.cc
+++ 백준/Bronze/21613. Silent Auction/Silent Auction.cc
@@ -4,7 +4,7 @@
 int main() {
   int N;
   int arr[100];
-  char s[100][10];
+  char s[100][9];
   scanf("%d", &N);
   for (int i = 0; i < N; i++) scanf("%s%d", s[i], &arr[i]);
   printf("%s", s[std::max_element(arr, arr + N) - arr]);
