22b087672a81d481426bc11955340542377c5d7f
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-03-25 18:20:00 +0900

    [Bronze III] Title: 수학적 호기심, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.cc
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.cc
@@ -1,14 +1,18 @@
-#include <cstdio>
+#include <iostream>
+
+using namespace std;
 
 int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
   int T, n, m, c;
-  scanf("%d", &T);
+  cin >> T;
   while (T--) {
     c = 0;
-    scanf("%d%d", &n, &m);
+    cin >> n >> m;
     for (int i = 1; i < n - 1; i++)
       for (int j = i + 1; j < n; j++)
         c += !((i * i + j * j + m) % (i * j));
-    printf("%d\n", c);
+    cout << c << '\n';
   }
 }
