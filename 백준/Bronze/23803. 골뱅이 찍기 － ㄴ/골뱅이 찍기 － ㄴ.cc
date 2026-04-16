1c39345e17690678747f21096736cf77ff9d0068
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-04-16 21:58:44 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.cc
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.cc
@@ -1,14 +1,12 @@
-#include <cstdio>
-
-int main() {
-  int N, i, j;
-  scanf("%d", &N);
-  for (i = 0; i < 4 * N; i++) {
-    for (j = 0; j < N; j++) printf("@");
-    puts("");
-  }
-  for (i = 0; i < N; i++) {
-    for (j = 0; j < 5 * N; j++) printf("@");
-    puts("");
-  }
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
