3212e2194a4780c7e112a501d9c2b694ee490c63
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-04-16 21:50:40 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.cc
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.cc
@@ -1,14 +1,14 @@
 #include <cstdio>
 
 int main() {
-  int N;
+  int N, i, j;
   scanf("%d", &N);
-  for (int i = 0; i < 4 * N; i++) {
-    for (int j = 0; j < N; j++) putchar('@');
-    putchar('\n');
+  for (i = 0; i < 4 * N; i++) {
+    for (j = 0; j < N; j++) printf("@");
+    puts("");
   }
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < 5 * N; j++) putchar('@');
-    putchar('\n');
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < 5 * N; j++) printf("@");
+    puts("");
   }
 }
